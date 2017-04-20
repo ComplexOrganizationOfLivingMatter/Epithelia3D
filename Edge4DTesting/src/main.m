0862249c397f5e423b1259520f03e2a71f723c53
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-04-20 12:16:54 +0200

    getting all the info of the selected cells from the planes

--- Edge4DTesting/src/main.m
+++ Edge4DTesting/src/main.m
@@ -4,7 +4,7 @@ alpha = 0.246508; % microns
 w = 665;
 h = 332;
 d = 211;
-fid = fopen('D:\Pablo\Epithelia3D\Edge4D\data\OR_OliGreen_BP106_early03.raw');
+fid = fopen('D:\Pablo\Epithelia3D\Edge4DTesting\data\OR_OliGreen_BP106_early03.raw');
 label_matrix = fread(fid, w * h * d, '*int32');
 labels3d = reshape(label_matrix, [w h d]);
 fclose(fid);
@@ -13,13 +13,13 @@ labels3d = double(labels3d);
 
 colors = [colorcube(256); jet(256); parula(256); colorcube(256)];
 
-for i = 1:size(labels3d, 3)
-    img = labels3d(:, :, i);
-    %img(img ~= 0) = img(img ~= 0) - min(img(img ~= 0));
-    imwrite(img, colors, strcat('results/segments/segment_', num2str(i), '.jpg')); 
-end
+% for i = 1:size(labels3d, 3)
+%     img = labels3d(:, :, i);
+%     %img(img ~= 0) = img(img ~= 0) - min(img(img ~= 0));
+%     imwrite(img, colors, strcat('results/segments/segment_', num2str(i), '.jpg')); 
+% end
 
-load('D:\Pablo\Epithelia3D\Edge4D\results\locationsOfMotifs.mat')
+load('D:\Pablo\Epithelia3D\Edge4DTesting\results\locationsOfMotifs.mat')
 imshow(labels3d(:, :, 100));
 
 positions = {[119.53 89.48 56.5 52.15]
@@ -34,10 +34,10 @@ positions = {[119.53 89.48 56.5 52.15]
 [148.17 513.46 49.87 57.91]};
 positionsVector = vertcat(positions{:});
 
-for i = 1:size(positionsVector, 1)
-    croppedImg = imcrop(segment_50, positionsVector(i, :));
-    imwrite(croppedImg, strcat('results\motifSegments_atSegment50\motif_', num2str(i), '.png'));
-end
+% for i = 1:size(positionsVector, 1)
+%     croppedImg = imcrop(segment_50, positionsVector(i, :));
+%     imwrite(croppedImg, strcat('results\motifSegments_atSegment50\motif_', num2str(i), '.png'));
+% end
 
 selectedPairOfCells = [533, 780;
     557, 463;
@@ -65,17 +65,18 @@ selectedPairOfCells = [533, 780;
     532, 297];
 
 
-imgToShow = img;
-imgToShow(ismember(img, unique(selectedPairOfCells)) == 0) = 0;
-imshow(imgToShow);
+% imgToShow = img;
+% imgToShow(ismember(img, unique(selectedPairOfCells)) == 0) = 0;
+% imshow(imgToShow);
 
 img = labels3d(:, :, 50);
 se = strel('disk', 3);
 selectedCells = zeros(size(selectedPairOfCells, 1), 4);
+infoCells = {};
 for i = 1:size(selectedPairOfCells, 1)
     cell1 = selectedPairOfCells(i, 1);
     cell2 = selectedPairOfCells(i, 2);
-    
+    i
     cell1_dilate = imdilate(bwperim(img == cell1), se);
     cell2_dilate = imdilate(bwperim(img == cell2), se);
     
@@ -85,7 +86,27 @@ for i = 1:size(selectedPairOfCells, 1)
     neighboursCell1(neighboursCell1 == 0) = [];
     neighboursCell2(neighboursCell2 == 0) = [];
     selectedCells(i, :) = intersect(neighboursCell1, neighboursCell2);
+
+    for numCell = 1:4
+        regionPropsOfCells = [];
+        for zPlane = 1:size(labels3d, 3)
+            regionOfCell = struct2table(regionprops(labels3d(:, :, zPlane) == selectedCells(i, numCell), 'all'), 'AsArray', true);
+            if size(regionOfCell, 1) == 1 && regionOfCell.Area > 10
+                regionOfCell.zPlane = zPlane;
+                if isempty(regionPropsOfCells)
+                    regionPropsOfCells = regionOfCell;
+                else
+                    regionPropsOfCells(end+1, :) = regionOfCell;
+                end
+            end
+        end
+        infoCells{(4 * (i-1)) + numCell} = regionPropsOfCells;
+    end
+    
+    
     if i == 20
         img = labels3d(:, :, 100);
     end
 end
+
+infoCells
