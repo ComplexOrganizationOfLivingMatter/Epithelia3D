7e290e5af8b4204c140bf37e9009081359086ec0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-04-24 14:07:56 +0200

    roicells

--- Edge4DTesting/src/main.m
+++ Edge4DTesting/src/main.m
@@ -24,12 +24,13 @@ gettingInfoFromMotifs();
 roiCells = [105;107;119;123;131;144;149;158;179;194;215;218;221;223;229;282;289;292;301;304;308;320;323;331;335;339;341;346;350;360;364;366;372;373;375;381;382;385;389;390;406;409;410;422;425;426;429;430;436;437;438;440;443;445;447;460;461;462;463;464;471;472;476;477;497;501;503;505;506;510;514;516;517;530;531;539;545;547;550;557;559;566;568;569;571;572;573;576;584;585;587;595;597;598;599;600;604;606;609;613;614;617;618;631;634;636;639;645;648;659;660;663;666;667;677;678;679;688;692;694;697;700;705;712;714;728;780];
 imgPlaneWithLabels = labels3d(:, :, 40);
 
+%Getting mean area
 meanAreasAtROI1 = [];
 for numPlane = 1:size(labels3d, 3)
     imgPlaneWithLabels = labels3d(:, :, numPlane);
     imgPlaneWithLabelsCropped = imgPlaneWithLabels .* ismember(imgPlaneWithLabels, roiCells);
     imgPlaneWithLabelsNoHoles = imfill(imgPlaneWithLabelsCropped, 8, 'holes');
-    neighbourhoodAreas = arrayfun(@(x) sum(sum(imgPlaneWithLabelsNoHoles == x)), unique(imgPlaneWithLabelsNoHoles));
+    neighbourhoodAreas = arrayfun(@(x) sum(sum(imgPlaneWithLabelsNoHoles == x)), roiCells);
     meanAreasAtROI1(numPlane) = mean(neighbourhoodAreas);
 end
 
