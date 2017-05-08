9d44d494fd99e95f2fc138f1485f5e92d3a01f0b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-08 12:31:29 +0200

    creating a big bounding box to get all the frames

--- Embryo_Sol/main.m
+++ Embryo_Sol/main.m
@@ -47,11 +47,35 @@ for i = 1:size(roiMotifsInit, 1)
     if roiMotifsInit(i, 5) > 0
         img = imread(strcat(embryoDir, '\ImageSequence\Embr4_Channel0', num2str(roiMotifsInit(i, 5)), '.tif'));
         selectedRoi = roiMotifsInit(i, 1:4);
-        imwrite(imcrop(img, selectedRoi), strcat(actualDir, num2str(roiMotifsInit(i, 5)), '.tif'));
+        imwrite(imcrop(img, selectedRoi), strcat(actualDir, 'end_' ,num2str(roiMotifsInit(i, 5)), '.tif'));
 
         img = imread(strcat(embryoDir, '\ImageSequence\Embr4_Channel0', num2str(roiMotifsEnd(i, 5)), '.tif'));
         selectedRoi = roiMotifsEnd(i, 1:4);
-        imwrite(imcrop(img, selectedRoi), strcat(actualDir, num2str(roiMotifsEnd(i, 5)), '.tif'));
+        imwrite(imcrop(img, selectedRoi), strcat(actualDir, 'init_' ,num2str(roiMotifsEnd(i, 5)), '.tif'));
+        
+        %Bounding box of the two bounding boxes
+        roiPointsInit = bbox2points(roiMotifsInit(i, 1:4));
+        roiPointsEnd = bbox2points(roiMotifsEnd(i, 1:4));
+        
+        minX = min([roiPointsInit(:, 1); roiPointsEnd(:, 1)]);
+        minY = min([roiPointsInit(:, 2); roiPointsEnd(:, 2)]);
+        maxX = max([roiPointsInit(:, 1); roiPointsEnd(:, 1)]);
+        maxY = max([roiPointsInit(:, 2); roiPointsEnd(:, 2)]);
+        %Formula
+        bigBoundingBox = [minX minY xMax-xMin+1 maxY-minY+1];
+        
+        RGB = insertShape(img,'FilledRectangle',roiMotifsEnd(i, 1:4),'Color','green');
+        RGB = insertShape(RGB,'FilledRectangle',roiMotifsInit(i, 1:4),'Color','yellow');
+        RGB = insertShape(RGB,'FilledRectangle',bigBoundingBox,'Color','red');
+        imshow(RGB)
+        
+        initFrame = min(roiMotifsEnd(i, 5), roiMotifsInit(i, 5));
+        endFrame = max(roiMotifsInit(i, 5), roiMotifsEnd(i, 5));
+        %Printing
+        for numFrame = initFrame : endFrame
+            img = imread(strcat(embryoDir, '\ImageSequence\Embr4_Channel0', num2str(numFrame), '.tif'));
+            imwrite(imcrop(img, bigBoundingBox), strcat(actualDir, num2str(numFrame), '.tif'));
+        end
     end
 end
 
