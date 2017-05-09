25e317a2a34984b98ffc924ae0be835fa09fa387
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-09 10:12:06 +0200

    moving main

--- Gastrulation/Edge4DTesting/main.m
+++ Gastrulation/Edge4DTesting/main.m
@@ -24,11 +24,11 @@ labels3d = double(labels3d);
 
 colors = [colorcube(256); jet(256); parula(256); colorcube(256)];
 
-% for i = 1:size(labels3d, 3)
-%     img = labels3d(:, :, i);
-%     %img(img ~= 0) = img(img ~= 0) - min(img(img ~= 0));
-%     imwrite(img, colors, strcat('results/segments/segment_', num2str(i), '.jpg')); 
-% end
+for i = 1:size(labels3d, 3)
+    img = labels3d(:, :, i);
+    %img(img ~= 0) = img(img ~= 0) - min(img(img ~= 0));
+    imwrite(img, colors, strcat('results/mid04/segments/segment_', num2str(i), '.jpg')); 
+end
 
 gettingInfoFromMotifs();
 
