87c8477ce63144963d71751732cf1039d77b1f41
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-05-09 10:10:58 +0200

    embryo mid info

--- Gastrulation/Edge4DTesting/src/main.m
+++ Gastrulation/Edge4DTesting/src/main.m
@@ -1,13 +1,24 @@
-
-% isotropic voxel dimensions
+%% embryo Mid
 alpha = 0.246508; % microns
-w = 665;
+w = 418;
 h = 332;
-d = 211;
-fid = fopen('D:\Pablo\Epithelia3D\Edge4DTesting\data\OR_OliGreen_BP106_early03.raw');
+d = 248;
+fid = fopen('D:\Pablo\Epithelia3D\Edge4DTesting\data\OR_OliGreen_BP106_mid04.raw');
 label_matrix = fread(fid, w * h * d, '*int32');
 labels3d = reshape(label_matrix, [w h d]);
 fclose(fid);
+
+
+% %% EMBRYO EARLY
+% % isotropic voxel dimensions
+% alpha = 0.246508; % microns
+% w = 665;
+% h = 332;
+% d = 211;
+% fid = fopen('D:\Pablo\Epithelia3D\Edge4DTesting\data\OR_OliGreen_BP106_early03.raw');
+% label_matrix = fread(fid, w * h * d, '*int32');
+% labels3d = reshape(label_matrix, [w h d]);
+% fclose(fid);
 
 labels3d = double(labels3d);
 
