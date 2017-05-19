e3a5eebf321a404b96b4d191b2d880f75603f8d4
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-05-19 18:05:44 +0200

    update code with new data

--- Gastrulation/Embryo_Sol/src/organizeMotifsIntoImgSequence.m
+++ Gastrulation/Embryo_Sol/src/organizeMotifsIntoImgSequence.m
@@ -1,7 +1,7 @@
 %%organize motif in sequence.mat
 
-globalMotifsPath={'..\data\SegmentedMotifs\OR\Embryo4\','..\data\SegmentedMotifs\scribGFP\Embryo3\','..\data\SegmentedMotifs\scribGFP\Embryo4\'};
-saveMotifsSeqPath={'..\data\Image_Sequence_Embryo4OR.mat','..\data\Image_Sequence_Embryo3ScribGFP.mat','..\data\Image_Sequence_Embryo4ScribGFP.mat'};
+globalMotifsPath={'data\SegmentedMotifs\OR\Embryo4\','data\SegmentedMotifs\scribGFP\Embryo2\','data\SegmentedMotifs\scribGFP\Embryo3\','data\SegmentedMotifs\scribGFP\Embryo4\'};
+saveMotifsSeqPath={'data\Image_Sequence_Embryo4OR.mat','data\Image_Sequence_Embryo2ScribGFP.mat','data\Image_Sequence_Embryo3ScribGFP.mat','data\Image_Sequence_Embryo4ScribGFP.mat'};
 
 nMotif=25;
 
@@ -18,9 +18,15 @@ for j=1:length(globalMotifsPath)
            Img=bwareaopen(Img,5,4);
            Img2=double(watershed(Img));
            Img2(Img2==1)=0;
+           Img2(Img2==Img2(end,1))=0;
+           Img2(Img2==Img2(end,end))=0;
+           Img2(Img2==Img2(1,end))=0;
            Img_L=bwlabel(Img2);
            Img_Seq{i,1}=Img_L;
            unique(Img_L)
+           if max(unique(Img_L))>4 || max(unique(Img_L))<4
+              pause 
+           end
        end
     end
 
