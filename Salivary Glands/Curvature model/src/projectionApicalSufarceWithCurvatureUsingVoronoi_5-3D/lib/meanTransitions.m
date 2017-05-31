a124b5827757f405e4372b0ba954ba41600b7aec
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-05-31 18:25:19 +0200

    NtransitionsByReductionOfSpace

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/meanTransitions.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/meanTransitions.m
@@ -1,39 +1,38 @@
 
 nImages=20;
 
-listNlossBasApi1=[];
-listNlossBasApi2=[];
-listNwinBasApi1=[];
-listNwinBasApi2=[];
-listNtransitionsBasApi1=[];
-listNtransitionsBasApi2=[];
+listNloss=[];
+listNwin=[];
+listNtransitions=[];
 
 for i=1:nImages
     name2load=['D:\Pedro\Epithelia3D\Salivary Glands\Curvature model\data\Image_' num2str(i) '_Diagram_5\Image_' num2str(i) '_Diagram_5.mat'];
-    load(name2load,'nLossBasApi1','nLossBasApi2','numberOfTransitionsBasApi1','numberOfTransitionsBasApi2','nWinBasApi1','nWinBasApi2')
+    load(name2load,'listTransitionsByCurvature')
     
-    listNlossBasApi1(end+1)=nLossBasApi1;
-    listNlossBasApi2(end+1)=nLossBasApi2;
-    listNwinBasApi1(end+1)=nWinBasApi1;
-    listNwinBasApi2(end+1)=nWinBasApi2;
-    listNtransitionsBasApi1(end+1)=numberOfTransitionsBasApi1;
-    listNtransitionsBasApi2(end+1)=numberOfTransitionsBasApi2;
+    listNloss(:,end+1)=listTransitionsByCurvature.nLoss;
+    listNwin(:,end+1)=listTransitionsByCurvature.nWins;
+    listNtransitions(:,end+1)=listTransitionsByCurvature.nTransitions;
 
 end
 
-meanLossBasApi1=mean(listNlossBasApi1);
-meanLossBasApi2=mean(listNlossBasApi2);
-meanWinBasApi1=mean(listNwinBasApi1);
-meanWinBasApi2=mean(listNwinBasApi2);
-meanTransitionsBasApi1=mean(listNtransitionsBasApi1);
-meanTransitionsBasApi2=mean(listNtransitionsBasApi2);
+meanNLoss=mean(listNloss);
+meanNWin=mean(listNloss);
+meanNTransitions=mean(listNloss);
+stdNLoss=std(listNloss);
+stdNWin=std(listNloss);
+stdNTransitions=std(listNloss);
 
-stdLossBasApi1=std(listNlossBasApi1);
-stdLossBasApi2=std(listNlossBasApi2);
-stdWinBasApi1=std(listNwinBasApi1);
-stdWinBasApi2=std(listNwinBasApi2);
-stdTransitionsBasApi1=std(listNtransitionsBasApi1);
-stdTransitionsBasApi2=std(listNtransitionsBasApi2);
+curvatures=listTransitionsByCurvature.reductionProportion;
 
+    listTransitionsByCurvature=[curvatures,meanNWin,meanNLoss,meanNTransitions,stdWin,stdNLoss,stdNTransitions];
 
-clearvars -except meanLossBasApi1 meanLossBasApi2 meanWinBasApi1 meanWinBasApi2 meanTransitionsBasApi1 meanTransitionsBasApi2 stdLossBasApi1 stdLossBasApi2 stdWinBasApi1 stdWinBasApi2 stdTransitionsBasApi1 stdTransitionsBasApi2
+    listTransitionsByCurvature=array2table(listTransitionsByCurvature);
+    listTransitionsByCurvature.Properties.VariableNames{1} = 'reductionProportion';
+    listTransitionsByCurvature.Properties.VariableNames{2} = 'meanWins';
+    listTransitionsByCurvature.Properties.VariableNames{3} = 'meanLoss';
+    listTransitionsByCurvature.Properties.VariableNames{4} = 'meanTransitions';
+    listTransitionsByCurvature.Properties.VariableNames{5} = 'stdWins';
+    listTransitionsByCurvature.Properties.VariableNames{6} = 'stdLoss';
+    listTransitionsByCurvature.Properties.VariableNames{7} = 'stdTransitions';
+
+clearvars -except listTransitionsByCurvature
--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/pipeline.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/pipeline.m
@@ -6,61 +6,77 @@ addpath('lib')
 
 pathV5data=dir([path3dVoronoi '*_5_V*']);
 
+listOfSurfaceReduction=0.1:0.1:1;
 for i=1:size(pathV5data,1)
     
-    load([path3dVoronoi pathV5data(i).name])
+    listTransitionsByCurvature=[];
+    listSeedsApical={};
+    listLOriginalApical={};
     
+    load([path3dVoronoi pathV5data(i).name])
+
     [H,W]=size(L_original);
     %% Diameter of cell calculation and estimate heigth of cell and 'lumen'
     mask=L_original;
     for j=1:length(border_cells)
         mask(L_original==border_cells(j))=0;
     end
-    majorAxis=regionprops(mask,'majorAxis');
-    majorAxis=cat(1,majorAxis.MajorAxisLength);
-    majorAxis=majorAxis(majorAxis~=0);
-    majorAxisMean=mean(majorAxis);
     
-    heigthVoronoiCell=2*majorAxisMean;
-    lumenRadius2=heigthVoronoiCell/2;
-    lumenRadius1=heigthVoronoiCell;
+    for j=1:length(listOfSurfaceReduction)
+
         
-    %% Curvature implication - recalculate seeds and labelled images
-    curvature2=lumenRadius2/(lumenRadius2+heigthVoronoiCell);
-    curvature1=lumenRadius1/(lumenRadius1+heigthVoronoiCell);
-        
-    seedsBasal=sortrows(seeds_values_before,1);
-    seedsApical2=[seedsBasal(:,1:2),round(seedsBasal(:,3)*curvature2)];
-    seedsApical1=[seedsBasal(:,1:2),round(seedsBasal(:,3)*curvature1)];
 
-    L_originalApical2=generateCylindricalVoronoi(seedsApical2,H,round(W*curvature2));
-    L_originalApical1=generateCylindricalVoronoi(seedsApical1,H,round(W*curvature1));
+        %% Curvature implication - recalculate seeds and labelled images
+        curvature=listOfSurfaceReduction(j);
 
-    %% Representations
-    seeds_values_before=sortrows(seeds_values_before,1);
-    numCells=size(seeds_values_before,1);
+        seedsBasal=sortrows(seeds_values_before,1);
+        seedsApical=[seedsBasal(:,1:2),round(seedsBasal(:,3)*curvature)];
 
-    name2save=pathV5data(i).name;
-    name2save=name2save(1:end-16);
-    
-    representAndSaveFigureWithColourfulCells(L_original,numCells,seeds_values_before(:,2:3),directory2save,name2save,'_basal')
-    representAndSaveFigureWithColourfulCells(L_originalApical2,numCells,seedsApical2(:,2:3),directory2save,name2save, '_apical2')
-    representAndSaveFigureWithColourfulCells(L_originalApical1,numCells,seedsApical1(:,2:3),directory2save,name2save, '_apical1')
- 
-      
-    %% Distance matrix between Centroids of Neighbors using adjacency matrix
-    [distanceMatrixNeighsBasal, adjacencyMatrixBasal,distanceMatrixBasalGeneral]=calculateDistanceMatrixInVoronoi3D(new_seeds_values,L_original,W);
-    [distanceMatrixNeighsApical2, adjacencyMatrixApical2,distanceMatrixApical2General]=calculateDistanceMatrixInVoronoi3D(seedsApical2,L_originalApical2,round(W*curvature2));
-    [distanceMatrixNeighsApical1, adjacencyMatrixApical1,distanceMatrixApical1General]=calculateDistanceMatrixInVoronoi3D(seedsApical1,L_originalApical1,round(W*curvature1));
+        L_originalApical=generateCylindricalVoronoi(seedsApical,H,round(W*curvature));
 
-    %% Testing neighs exchanges
-    [ numberOfTransitionsBasApi2,nWinBasApi2,nLossBasApi2 ] = testingNeighsExchange(L_original,L_originalApical2);
-    [ numberOfTransitionsBasApi1,nWinBasApi1,nLossBasApi1 ] = testingNeighsExchange(L_original,L_originalApical1);
+        %% Representations
+        seeds_values_before=sortrows(seeds_values_before,1);
+        numCells=size(seeds_values_before,1);
 
+        name2save=pathV5data(i).name;
+        name2save=name2save(1:end-16);
+
+        if j==1
+            representAndSaveFigureWithColourfulCells(L_original,numCells,seeds_values_before(:,2:3),directory2save,name2save,'_basal')
+        end
+        representAndSaveFigureWithColourfulCells(L_originalApical,numCells,seedsApical(:,2:3),directory2save,name2save, ['_apical_' num2str(curvature) '_reduction'])
+
+
+%         %% Distance matrix between Centroids of Neighbors using adjacency matrix
+%         [distanceMatrixNeighsBasal, adjacencyMatrixBasal,distanceMatrixBasalGeneral]=calculateDistanceMatrixInVoronoi3D(new_seeds_values,L_original,W);
+%         [distanceMatrixNeighsApical, adjacencyMatrixApical,distanceMatrixApical1General]=calculateDistanceMatrixInVoronoi3D(seedsApical,L_originalApical,round(W*curvature));
+
+        %% Testing neighs exchanges
+        [numberOfTransitionsBasApi,nWinBasApi,nLossBasApi] = testingNeighsExchange(L_original,L_originalApical);
+
+
+        listTransitionsByCurvature(end+1,:)=[curvature,nWinBasApi,nLossBasApi,numberOfTransitionsBasApi];
+        listSeedsApical{end+1,1}=curvature;listSeedsApical{end,2}=seedsApical;
+        listLOriginalApical{end+1,1}=curvature;listLOriginalApical{end,2}=L_originalApical;
+        close all
+
+
+    end
     
-    save([directory2save name2save '\' name2save '.mat'],'heigthVoronoiCell','seedsApical1','seedsApical2','seedsBasal','curvature1','curvature2','lumenRadius1','lumenRadius2','majorAxisMean','L_originalApical1','L_originalApical2','L_original','distanceMatrixNeighsBasal','adjacencyMatrixBasal','distanceMatrixBasalGeneral','distanceMatrixNeighsApical2','adjacencyMatrixApical2','distanceMatrixApical2General','distanceMatrixNeighsApical1','adjacencyMatrixApical1','distanceMatrixApical1General','numberOfTransitionsBasApi1','nWinBasApi1','nLossBasApi1','numberOfTransitionsBasApi2','nWinBasApi2','nLossBasApi2')
+    listTransitionsByCurvature=array2table(listTransitionsByCurvature);
+    listTransitionsByCurvature.Properties.VariableNames{1} = 'reductionProportion';
+    listTransitionsByCurvature.Properties.VariableNames{2} = 'nWins';
+    listTransitionsByCurvature.Properties.VariableNames{3} = 'nLoss';
+    listTransitionsByCurvature.Properties.VariableNames{4} = 'nTransitions';
     
-    close all
+    listSeedsApical=cell2table(listSeedsApical);
+    listSeedsApical.Properties.VariableNames{1} = 'reductionProportion';
+    listSeedsApical.Properties.VariableNames{2} = 'seedsApical';
     
+    listLOriginalApical=cell2table(listLOriginalApical);
+    listLOriginalApical.Properties.VariableNames{1} = 'reductionProportion';
+    listLOriginalApical.Properties.VariableNames{2} = 'L_originalApical';
     
+    save([directory2save name2save '\' name2save '.mat'],'seedsBasal','L_original','listLOriginalApical','listSeedsApical','listTransitionsByCurvature')
+
 end
