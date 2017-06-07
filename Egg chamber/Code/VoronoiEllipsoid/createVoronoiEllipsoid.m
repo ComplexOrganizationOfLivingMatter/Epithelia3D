c0576c93f8dfe6d1ac12c50729f6892eddae79cc
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-07 16:26:58 +0200

    distance is not right

--- Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
+++ Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
@@ -1,4 +1,4 @@
-numberOfPoints = 300;
+resolutionEllipse = 300;
 numberOfCellsInVoronoi = 400;
 xCenter = 0;
 yCenter = 0;
@@ -6,11 +6,16 @@ zCenter = 0;
 xRadius = 1;
 yRadius = 0.6;
 zRadius = 0.6;
+ellipsoidCreated = oblateSpheroid;
+ellipsoidCreated.SemimajorAxis = zRadius;
+ellipsoidCreated.SemiminorAxis = yRadius;
+ellipsoidCreated.InverseFlattening = 1/xRadius;
+
 areaOfEllipsoid = ellipsoidSurfaceArea([xRadius, yRadius, zRadius]);
 
-minDistanceBetweenCentroids = areaOfEllipsoid / (numberOfCellsInVoronoi * 0.95);
+minDistanceBetweenCentroids = sqrt(areaOfEllipsoid) / (numberOfCellsInVoronoi);
 
-[x, y, z] = ellipsoid(xCenter, yCenter, zCenter, xRadius, yRadius, zRadius, numberOfPoints);
+[x, y, z] = ellipsoid(xCenter, yCenter, zCenter, xRadius, yRadius, zRadius, resolutionEllipse);
 
 numberOfCentroids = 1;
 totalNumberOfPossibleCentroids = size(x, 1) * size(x, 1);
@@ -24,6 +29,7 @@ while numberOfCentroids < numberOfCellsInVoronoi
     indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
     randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
     
+    distance(randomCentroid, centroids(1), ellipsoidCreated);
     minDistanceToExistingCentroids = min(pdist2(randomCentroid, centroids));
     if minDistanceToExistingCentroids > minDistanceBetweenCentroids
         numberOfCentroids = numberOfCentroids + 1;
