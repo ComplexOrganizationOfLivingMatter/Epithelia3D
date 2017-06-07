9a8ffc6dceb869a27d87d8a8adbe228cc50adba8
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-07 13:08:56 +0200

    init creating voronoi as ellipsoid

---
+++ Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
@@ -0,0 +1,11 @@
+numberOfPoints = 300;
+numberOfCellsInVoronoi = 400;
+[x, y, z] = ellipsoid(0,0,0,5.9,3.25,3.25, numberOfPoints);
+
+centroids = zeros(numberOfCellsInVoronoi, 3);
+numberOfCentroids = 0;
+
+while numberOfCentroids < numberOfCellsInVoronoi
+    
+    
+end
