c5a9baa7fb35d3dac2714279c05111b231be8fbb
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-07 15:52:39 +0200

    random centroids with a given distance between them

--- Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
+++ Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
@@ -1,11 +1,27 @@
 numberOfPoints = 300;
 numberOfCellsInVoronoi = 400;
-[x, y, z] = ellipsoid(0,0,0,5.9,3.25,3.25, numberOfPoints);
+minDistanceBetweenCentroids = 0.5;
+xCenter = 0;
+yCenter = 0;
+zCenter = 0;
+ellipsoidSurfaceArea(
+[x, y, z] = ellipsoid(xCenter, yCenter, zCenter,5.9,3.25,3.25, numberOfPoints);
 
-centroids = zeros(numberOfCellsInVoronoi, 3);
-numberOfCentroids = 0;
+numberOfCentroids = 1;
+totalNumberOfPossibleCentroids = size(x, 1) * size(x, 1);
 
+%First Centroid
+indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
+randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
+centroids = randomCentroid;
+%The rest centroids
 while numberOfCentroids < numberOfCellsInVoronoi
+    indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
+    randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
     
-    
+    minDistanceToExistingCentroids = min(pdist2(randomCentroid, centroids))
+    if minDistanceToExistingCentroids > minDistanceBetweenCentroids
+        numberOfCentroids = numberOfCentroids + 1;
+        centroids(numberOfCentroids, :) = randomCentroid;
+    end
 end
