fb4dba061762f82246edae301ebfda52f6b195b3
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-07 16:03:35 +0200

    centroids are in a ellpsoid surface

--- Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
+++ Egg chamber/Code/VoronoiEllipsoid/createVoronoiEllipsoid.m
@@ -1,11 +1,16 @@
 numberOfPoints = 300;
 numberOfCellsInVoronoi = 400;
-minDistanceBetweenCentroids = 0.5;
 xCenter = 0;
 yCenter = 0;
 zCenter = 0;
-ellipsoidSurfaceArea(
-[x, y, z] = ellipsoid(xCenter, yCenter, zCenter,5.9,3.25,3.25, numberOfPoints);
+xRadius = 1;
+yRadius = 0.6;
+zRadius = 0.6;
+areaOfEllipsoid = ellipsoidSurfaceArea([xRadius, yRadius, zRadius]);
+
+minDistanceBetweenCentroids = areaOfEllipsoid / (numberOfCellsInVoronoi * 0.95);
+
+[x, y, z] = ellipsoid(xCenter, yCenter, zCenter, xRadius, yRadius, zRadius, numberOfPoints);
 
 numberOfCentroids = 1;
 totalNumberOfPossibleCentroids = size(x, 1) * size(x, 1);
@@ -19,9 +24,10 @@ while numberOfCentroids < numberOfCellsInVoronoi
     indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
     randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
     
-    minDistanceToExistingCentroids = min(pdist2(randomCentroid, centroids))
+    minDistanceToExistingCentroids = min(pdist2(randomCentroid, centroids));
     if minDistanceToExistingCentroids > minDistanceBetweenCentroids
         numberOfCentroids = numberOfCentroids + 1;
         centroids(numberOfCentroids, :) = randomCentroid;
     end
 end
+scatter3(centroids(:, 1), centroids(:, 2), centroids(:, 3))
