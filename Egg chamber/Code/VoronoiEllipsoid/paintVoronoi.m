5c72a9a4772b9283f1dcb429e41d361614de9805
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-09 10:26:25 +0200

    adding a few points on the inside of the ellipsoid to improve it. Doesn't work

--- Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
@@ -4,7 +4,30 @@ function [ ] = paintVoronoi(x, y, z)
     figure('Color','w') 
     plot3(x,y,z,'Marker','.','MarkerEdgeColor','r','MarkerSize',10, 'LineStyle', 'none')
     X=[x y z];
-    [V,C]=voronoin(X);
+    X(end+1, :) = [0 0 0];
+    X(end+1, :) = [-0.3 0 0];
+    X(end+1, :) = [-0.5 0 0];
+    X(end+1, :) = [0.3 0 0];
+    X(end+1, :) = [0.5 0 0];
+    X(end+1, :) = [-0.0 0.5 0];
+    X(end+1, :) = [-0.0 0.3 0];
+    X(end+1, :) = [0.0 -0.3 0];
+    X(end+1, :) = [0.0 -0.5 0];
+    X(end+1, :) = [-0.0 0.0 -0.5];
+    X(end+1, :) = [-0.0 0.0 -0.3];
+    X(end+1, :) = [0.0 -0.0 0.3];
+    X(end+1, :) = [0.0 -0.0 0.5];
+    [V, C] = voronoin(X);
+    T = delaunayn(X);
+    tetramesh(T,X, 'FaceAlpha', 1);
+%     paintRegion(x, y, z, z < 0 )
+%     paintRegion(x, y, z,  x < 0 & z >= 0)
+%     paintRegion(x, y, z,  x >= 0 & z < 0 )
+%     paintRegion(x, y, z,  x >= 0 & z >= 0)
+    zlabel('z');
+    ylabel('y');
+    xlabel('x');
+    [V,C]=voronoin(X, {'Q1'});
     V;
     for k=1:length(C)
         disp(C{k})
