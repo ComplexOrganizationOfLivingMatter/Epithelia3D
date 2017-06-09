86615f28a4271401ee9af3746be9056a36805b5b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-09 17:23:58 +0200

    trying but not getting all the things right

--- Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
@@ -30,10 +30,10 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
     %ylabel('y');
     %xlabel('x');
     
-    %[xEllipsoid, yEllipsoid, zEllipsoid] = ellipsoid(0, 0, 0, xRadius, yRadius, zRadius, 1000);
-    %pointsInEllipsoid = [xEllipsoid(:), yEllipsoid(:), zEllipsoid(:)];
+    [xEllipsoid, yEllipsoid, zEllipsoid] = ellipsoid(0, 0, 0, xRadius, yRadius, zRadius, 300);
+    pointsInEllipsoid = [xEllipsoid(:), yEllipsoid(:), zEllipsoid(:)];
     figure;
-    [V,C]=voronoin(X, {'Q8'});
+    [V,C]=voronoin(X);
     clmap = colorcube();
     ncl = size(clmap,1);
     verticesPerCell = cell(length(Y), 1);
@@ -46,8 +46,16 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
         verticesPerCellBefore(k, 1) = {VertCell};
         
         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
-        VertCell (indicesOutsideEllipsoid < 0.9 | indicesOutsideEllipsoid > 1.1, :) = [];
-        %VertCell = replaceVerticesOutsideForEllipsoidPoints(VertCell , pointsInEllipsoid);
+        VertCell (indicesOutsideEllipsoid < 0.7 | indicesOutsideEllipsoid > 1.3, :) = [];
+        indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
+        slightlyBadVertices = indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05;
+        if any(slightlyBadVertices) 
+            numBadInd = find(slightlyBadVertices);
+            %VertCell(indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05, :) = replaceVerticesOutsideForEllipsoidPoints(VertCell(indicesOutsideEllipsoid < 0.9 | indicesOutsideEllipsoid > 1.1, :) , pointsInEllipsoid);
+            for numBad = 1:length(numBadInd)
+                VertCell(numBadInd(numBad), :) = VertCell(numBadInd(numBad), :) / indicesOutsideEllipsoid(numBadInd(numBad));
+            end
+        end
         VertCell = [VertCell; 0, 0, 0];
         %VertCell = VertCell(ismember(VertCell, Y, 'rows') == 0, :);
         KVert = convhulln(VertCell);
@@ -59,7 +67,7 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
     %plot3(x,y,z,'Marker','.','MarkerEdgeColor','r','MarkerSize',10, 'LineStyle', 'none')
     
     %calculatePolygonDistributionFromVerticesInEllipsoid(X, verticesPerCellBefore )
-    calculatePolygonDistributionFromVerticesInEllipsoid(XInitial, verticesPerCell )
+    %calculatePolygonDistributionFromVerticesInEllipsoid(XInitial, verticesPerCell )
     
 end
 
