b6a2af0dec9bc0010c063441328c65b1f85726b0
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-09 11:34:46 +0200

    we've paint it for a sphere

--- Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
@@ -4,40 +4,57 @@ function [ ] = paintVoronoi(x, y, z)
     figure('Color','w') 
     plot3(x,y,z,'Marker','.','MarkerEdgeColor','r','MarkerSize',10, 'LineStyle', 'none')
     X=[x y z];
-    X(end+1, :) = [0 0 0];
-    X(end+1, :) = [-0.3 0 0];
-    X(end+1, :) = [-0.5 0 0];
-    X(end+1, :) = [0.3 0 0];
-    X(end+1, :) = [0.5 0 0];
-    X(end+1, :) = [-0.0 0.5 0];
-    X(end+1, :) = [-0.0 0.3 0];
-    X(end+1, :) = [0.0 -0.3 0];
-    X(end+1, :) = [0.0 -0.5 0];
-    X(end+1, :) = [-0.0 0.0 -0.5];
-    X(end+1, :) = [-0.0 0.0 -0.3];
-    X(end+1, :) = [0.0 -0.0 0.3];
-    X(end+1, :) = [0.0 -0.0 0.5];
-    [V, C] = voronoin(X);
-    T = delaunayn(X);
-    tetramesh(T,X, 'FaceAlpha', 1);
+    Y = X * 0.8;
+    X = [X; Y];
+%     X(end+1, :) = [0 0 0];
+%     X(end+1, :) = [-0.3 0 0];
+%     X(end+1, :) = [-0.5 0 0];
+%     X(end+1, :) = [0.3 0 0];
+%     X(end+1, :) = [0.5 0 0];
+%     X(end+1, :) = [-0.0 0.5 0];
+%     X(end+1, :) = [-0.0 0.3 0];
+%     X(end+1, :) = [0.0 -0.3 0];
+%     X(end+1, :) = [0.0 -0.5 0];
+%     X(end+1, :) = [-0.0 0.0 -0.5];
+%     X(end+1, :) = [-0.0 0.0 -0.3];
+%     X(end+1, :) = [0.0 -0.0 0.3];
+%     X(end+1, :) = [0.0 -0.0 0.5];
+    %[V, C] = voronoin(X);
+    %T = delaunayn(X);
+    %tetramesh(T,X, 'FaceAlpha', 1);
 %     paintRegion(x, y, z, z < 0 )
 %     paintRegion(x, y, z,  x < 0 & z >= 0)
 %     paintRegion(x, y, z,  x >= 0 & z < 0 )
 %     paintRegion(x, y, z,  x >= 0 & z >= 0)
-    zlabel('z');
-    ylabel('y');
-    xlabel('x');
-    [V,C]=voronoin(X, {'Q1'});
+    %zlabel('z');
+    %ylabel('y');
+    %xlabel('x');
+    figure;
+    [V,C]=voronoin(X);
     V;
     for k=1:length(C)
         disp(C{k})
     end
+    clmap = colorcube();
+    ncl = size(clmap,1);
     for k=1:length(C)
-        if all(C{k}~=1)
-            VertCell = V(C{k},:);
+        %if all(C{k}~=1)
+        
+            cl = clmap(mod(k,ncl)+1,:);
+            sides = C{k};
+            VertCell = V(sides(sides~=1),:);
+            VertCell = [VertCell; 0, 0, 0];
+            %VertCell = VertCell(ismember(VertCell, Y, 'rows') == 0, :);
             KVert = convhulln(VertCell);
-            patch('Vertices',VertCell,'Faces',KVert,'FaceColor','g','FaceAlpha',0.5)
-        end
+            %KVert = KVert(sum(KVert ~= (size(VertCell, 1)), 2) == 3, :);
+            %fill3(VertCell(:, 1), VertCell(:, 2), VertCell(:, 3), 'r')
+            patch('Vertices',VertCell,'Faces', KVert,'FaceColor', cl ,'FaceAlpha',1, 'EdgeColor', 'none')
+            
+            hold on;
+            if k == 220
+                disp('0'); 
+            end
+        %end
     end
 end
 
