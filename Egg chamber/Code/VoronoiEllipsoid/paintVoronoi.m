be25079710d936103be95146feddde8de418cf2d
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-12 12:28:32 +0200

    returning verticesPerCell and removing shit

--- Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
@@ -1,4 +1,4 @@
-function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
+function [ verticesPerCell ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
 %PAINTVORONOI Summary of this function goes here
 %   Detailed explanation goes here
     %figure('Color','w') 
@@ -6,34 +6,14 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
     XInitial=[x y z];
     Y = XInitial * 0.9;
     X = [XInitial*1.1; Y];
-%     X(end+1, :) = [0 0 0];
-%     X(end+1, :) = [-0.3 0 0];
-%     X(end+1, :) = [-0.5 0 0];
-%     X(end+1, :) = [0.3 0 0];
-%     X(end+1, :) = [0.5 0 0];
-%     X(end+1, :) = [-0.0 0.5 0];
-%     X(end+1, :) = [-0.0 0.3 0];
-%     X(end+1, :) = [0.0 -0.3 0];
-%     X(end+1, :) = [0.0 -0.5 0];
-%     X(end+1, :) = [-0.0 0.0 -0.5];
-%     X(end+1, :) = [-0.0 0.0 -0.3];
-%     X(end+1, :) = [0.0 -0.0 0.3];
-%     X(end+1, :) = [0.0 -0.0 0.5];
-    %[V, C] = voronoin(X);
-    %T = delaunayn(X);
-    %tetramesh(T,X, 'FaceAlpha', 1);
-%     paintRegion(x, y, z, z < 0 )
-%     paintRegion(x, y, z,  x < 0 & z >= 0)
-%     paintRegion(x, y, z,  x >= 0 & z < 0 )
-%     paintRegion(x, y, z,  x >= 0 & z >= 0)
-    %zlabel('z');
-    %ylabel('y');
-    %xlabel('x');
     
     [xEllipsoid, yEllipsoid, zEllipsoid] = ellipsoid(0, 0, 0, xRadius, yRadius, zRadius, 300);
     pointsInEllipsoid = [xEllipsoid(:), yEllipsoid(:), zEllipsoid(:)];
+    
     figure;
     [V,C]=voronoin(X);
+    %T = delaunayn([XInitial; [0 0 0]]);
+    %tetramesh(T,X);
     clmap = colorcube();
     ncl = size(clmap,1);
     verticesPerCell = cell(length(Y), 1);
@@ -46,14 +26,14 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
         verticesPerCellBefore(k, 1) = {VertCell};
         
         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
-        VertCell (indicesOutsideEllipsoid < 0.7 | indicesOutsideEllipsoid > 1.3, :) = [];
+        VertCell (indicesOutsideEllipsoid < 0.85 | indicesOutsideEllipsoid > 1.15, :) = [];
         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
         slightlyBadVertices = indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05;
         if any(slightlyBadVertices) 
             numBadInd = find(slightlyBadVertices);
             %VertCell(indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05, :) = replaceVerticesOutsideForEllipsoidPoints(VertCell(indicesOutsideEllipsoid < 0.9 | indicesOutsideEllipsoid > 1.1, :) , pointsInEllipsoid);
             for numBad = 1:length(numBadInd)
-                VertCell(numBadInd(numBad), :) = VertCell(numBadInd(numBad), :) / indicesOutsideEllipsoid(numBadInd(numBad));
+                VertCell(numBadInd(numBad), :) = sqrt(VertCell(numBadInd(numBad), :).^2 / indicesOutsideEllipsoid(numBadInd(numBad))) .* sign(VertCell(numBadInd(numBad), :));
             end
         end
         VertCell = [VertCell; 0, 0, 0];
@@ -63,11 +43,6 @@ function [ ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
         patch('Vertices',VertCell,'Faces', KVert,'FaceColor', cl ,'FaceAlpha', 1, 'EdgeColor', 'none')
         verticesPerCell(k, 1) = {VertCell(1:end-1, :)};
         hold on;
-    end
-    %plot3(x,y,z,'Marker','.','MarkerEdgeColor','r','MarkerSize',10, 'LineStyle', 'none')
-    
-    %calculatePolygonDistributionFromVerticesInEllipsoid(X, verticesPerCellBefore )
-    %calculatePolygonDistributionFromVerticesInEllipsoid(XInitial, verticesPerCell )
-    
+    end    
 end
 
