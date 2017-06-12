c3ee79e5e88a081820b206610ffd5b0d86af1da1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-12 11:40:45 +0200

    refactoring the code

---
+++ Egg chamber/Code/VoronoiEllipsoid/voronoiOnEllipsoidSurface.m
@@ -0,0 +1,9 @@
+function [ ] = voronoiOnEllipsoidSurface( centerOfEllipsoid )
+%VORONOIONELLIPSOIDSURFACE Summary of this function goes here
+%   Detailed explanation goes here
+    xCenter = centerOfEllipsoid(1);
+    yCenter = centerOfEllipsoid(2);
+    zCenter = centerOfEllipsoid(3);
+
+end
+
---
+++ Egg chamber/Code/main.m
@@ -0,0 +1,1 @@
+voronoiOnEllipsoidSurface( centerOfEllipsoid )
