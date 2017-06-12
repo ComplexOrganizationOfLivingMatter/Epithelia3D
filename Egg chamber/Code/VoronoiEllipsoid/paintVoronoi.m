dcec94baacebf4170dcae01e8776487e961fc25a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-12 18:13:32 +0200

    formula for extrapolating the points into the ellipsoid surface

--- Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintVoronoi.m
@@ -7,8 +7,8 @@ function [ verticesPerCell ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
     Y = XInitial * 0.9;
     X = [XInitial*1.1; Y];
     
-    [xEllipsoid, yEllipsoid, zEllipsoid] = ellipsoid(0, 0, 0, xRadius, yRadius, zRadius, 300);
-    pointsInEllipsoid = [xEllipsoid(:), yEllipsoid(:), zEllipsoid(:)];
+%     [xEllipsoid, yEllipsoid, zEllipsoid] = ellipsoid(0, 0, 0, xRadius, yRadius, zRadius, 1000);
+%     pointsInEllipsoid = [xEllipsoid(:), yEllipsoid(:), zEllipsoid(:)];
     
     figure('Visible', 'off');
     [V,C]=voronoin(X);
@@ -24,19 +24,30 @@ function [ verticesPerCell ] = paintVoronoi(x, y, z, xRadius, yRadius, zRadius)
         sides = C{k};
         VertCell = V(sides(sides~=1),:);
         verticesPerCellBefore(k, 1) = {VertCell};
-        
         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
         VertCell (indicesOutsideEllipsoid < 0.8 | indicesOutsideEllipsoid > 1.2, :) = [];
+        
         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
-        slightlyBadVertices = indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05;
-        if any(slightlyBadVertices) 
-            numBadInd = find(slightlyBadVertices);
-            %VertCell(indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05, :) = replaceVerticesOutsideForEllipsoidPoints(VertCell(indicesOutsideEllipsoid < 0.9 | indicesOutsideEllipsoid > 1.1, :) , pointsInEllipsoid);
-            for numBad = 1:length(numBadInd)
-                VertCell(numBadInd(numBad), :) = sqrt(VertCell(numBadInd(numBad), :).^2 / indicesOutsideEllipsoid(numBadInd(numBad))) .* sign(VertCell(numBadInd(numBad), :));
-            end
-        end
-        VertCell = [VertCell; 0, 0, 0];
+
+        upSide = xRadius^2 * yRadius^2 * zRadius^2;
+        format long
+        downSide = (yRadius^2 * zRadius^2 * VertCell(indicesOutsideEllipsoid > 1, 1).^2) + (xRadius^2 * zRadius^2 * VertCell(indicesOutsideEllipsoid > 1, 2).^2) + (yRadius^2 * xRadius^2 * VertCell(indicesOutsideEllipsoid > 1, 3).^2);
+        conversorFactor = sqrt(upSide./downSide);
+        VertCell(indicesOutsideEllipsoid > 1, :) = arrayfun(@(x, y) x * y, VertCell(indicesOutsideEllipsoid > 1, :), repmat(conversorFactor, 1, 3));
+        %         indicesOutsideEllipsoid = (VertCell(:, 1).^2 / xRadius^2) + (VertCell(:, 2).^ 2 / yRadius^2) + (VertCell(:, 3).^2 / zRadius^2);
+%         slightlyBadVertices = indicesOutsideEllipsoid < 0.99 | indicesOutsideEllipsoid > 1.01;
+%         if any(slightlyBadVertices) 
+%             numBadInd = find(slightlyBadVertices);
+%             %VertCell(indicesOutsideEllipsoid < 0.95 | indicesOutsideEllipsoid > 1.05, :) = replaceVerticesOutsideForEllipsoidPoints(VertCell(indicesOutsideEllipsoid < 0.9 | indicesOutsideEllipsoid > 1.1, :) , pointsInEllipsoid);
+%             for numBad = 1:length(numBadInd)
+%                 %distMat = mean(pdist2([0 0 0; VertCell(numBadInd(numBad), :)], pointsInEllipsoid), 1);
+%                 %[~, index] = min(distMat);
+%                 %VertCell(numBadInd(numBad), :) = pointsInEllipsoid(index, :);
+%                 %VertCell(numBadInd(numBad), :) = sqrt(VertCell(numBadInd(numBad), :).^2 / indicesOutsideEllipsoid(numBadInd(numBad))) .* sign(VertCell(numBadInd(numBad), :));
+%             end
+%         end
+        
+        VertCell = [VertCell; XInitial(k, :)];
         %VertCell = VertCell(ismember(VertCell, Y, 'rows') == 0, :);
         KVert = convhulln(VertCell);
         %KVert = KVert(sum(KVert ~= (size(VertCell, 1)), 2) == 3, :);
