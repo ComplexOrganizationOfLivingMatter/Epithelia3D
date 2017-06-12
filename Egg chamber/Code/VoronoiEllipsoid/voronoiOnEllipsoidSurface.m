b1b8b6e7032a293438a5bb46089d304a5830e779
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-12 11:57:47 +0200

    adding the code and comments

--- Egg chamber/Code/VoronoiEllipsoid/voronoiOnEllipsoidSurface.m
+++ Egg chamber/Code/VoronoiEllipsoid/voronoiOnEllipsoidSurface.m
@@ -1,9 +1,61 @@
-function [ ] = voronoiOnEllipsoidSurface( centerOfEllipsoid )
+function [ ] = voronoiOnEllipsoidSurface( centerOfEllipsoid, ellipsoidDimensions, maxNumberOfCellsInVoronoi, apicalReduction, minDistanceBetweenCentroids )
 %VORONOIONELLIPSOIDSURFACE Summary of this function goes here
 %   Detailed explanation goes here
-    xCenter = centerOfEllipsoid(1);
-    yCenter = centerOfEllipsoid(2);
-    zCenter = centerOfEllipsoid(3);
 
+    %Init all the info for creating the voronoi
+    ellipsoidInfo.xCenter = centerOfEllipsoid(1);
+    ellipsoidInfo.yCenter = centerOfEllipsoid(2);
+    ellipsoidInfo.zCenter = centerOfEllipsoid(3);
+    
+    ellipsoidInfo.xRadius = ellipsoidDimensions(1);
+    ellipsoidInfo.yRadius = ellipsoidDimensions(2);
+    ellipsoidInfo.zRadius = ellipsoidDimensions(3);
+    
+    ellipsoidInfo.maxNumberOfCellsInVoronoi = maxNumberOfCellsInVoronoi;
+    ellipsoidInfo.apicalReduction = apicalReduction;
+    ellipsoidInfo.minDistanceBetweenCentroids = minDistanceBetweenCentroids;
+    
+    
+    ellipsoidInfo.areaOfEllipsoid = ellipsoidSurfaceArea([ellipsoidInfo.xRadius, ellipsoidInfo.yRadius, ellipsoidInfo.zRadius]);
+
+    %(resolutionEllipse + 1) * (resolutionEllipse + 1) number of points
+    %generated at the surface of the ellipsoid
+    ellipsoidInfo.resolutionEllipse = 300; %300 seems to be a good number
+    [x, y, z] = ellipsoid(ellipsoidInfo.xCenter, ellipsoidInfo.yCenter, ellipsoidInfo.zCenter, ellipsoidInfo.xRadius, ellipsoidInfo.yRadius, ellipsoidInfo.zRadius, ellipsoidInfo.resolutionEllipse);
+    
+    totalNumberOfPossibleCentroids = size(x, 1) * size(x, 1);
+    
+    %The actual number of centroids that will be increased per iteration
+    numberOfCentroids = 1;
+    %First Centroid created
+    indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
+    randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
+    finalCentroids = randomCentroid;
+    %The remaining centroids
+    %We'll add a maxNumberOfCellsInVoronoi or if the distance doesn't allow
+    %us the number will be decreased
+    while numberOfCentroids < maxNumberOfCellsInVoronoi && totalNumberOfPossibleCentroids ~= 0
+        %Generate a random index
+        indexRandomCentroid = randi(totalNumberOfPossibleCentroids);
+        %Get the centroid
+        randomCentroid = [x(indexRandomCentroid), y(indexRandomCentroid), z(indexRandomCentroid)];
+        %Remove the centroid because we don't want to have the same
+        %centroid twice
+        x(indexRandomCentroid) = [];
+        y(indexRandomCentroid) = [];
+        z(indexRandomCentroid) = [];
+        totalNumberOfPossibleCentroids = totalNumberOfPossibleCentroids - 1;
+
+        %Check if the minimum distance is satisfaed
+        minDistanceToExistingCentroids = min(pdist2(randomCentroid, finalCentroids));
+        %If it is farther enough it can be added
+        if minDistanceToExistingCentroids > minDistanceBetweenCentroids
+            numberOfCentroids = numberOfCentroids + 1;
+            finalCentroids(numberOfCentroids, :) = randomCentroid;
+        end
+    end
+    
+    %
+    save('data/ellipsoid_x1_y06_z06_apicalReduction02', 'ellipsoidInfo', 'centroids', 'minDistanceBetweenCentroids');
 end
 
--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -1,1 +1,3 @@
-voronoiOnEllipsoidSurface( centerOfEllipsoid )
+
+
+voronoiOnEllipsoidSurface( [0 0 0], [1 0.6 0.6], 600, 0.2, 0.1 );
