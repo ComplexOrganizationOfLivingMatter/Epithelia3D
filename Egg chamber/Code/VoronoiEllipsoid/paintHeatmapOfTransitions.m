468287856f087a890a741c248398befecc390810
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-14 10:45:02 +0200

    corrected and inverted colors

--- Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
@@ -2,10 +2,11 @@ function [ ] = paintHeatmapOfTransitions( ellipsoidInfo, initialNeighbourhood )
 %PAINTHEATMAPOFTRANSITIONS Summary of this function goes here
 %   Detailed explanation goes here
 
-    transitionsPerCell = cellfun(@(x, y) sum(xor(x, y)), ellipsoidInfo.neighbourhood, initialNeighbourhood);
+    transitionsPerCell = cellfun(@(x, y) size(setxor(x, y), 1), ellipsoidInfo.neighbourhood, initialNeighbourhood);
     
     figure('Visible', 'off');
-    clmap = hot(20);
+    clmap = hot(10);
+    clmap = clmap(size(clmap, 1):-1:1, :);
     ncl = size(clmap,1);
 
     for cellIndex = 1:size(ellipsoidInfo.verticesPerCell, 1)
