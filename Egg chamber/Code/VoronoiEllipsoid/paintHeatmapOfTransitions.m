91548340cee3df3f953faa6ceb31b19eff8a0113
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-14 12:12:13 +0200

    painting perimeter

--- Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
@@ -15,6 +15,25 @@ function [ ] = paintHeatmapOfTransitions( ellipsoidInfo, initialNeighbourhood )
         KVert = convhulln([VertCell; ellipsoidInfo.finalCentroids(cellIndex, :)]);
         patch('Vertices',[VertCell; ellipsoidInfo.finalCentroids(cellIndex, :)],'Faces', KVert,'FaceColor', cl ,'FaceAlpha', 1, 'EdgeColor', 'none')
         hold on;
+        %painting perimeter of cell
+        indicesOfVertices = find(cellfun(@(x) ismember(cellIndex, x), ellipsoidInfo.verticesConnectCells));
+        actualVerticesConnectCells = [ellipsoidInfo.verticesConnectCells{indicesOfVertices}]';
+        perimeterEdges = ellipsoidInfo.vertices(indicesOfVertices(1), :);
+        previousVerticesConnectCells = actualVerticesConnectCells(1, :);
+        indicesOfVertices(1) = [];
+        actualVerticesConnectCells(1, :) = [];
+        
+        while size(actualVerticesConnectCells, 1) > 0
+            nextEdge = find(cellfun(@(x) sum(ismember(previousVerticesConnectCells, x)) == 2, mat2cell(actualVerticesConnectCells, ones(size(actualVerticesConnectCells, 1), 1), 3)));
+            perimeterEdges(end+1, :) = ellipsoidInfo.vertices(indicesOfVertices(nextEdge(1)), :);
+            previousVerticesConnectCells = actualVerticesConnectCells(nextEdge(1), :);
+            actualVerticesConnectCells(nextEdge(1), :) = [];
+            indicesOfVertices(nextEdge(1)) = [];
+        end
+        plot3([perimeterEdges(end, 1); perimeterEdges(1, 1)], [perimeterEdges(end, 2); perimeterEdges(1, 2)], [perimeterEdges(end, 3); perimeterEdges(1, 3)], 'k')
+        for i = 1:size(perimeterEdges, 1) - 1
+            plot3([perimeterEdges(i, 1); perimeterEdges(i+1, 1)], [perimeterEdges(i, 2); perimeterEdges(i+1, 2)], [perimeterEdges(i, 3); perimeterEdges(i+1, 3)], 'k')
+        end
     end
     axis equal
 end
