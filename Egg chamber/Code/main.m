31a7e54b90dca37d2659b6f459edf9f7b71ad009
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-06-16 15:34:52 +0200

    bugfix

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -5,6 +5,6 @@ close all
 mkdir('..\resultsVoronoiEllipsoid');
 parfor radiusY = 0:30
     for radiusZ = 0:30
-        voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 + (radiusY + radiusZ - radiusX)/10);
+        voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 + (radiusY + radiusZ - 10)/10);
     end
 end
