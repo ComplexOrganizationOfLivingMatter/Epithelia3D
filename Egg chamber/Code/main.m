ae955893edb505485c9f03768a734505cd09d7ee
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-16 15:25:19 +0200

    several tests

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -3,6 +3,8 @@ addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 close all
 mkdir('..\resultsVoronoiEllipsoid');
-for radius = 5:9
-    voronoiOnEllipsoidSurface( [0 0 0], [10 radius radius], 400, 1 );
+parfor radiusY = 0:10
+    for radiusZ = 0:10
+        voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 );
+    end
 end
