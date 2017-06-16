2c831810fc4deee4fa4de70c5dc919451d7fe559
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-06-16 15:41:32 +0200

    not checking the sphere

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -4,7 +4,7 @@ addpath(genpath('lib'));
 close all
 mkdir('..\resultsVoronoiEllipsoid');
 parfor radiusY = 0:30
-    for radiusZ = 0:30
+    for radiusZ = 1:30
         voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 + (radiusY + radiusZ - 10)/10);
     end
 end
