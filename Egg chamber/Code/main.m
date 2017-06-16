05f8be72db1d488e1b9dfefa487bda5d4dd7155e
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-06-16 15:31:01 +0200

    good distance between points

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -3,8 +3,8 @@ addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 close all
 mkdir('..\resultsVoronoiEllipsoid');
-parfor radiusY = 0:10
-    for radiusZ = 0:10
-        voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 );
+parfor radiusY = 0:30
+    for radiusZ = 0:30
+        voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1 + (radiusY + radiusZ - radiusX)/10);
     end
 end
