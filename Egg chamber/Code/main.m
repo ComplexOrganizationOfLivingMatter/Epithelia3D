7ad57f61e56c75fe16ff37813f05f8794133adff
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-16 10:20:37 +0200

    mkdir of output directory

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -2,6 +2,7 @@
 addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 close all
+mkdir('..\resultsVoronoiEllipsoid');
 for radius = 5:9
     voronoiOnEllipsoidSurface( [0 0 0], [10 radius radius], 400, 1 );
 end
