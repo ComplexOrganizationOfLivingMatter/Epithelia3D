b521df14319fdaf687978062defd6b35c907343c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-19 12:07:33 +0200

    changing to do it parallel

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -3,12 +3,17 @@ addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 close all
 mkdir('..\resultsVoronoiEllipsoid');
-transitionByRadius = cell(radiusY, radiusZ);
-for radiusY = 0:30
-    transitionRadiusZ = cell(radiusZ, 1);
-    parfor radiusZ = 0:30
-        a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 2 + ((radiusY + radiusZ - 10)/10));
-        transitionByRadius(radiusY, radiusZ) = a;
+maxRadiusY = 20;
+maxRadiusZ = 20;
+transitionByRadius = cell(maxRadiusY, maxRadiusZ);
+parfor radiusY = 0:maxRadiusY
+    for radiusZ = 0:maxRadiusZ
+        a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY 10+radiusZ], 500, 1);
+        if isempty(a)
+            transitionByRadius(radiusY, radiusZ) = a;
+        else
+            transitionByRadius(radiusY, radiusZ) = vertcat(a{:});
+        end
     end
-    transitionByRadius(radiusY, 1:30) = transitionByRadius;
 end
+transitionByRadius
