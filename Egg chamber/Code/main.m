6e0502c5c5f8f6a8555ec2b08c97acfdfeda1082
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-19 16:33:09 +0200

    writing table

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -3,10 +3,10 @@ addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 close all
 mkdir('..\resultsVoronoiEllipsoid');
-maxRadiusY = 20;
+maxRadiusY = 5;
 maxRadiusZ = 20;
 transitionByRadius = cell(maxRadiusY+1, maxRadiusZ+1);
-parfor radiusY = 1:maxRadiusY+1
+for radiusY = 1:maxRadiusY+1
     for radiusZ = 1:maxRadiusZ+1
         a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY-1 10+radiusZ-1], 500, 1);
         if isempty(a)
@@ -16,4 +16,4 @@ parfor radiusY = 1:maxRadiusY+1
         end
     end
 end
-transitionByRadius
+writetable(vertcat(transitionByRadius{:}), strcat('..\resultsVoronoiEllipsoid\transitionsInfo_', date, '.csv'), 'Delimiter', ';')
