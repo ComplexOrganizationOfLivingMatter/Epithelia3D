a38b02be35d430afa19ab556eb6febb745e41529
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-19 12:31:02 +0200

    proper assignation of errors

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -10,9 +10,9 @@ parfor radiusY = 1:maxRadiusY+1
     for radiusZ = 1:maxRadiusZ+1
         a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY-1 10+radiusZ-1], 500, 1);
         if isempty(a)
-            transitionByRadius(radiusY, radiusZ) = a;
+            transitionByRadius(radiusY, radiusZ) = {[]};
         else
-            transitionByRadius(radiusY, radiusZ) = vertcat(a{:});
+            transitionByRadius(radiusY, radiusZ) = {vertcat(a{:})};
         end
     end
 end
