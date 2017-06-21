75edfd231e82816e63c3763e38e027f7f4eb8000
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-06-21 15:04:58 +0200

    close all inside for

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -1,14 +1,15 @@
 
 addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
-close all
-for numRandomization = 1:20
+
+for numRandomization = 1:10
     outputDir = strcat('..\resultsVoronoiEllipsoid\random_', num2str(numRandomization));
     mkdir(outputDir);
     maxRadiusY = 20;
     maxRadiusZ = 20;
     transitionByRadius = cell(maxRadiusY+1, maxRadiusZ+1);
     for radiusY = 1:maxRadiusY+1
+        close all
         parfor radiusZ = 1:maxRadiusZ+1
             a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY-1 10+radiusZ-1], 500, outputDir);
             if isempty(a)
