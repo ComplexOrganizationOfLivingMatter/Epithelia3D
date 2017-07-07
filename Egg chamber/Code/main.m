9c3ca9ad060fb37acda8fa119bc0dd3aa553191f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-07 17:23:29 +0200

    corrected main

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -2,23 +2,23 @@
 addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 
-for numRandomization = 1:10
+for numRandomization = 2:5
     outputDir = strcat('..\resultsVoronoiEllipsoid\random_', num2str(numRandomization));
     mkdir(outputDir);
     maxRadiusY = 10;
-    maxRadiusZ = 15;
+    maxRadiusZ = 10;
     transitionByRadius = cell(maxRadiusY+1, maxRadiusZ+1);
-    for radiusY = 1:maxRadiusY+1
+    for radiusZ = 1:maxRadiusY+1
         close all
-        parfor radiusZ = 1:maxRadiusZ+1
-            a = voronoiOnEllipsoidSurface( [0 0 0], [10 10+radiusY-1 10+radiusZ-1], 500, outputDir);
+        parfor radiusY = radiusZ+1:maxRadiusZ+1
+            a = voronoiOnEllipsoidSurface( [0 0 0], [10+radiusZ-1 10+radiusY-1 10+radiusZ-1], 500, outputDir);
             if isempty(a)
                 transitionByRadius(radiusY, radiusZ) = {[]};
             else
                 transitionByRadius(radiusY, radiusZ) = {vertcat(a{:})};
             end
         end
-        transitionByRadius
+        transitionByRadius;
     end
     writetable(vertcat(transitionByRadius{:}), strcat(outputDir, '\transitionsInfo_', date, '.csv'), 'Delimiter', ';');
 end
