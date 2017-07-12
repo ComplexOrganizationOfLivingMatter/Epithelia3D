0d6d97e69584f1e757e77818b09a2af6a7c9b25f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-12 10:39:56 +0200

    removing empty cells

--- Egg chamber/Code/VoronoiEllipsoid/getAnglesOfEdgeTransition.m
+++ Egg chamber/Code/VoronoiEllipsoid/getAnglesOfEdgeTransition.m
@@ -39,9 +39,16 @@ function [tableDataAngles,anglesPerRegion] = getAnglesOfEdgeTransition( ellipsoi
     for i=1:size(neighReducted,1)
         cellsFormingEdgeTransition{i,1}=neighReducted(i,:);
         cellsFormingEdgeTransition{i,2}=intersect(ellipsoidInfo.neighbourhood{neighReducted(i,:),1})';
-        cellsFormingEdgeTransition{i,3}=intersect(ellipsoidInfo.verticesPerCell{cellsFormingEdgeTransition{i,2}},'rows');
+        
+        if size(cellsFormingEdgeTransition(i,2)) > 1
+            cellsFormingEdgeTransition{i,3}=intersect(ellipsoidInfo.verticesPerCell{cellsFormingEdgeTransition{i,2}},'rows');
+        else %2 transitios have ocurred
+            cellsFormingEdgeTransition
+        end
     end
-    tableDataAngles=cell2table(cellsFormingEdgeTransition);
+    %Removing empty cells
+    notEmptyCells = cellfun(@(x) isempty(x(3)) == 0 , cellsFormingEdgeTransition);
+    tableDataAngles=cell2table(cellsFormingEdgeTransition(notEmptyCells));
     tableDataAngles.Properties.VariableNames = {'newNeighbors' 'cellsSharingEdge' 'verticesOfEdge'};
 
 
