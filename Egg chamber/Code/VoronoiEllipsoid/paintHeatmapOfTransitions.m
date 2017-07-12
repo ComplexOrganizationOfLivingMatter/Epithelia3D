2e87a557dcca2c098992e20f76488ea189c70682
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-12 16:49:18 +0200

    transtions = exchange of neighbours

--- Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
+++ Egg chamber/Code/VoronoiEllipsoid/paintHeatmapOfTransitions.m
@@ -1,9 +1,9 @@
-function [ transitionsInfo ] = paintHeatmapOfTransitions( ellipsoidInfo, initialEllipsoid, outputDir )
+function [ exchangeNeighboursInfo ] = paintHeatmapOfTransitions( ellipsoidInfo, initialEllipsoid, outputDir )
 %PAINTHEATMAPOFTRANSITIONS Summary of this function goes here
 %   Detailed explanation goes here
     
     try
-        transitionsPerCell = cellfun(@(x, y) size(setxor(x, y), 1), ellipsoidInfo.neighbourhood, initialEllipsoid.neighbourhood);
+        exchangeNeighboursPerCell = cellfun(@(x, y) size(setxor(x, y), 1), ellipsoidInfo.neighbourhood, initialEllipsoid.neighbourhood);
 
         figure('Visible', 'off');
         clmap = hot(10);
@@ -13,7 +13,7 @@ function [ transitionsInfo ] = paintHeatmapOfTransitions( ellipsoidInfo, initial
         ellipsoidInfo.cellArea = zeros(size(ellipsoidInfo.verticesPerCell, 1), 1);
 
         for cellIndex = 1:size(ellipsoidInfo.verticesPerCell, 1)
-            cl = clmap(mod(transitionsPerCell(cellIndex),ncl)+1,:);
+            cl = clmap(mod(exchangeNeighboursPerCell(cellIndex),ncl)+1,:);
             VertCell = ellipsoidInfo.verticesPerCell{cellIndex};
             KVert = convhulln([VertCell; ellipsoidInfo.finalCentroids(cellIndex, :)]);
             patch('Vertices',[VertCell; ellipsoidInfo.finalCentroids(cellIndex, :)],'Faces', KVert,'FaceColor', cl ,'FaceAlpha', 1, 'EdgeColor', 'none')
@@ -45,64 +45,64 @@ function [ transitionsInfo ] = paintHeatmapOfTransitions( ellipsoidInfo, initial
         end
         
         %%Creating row of excel
-        transitionsInfo.xRadius = ellipsoidInfo.xRadius;
-        transitionsInfo.yRadius = ellipsoidInfo.yRadius;
-        transitionsInfo.zRadius = ellipsoidInfo.zRadius;
-        transitionsInfo.bordersSituatedAt = 2/3;
-        transitionsInfo.totalCells = size(transitionsPerCell, 1);
-        transitionsInfo.cellHeight = ellipsoidInfo.cellHeight;
+        exchangeNeighboursInfo.xRadius = ellipsoidInfo.xRadius;
+        exchangeNeighboursInfo.yRadius = ellipsoidInfo.yRadius;
+        exchangeNeighboursInfo.zRadius = ellipsoidInfo.zRadius;
+        exchangeNeighboursInfo.bordersSituatedAt = 2/3;
+        exchangeNeighboursInfo.totalCells = size(exchangeNeighboursPerCell, 1);
+        exchangeNeighboursInfo.cellHeight = ellipsoidInfo.cellHeight;
         %We get select the cells at the borders from the initial ellipsoid.
         %Not from the reduced ellipsoid. Thus, we'll always get the same
         %centroids for all the different cell heights.
-        cellsAtXBorderRight = initialEllipsoid.finalCentroids(:, 1) < -(transitionsInfo.bordersSituatedAt * initialEllipsoid.xRadius);
-        cellsAtYBorderRight = initialEllipsoid.finalCentroids(:, 2) < -(transitionsInfo.bordersSituatedAt * initialEllipsoid.yRadius);
-        cellsAtZBorderRight = initialEllipsoid.finalCentroids(:, 3) < -(transitionsInfo.bordersSituatedAt * initialEllipsoid.zRadius);
+        cellsAtXBorderRight = initialEllipsoid.finalCentroids(:, 1) < -(exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.xRadius);
+        cellsAtYBorderRight = initialEllipsoid.finalCentroids(:, 2) < -(exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.yRadius);
+        cellsAtZBorderRight = initialEllipsoid.finalCentroids(:, 3) < -(exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.zRadius);
         
-        cellsAtXBorderLeft = initialEllipsoid.finalCentroids(:, 1) > (transitionsInfo.bordersSituatedAt * initialEllipsoid.xRadius);
-        cellsAtYBorderLeft = initialEllipsoid.finalCentroids(:, 2) > (transitionsInfo.bordersSituatedAt * initialEllipsoid.yRadius);
-        cellsAtZBorderLeft = initialEllipsoid.finalCentroids(:, 3) > (transitionsInfo.bordersSituatedAt * initialEllipsoid.zRadius);
+        cellsAtXBorderLeft = initialEllipsoid.finalCentroids(:, 1) > (exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.xRadius);
+        cellsAtYBorderLeft = initialEllipsoid.finalCentroids(:, 2) > (exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.yRadius);
+        cellsAtZBorderLeft = initialEllipsoid.finalCentroids(:, 3) > (exchangeNeighboursInfo.bordersSituatedAt * initialEllipsoid.zRadius);
         
-        transitionsInfo.percentageOfTransitionsPerCell = sum(transitionsPerCell) / size(transitionsPerCell, 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCell = sum(exchangeNeighboursPerCell) / size(exchangeNeighboursPerCell, 1);
         
         
-        transitionsInfo.percentageOfTransitionsPerCellAtXBorderLeft = sum(transitionsPerCell(cellsAtXBorderLeft)) / size(transitionsPerCell(cellsAtXBorderLeft), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtXBorderRight = sum(transitionsPerCell(cellsAtXBorderRight)) / size(transitionsPerCell(cellsAtXBorderRight), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtXMiddle = sum(transitionsPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0)) / size(transitionsPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtXBorderLeft = sum(exchangeNeighboursPerCell(cellsAtXBorderLeft)) / size(exchangeNeighboursPerCell(cellsAtXBorderLeft), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtXBorderRight = sum(exchangeNeighboursPerCell(cellsAtXBorderRight)) / size(exchangeNeighboursPerCell(cellsAtXBorderRight), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtXMiddle = sum(exchangeNeighboursPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0)) / size(exchangeNeighboursPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0), 1);
         
-        transitionsInfo.percentageOfTransitionsPerCellAtYBorderLeft = sum(transitionsPerCell(cellsAtYBorderLeft)) / size(transitionsPerCell(cellsAtYBorderLeft), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtYBorderRight = sum(transitionsPerCell(cellsAtYBorderRight)) / size(transitionsPerCell(cellsAtYBorderRight), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtYMiddle = sum(transitionsPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0)) / size(transitionsPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtYBorderLeft = sum(exchangeNeighboursPerCell(cellsAtYBorderLeft)) / size(exchangeNeighboursPerCell(cellsAtYBorderLeft), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtYBorderRight = sum(exchangeNeighboursPerCell(cellsAtYBorderRight)) / size(exchangeNeighboursPerCell(cellsAtYBorderRight), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtYMiddle = sum(exchangeNeighboursPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0)) / size(exchangeNeighboursPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0), 1);
         
-        transitionsInfo.percentageOfTransitionsPerCellAtZBorderLeft = sum(transitionsPerCell(cellsAtZBorderLeft)) / size(transitionsPerCell(cellsAtZBorderLeft), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtZBorderRight = sum(transitionsPerCell(cellsAtZBorderRight)) / size(transitionsPerCell(cellsAtZBorderRight), 1);
-        transitionsInfo.percentageOfTransitionsPerCellAtZMiddle = sum(transitionsPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0)) / size(transitionsPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtZBorderLeft = sum(exchangeNeighboursPerCell(cellsAtZBorderLeft)) / size(exchangeNeighboursPerCell(cellsAtZBorderLeft), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtZBorderRight = sum(exchangeNeighboursPerCell(cellsAtZBorderRight)) / size(exchangeNeighboursPerCell(cellsAtZBorderRight), 1);
+        exchangeNeighboursInfo.percentageOfexchangeNeighboursPerCellAtZMiddle = sum(exchangeNeighboursPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0)) / size(exchangeNeighboursPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0), 1);
         
-        transitionsInfo.numCellsAtXBorderLeft = size(transitionsPerCell(cellsAtXBorderLeft), 1);
-        transitionsInfo.numCellsAtXBorderRight = size(transitionsPerCell(cellsAtXBorderRight), 1);
-        transitionsInfo.numCellsAtXMiddle = size(transitionsPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0), 1);
+        exchangeNeighboursInfo.numCellsAtXBorderLeft = size(exchangeNeighboursPerCell(cellsAtXBorderLeft), 1);
+        exchangeNeighboursInfo.numCellsAtXBorderRight = size(exchangeNeighboursPerCell(cellsAtXBorderRight), 1);
+        exchangeNeighboursInfo.numCellsAtXMiddle = size(exchangeNeighboursPerCell(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0), 1);
         
-        transitionsInfo.numCellsAtYBorderLeft = size(transitionsPerCell(cellsAtYBorderLeft), 1);
-        transitionsInfo.numCellsAtYBorderRight = size(transitionsPerCell(cellsAtYBorderRight), 1);
-        transitionsInfo.numCellsAtYMiddle = size(transitionsPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0), 1);
+        exchangeNeighboursInfo.numCellsAtYBorderLeft = size(exchangeNeighboursPerCell(cellsAtYBorderLeft), 1);
+        exchangeNeighboursInfo.numCellsAtYBorderRight = size(exchangeNeighboursPerCell(cellsAtYBorderRight), 1);
+        exchangeNeighboursInfo.numCellsAtYMiddle = size(exchangeNeighboursPerCell(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0), 1);
         
-        transitionsInfo.numCellsAtZBorderLeft = size(transitionsPerCell(cellsAtZBorderLeft), 1);
-        transitionsInfo.numCellsAtZBorderRight = size(transitionsPerCell(cellsAtZBorderRight), 1);
-        transitionsInfo.numCellsAtZMiddle = size(transitionsPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0), 1);
+        exchangeNeighboursInfo.numCellsAtZBorderLeft = size(exchangeNeighboursPerCell(cellsAtZBorderLeft), 1);
+        exchangeNeighboursInfo.numCellsAtZBorderRight = size(exchangeNeighboursPerCell(cellsAtZBorderRight), 1);
+        exchangeNeighboursInfo.numCellsAtZMiddle = size(exchangeNeighboursPerCell(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0), 1);
         
-        transitionsInfo.meanCellArea = mean(ellipsoidInfo.cellArea);
-        transitionsInfo.stdCellArea = std(ellipsoidInfo.cellArea);
+        exchangeNeighboursInfo.meanCellArea = mean(ellipsoidInfo.cellArea);
+        exchangeNeighboursInfo.stdCellArea = std(ellipsoidInfo.cellArea);
         
-        transitionsInfo.areaOfXBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtXBorderLeft));
-        transitionsInfo.areaOfXBorderRight = sum(ellipsoidInfo.cellArea(cellsAtXBorderRight));
-        transitionsInfo.areaOfXMiddle = sum(ellipsoidInfo.cellArea(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0));
+        exchangeNeighboursInfo.areaOfXBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtXBorderLeft));
+        exchangeNeighboursInfo.areaOfXBorderRight = sum(ellipsoidInfo.cellArea(cellsAtXBorderRight));
+        exchangeNeighboursInfo.areaOfXMiddle = sum(ellipsoidInfo.cellArea(cellsAtXBorderRight == 0 & cellsAtXBorderLeft == 0));
         
-        transitionsInfo.areaOfYBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtYBorderLeft));
-        transitionsInfo.areaOfYBorderRight = sum(ellipsoidInfo.cellArea(cellsAtYBorderRight));
-        transitionsInfo.areaOfYMiddle = sum(ellipsoidInfo.cellArea(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0));
+        exchangeNeighboursInfo.areaOfYBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtYBorderLeft));
+        exchangeNeighboursInfo.areaOfYBorderRight = sum(ellipsoidInfo.cellArea(cellsAtYBorderRight));
+        exchangeNeighboursInfo.areaOfYMiddle = sum(ellipsoidInfo.cellArea(cellsAtYBorderRight == 0 & cellsAtYBorderLeft == 0));
         
-        transitionsInfo.areaOfZBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtZBorderLeft));
-        transitionsInfo.areaOfZBorderRight = sum(ellipsoidInfo.cellArea(cellsAtZBorderRight));
-        transitionsInfo.areaOfZMiddle = sum(ellipsoidInfo.cellArea(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0));
+        exchangeNeighboursInfo.areaOfZBorderLeft = sum(ellipsoidInfo.cellArea(cellsAtZBorderLeft));
+        exchangeNeighboursInfo.areaOfZBorderRight = sum(ellipsoidInfo.cellArea(cellsAtZBorderRight));
+        exchangeNeighboursInfo.areaOfZMiddle = sum(ellipsoidInfo.cellArea(cellsAtZBorderRight == 0 & cellsAtZBorderLeft == 0));
         
         axis equal
         if isequal(outputDir, '') == 0
