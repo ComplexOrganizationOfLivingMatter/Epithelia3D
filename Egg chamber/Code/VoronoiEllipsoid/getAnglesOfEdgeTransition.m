fe90f9029bc9c982c77dd87894c4af1d9fff0eeb
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-12 16:49:09 +0200

    adding new angles

--- Egg chamber/Code/VoronoiEllipsoid/getAnglesOfEdgeTransition.m
+++ Egg chamber/Code/VoronoiEllipsoid/getAnglesOfEdgeTransition.m
@@ -135,22 +135,40 @@ function [tableDataAngles,anglesPerRegion] = getAnglesOfEdgeTransition( ellipsoi
             savefig(strcat(outputDir, '\angles_ellipsoid_x', num2str(ellipsoidInfo.xRadius), '_y', num2str(ellipsoidInfo.yRadius), '_z', num2str(ellipsoidInfo.zRadius), '_cellHeight', num2str(ellipsoidInfoReducted.cellHeight), '.fig'));
 
 
-            anglesPerRegion.averageAnglesLess30EndRight=sum(anglesEndRight<30)/length(anglesEndRight);
-            anglesPerRegion.averageAnglesBetw30_60EndRight=sum(anglesEndRight>=30 & anglesEndRight<60)/length(anglesEndRight);
-            anglesPerRegion.averageAnglesMore60EndRight=sum(anglesEndRight>=60)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesLess15EndRight=sum(anglesEndRight<15)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesBetw15_30EndRight=sum(anglesEndRight>=15 & anglesEndRight<30)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesBetw30_45EndRight=sum(anglesEndRight>=30 & anglesEndRight<45)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesBetw45_60EndRight=sum(anglesEndRight>=45 & anglesEndRight<60)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesBetw60_75EndRight=sum(anglesEndRight>=60 & anglesEndRight<75)/length(anglesEndRight);
+            anglesPerRegion.averageAnglesMore75EndRight=sum(anglesEndRight>=75)/length(anglesEndRight);
 
-            anglesPerRegion.averageAnglesLess30EndLeft=sum(anglesEndLeft<30)/length(anglesEndLeft);
-            anglesPerRegion.averageAnglesBetw30_60EndLeft=sum(anglesEndLeft>=30 & anglesEndLeft<60)/length(anglesEndLeft);
-            anglesPerRegion.averageAnglesMore60EndLeft=sum(anglesEndLeft>=60)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesLess15EndLeft=sum(anglesEndLeft<15)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesBetw15_30EndLeft=sum(anglesEndLeft>=15 & anglesEndLeft<30)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesBetw30_45EndLeft=sum(anglesEndLeft>=30 & anglesEndLeft<45)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesBetw45_60EndLeft=sum(anglesEndLeft>=45 & anglesEndLeft<60)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesBetw60_75EndLeft=sum(anglesEndLeft>=60 & anglesEndLeft<75)/length(anglesEndLeft);
+            anglesPerRegion.averageAnglesMore75EndLeft=sum(anglesEndLeft>=75)/length(anglesEndLeft);
 
-            anglesPerRegion.averageAnglesLess30EndGlobal=sum([anglesEndLeft,anglesEndRight]<30)/length([anglesEndLeft,anglesEndRight]);
-            anglesPerRegion.averageAnglesBetw30_60EndGlobal=sum([anglesEndLeft,anglesEndRight]>=30 & [anglesEndLeft,anglesEndRight]<60)/length([anglesEndLeft,anglesEndRight]);
-            anglesPerRegion.averageAnglesMore60EndGlobal=sum([anglesEndLeft,anglesEndRight]>=60)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesLess15EndGlobal=sum([anglesEndLeft,anglesEndRight]<15)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesBetw15_30EndGlobal=sum([anglesEndLeft,anglesEndRight]>=15 & [anglesEndLeft,anglesEndRight]<30)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesBetw30_45EndGlobal=sum([anglesEndLeft,anglesEndRight]>=30 & [anglesEndLeft,anglesEndRight]<45)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesBetw45_60EndGlobal=sum([anglesEndLeft,anglesEndRight]>=45 & [anglesEndLeft,anglesEndRight]<60)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesBetw60_75EndGlobal=sum([anglesEndLeft,anglesEndRight]>=60 & [anglesEndLeft,anglesEndRight]<75)/length([anglesEndLeft,anglesEndRight]);
+            anglesPerRegion.averageAnglesMore75EndGlobal=sum([anglesEndLeft,anglesEndRight]>=75)/length([anglesEndLeft,anglesEndRight]);
 
-
-            anglesPerRegion.averageAnglesLess30CentralRegion=sum(anglesCentralRegion<30)/length(anglesCentralRegion);
-            anglesPerRegion.averageAnglesBetw30_60CentralRegion=sum(anglesCentralRegion>=30 & anglesCentralRegion<60)/length(anglesCentralRegion);
-            anglesPerRegion.averageAnglesMore60CentralRegion=sum(anglesCentralRegion>=60)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesLess15CentralRegion=sum(anglesCentralRegion<15)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesBetw15_30CentralRegion=sum(anglesCentralRegion>=15 & anglesCentralRegion<30)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesBetw30_45CentralRegion=sum(anglesCentralRegion>=30 & anglesCentralRegion<45)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesBetw45_60CentralRegion=sum(anglesCentralRegion>=45 & anglesCentralRegion<60)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesBetw60_75CentralRegion=sum(anglesCentralRegion>=60 & anglesCentralRegion<75)/length(anglesCentralRegion);
+            anglesPerRegion.averageAnglesMore75CentralRegion=sum(anglesCentralRegion>=75)/length(anglesCentralRegion);
+            
+            anglesPerRegion.averageAnglesLess15Total=sum(angles<15)/length(angles);
+            anglesPerRegion.averageAnglesBetw15_30Total=sum(angles>=15 & angles<30)/length(angles);
+            anglesPerRegion.averageAnglesBetw30_45Total=sum(angles>=30 & angles<45)/length(angles);
+            anglesPerRegion.averageAnglesBetw45_60Total=sum(angles>=45 & angles<60)/length(angles);
+            anglesPerRegion.averageAnglesBetw60_75Total=sum(angles>=60 & angles<75)/length(angles);
+            anglesPerRegion.averageAnglesMore75Total=sum(angles>=75)/length(angles);
 
             anglesPerRegion.numAnglesEndLeft=length(anglesEndLeft);
             anglesPerRegion.numAnglesEndRight=length(anglesEndRight);
