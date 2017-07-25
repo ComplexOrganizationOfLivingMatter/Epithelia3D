d8f60bb61c0d7014d81e95588e27c98df27def55
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-07-25 15:13:37 +0200

    clean main

--- Egg chamber/Code/main.m
+++ Egg chamber/Code/main.m
@@ -1,24 +1,49 @@
-
 addpath(genpath('VoronoiEllipsoid'));
 addpath(genpath('lib'));
 
-for numRandomization = 5:11
-    outputDir = strcat('..\resultsVoronoiEllipsoid\random_', num2str(numRandomization));
+%On this order:
+% - Zeppelin
+% - Filled donut
+% - Sphere
+% - Stage 4
+% - Stage 8
+allCombinations = {
+    15 10 10 [0.5, 1, 2]
+    10 15 15 [0.5, 1, 2]
+    10 10 10 [0.5, 1, 2]
+    100 50 50 6
+    38.57277778	31.61605556 31.61605556	5.506047536
+    };
+
+for numRandomization = 1:20
+    outputDir = strcat('..\results\random_', num2str(numRandomization));
     mkdir(outputDir);
-    maxRadiusY = 10;
-    maxRadiusZ = 10;
-    transitionByRadius = cell(maxRadiusY+1, maxRadiusZ+1);
-    for radiusZ = 1:maxRadiusY+1
-        close all
-        parfor radiusY = radiusZ+1:maxRadiusZ+1
-            a = voronoiOnEllipsoidSurface( [0 0 0], [10+radiusZ-1 10+radiusY-1 10+radiusZ-1], 500, outputDir);
-            if isempty(a)
-                transitionByRadius(radiusY, radiusZ) = {[]};
-            else
-                transitionByRadius(radiusY, radiusZ) = {vertcat(a{:})};
-            end
+    transitionByRadius = cell(size(allCombinations, 1));
+    parfor numCombination = 1:size(allCombinations, 1)
+        radiusX = allCombinations{numCombination, 1};
+        radiusY = allCombinations{numCombination, 2};
+        radiusZ = allCombinations{numCombination, 3};
+        hCell = allCombinations{numCombination, 4};
+        
+        if min([radiusX, radiusY, radiusZ]) ~= 10
+            radiusInModelY = 10;
+            radiusZ = (radiusZ * radiusInModelY)/radiusY;
+            radiusX = (radiusX * radiusInModelY)/radiusY;
+            hCell = (hCell * radiusInModelY)/radiusY;
+            radiusY = radiusInModelY;
         end
-        transitionByRadius;
+        
+        a = voronoiOnEllipsoidSurface([0 0 0], [radiusX radiusY radiusZ], 500, outputDir, hCell);
+        if isempty(a)
+            transitionByRadius(numCombination) = {[]};
+        else
+            transitionByRadius(numCombination) = {vertcat(a{:})};
+        end
     end
     writetable(vertcat(transitionByRadius{:}), strcat(outputDir, '\transitionsInfo_', date, '.csv'), 'Delimiter', ';');
+    transitionByRadiusAll(end+1, :) = vertcat(transitionByRadius{:});
 end
+writetable(transitionByRadiusAll, strcat('transitionsInfoAllRandomizations_', date, '.xls'), 'Delimiter', ';')
+%Calculate mean of all the transitions
+calculateMeanOfXls(strcat('transitionsInfoAllRandomizations_', date, '.xls'));
+
