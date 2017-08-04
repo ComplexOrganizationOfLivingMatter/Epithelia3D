3212e4f8b0bd4e1ee236c8e452c91c3e2138c75e
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-08-04 14:38:19 +0200

    2 cylinders

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
@@ -4,20 +4,25 @@ load('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\Image_10.mat')
 close all
 uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_ZoomOut.fig',1)
 
-[az, el] = view;
+
 refFig = gca;
 surfaceRatios=[1,3,6,9];
 colours = colorcube(50);
 colours(1, :) = [0 0 0];
-for i=1:length(surfaceRatios)
-    h = figure;
+for i=2:length(surfaceRatios)
+    %h = figure;
+    
+    %We put the first cylinder to append the bigger one
+    uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_referenceOK.fig',1)
+    hold on;
     Img=cell2mat(listLOriginalProjection.L_originalProjection(surfaceRatios(i)));
     imgRGB=Img;
     [imgRows,imgCols,imgPlanes] = size(imgRGB);
     [X,Y,Z] = cylinder(imgRows,imgCols);
     Z=Z*4000;
     X=X*.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));Y=Y*0.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));
-    warp(X,Y,Z,imgRGB,colours);
+    hRef = warp(X,Y,Z,imgRGB,colours);
+    hRef.FaceAlpha = 0.9;
     axis equal
     newFig = gca;
     newFig.CameraPositionMode = refFig.CameraPositionMode;
@@ -32,6 +37,6 @@ for i=1:length(surfaceRatios)
     
     newFig.Visible = 'off';
     set(get(0,'children'),'Color','w')
-    print(strcat('cylinder_Expansion_', num2str(listLOriginalProjection.surfaceRatio(surfaceRatios(i)))), '-dtiff', '-r600');
-    %Edge in black
+    print(strcat('cylinder_Alpha_09_Expansion_', num2str(listLOriginalProjection.surfaceRatio(surfaceRatios(i)))), '-dtiff', '-r600');
+    hold off;
 end
