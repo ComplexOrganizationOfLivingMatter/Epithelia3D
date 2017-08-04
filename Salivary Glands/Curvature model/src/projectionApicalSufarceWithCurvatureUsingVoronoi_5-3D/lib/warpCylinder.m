fe5405babfcc9abc6b3807dbc7bb65a5d719162b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-08-04 12:59:06 +0200

    putting camera as the reffigure and several interesting properties

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
@@ -1,13 +1,37 @@
 %warp cylinder
+clear all
+load('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\Image_10.mat')
+close all
+uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_ZoomOut.fig',1)
+
+[az, el] = view;
+refFig = gca;
 surfaceRatios=[1,3,6,9];
+colours = colorcube(50);
+colours(1, :) = [0 0 0];
 for i=1:length(surfaceRatios)
-    figure;
+    h = figure;
     Img=cell2mat(listLOriginalProjection.L_originalProjection(surfaceRatios(i)));
     imgRGB=Img;
     [imgRows,imgCols,imgPlanes] = size(imgRGB);
     [X,Y,Z] = cylinder(imgRows,imgCols);
     Z=Z*4000;
     X=X*.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));Y=Y*0.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));
-    warp(X,Y,Z,imgRGB,colormap(colorcube(50)));
+    warp(X,Y,Z,imgRGB,colours);
     axis equal
+    newFig = gca;
+    newFig.CameraPositionMode = refFig.CameraPositionMode;
+    newFig.CameraTargetMode = refFig.CameraPositionMode;
+    newFig.CameraUpVectorMode = refFig.CameraPositionMode;
+    newFig.CameraViewAngleMode = refFig.CameraPositionMode;
+    
+    newFig.CameraPosition = refFig.CameraPosition;
+    newFig.CameraTarget = refFig.CameraTarget;
+    newFig.CameraUpVector = refFig.CameraUpVector;
+    newFig.CameraViewAngle = refFig.CameraViewAngle;
+    
+    newFig.Visible = 'off';
+    set(get(0,'children'),'Color','w')
+    print(strcat('cylinder_', num2str(surfaceRatios(i))), '-dtiff', '-r600');
+    %Edge in black
 end
