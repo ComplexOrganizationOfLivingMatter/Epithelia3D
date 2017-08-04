017dd92b2cdefeaa4c6d3d0fca573d9b65d1df36
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-08-04 15:02:12 +0200

    nyaaaan

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/warpCylinder.m
@@ -3,18 +3,18 @@ clear all
 load('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\Image_10.mat')
 close all
 uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_ZoomOut.fig',1)
-
+%uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR5.fig',1)
 
 refFig = gca;
 surfaceRatios=[1,3,6,9];
 colours = colorcube(50);
 colours(1, :) = [0 0 0];
-for i=2:length(surfaceRatios)
-    %h = figure;
+for i=1:length(surfaceRatios)
+    h = figure;
     
     %We put the first cylinder to append the bigger one
-    uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_referenceOK.fig',1)
-    hold on;
+    %uiopen('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\voronoiCylinderSR1_referenceOK.fig',1)
+    %hold on;
     Img=cell2mat(listLOriginalProjection.L_originalProjection(surfaceRatios(i)));
     imgRGB=Img;
     [imgRows,imgCols,imgPlanes] = size(imgRGB);
@@ -22,7 +22,7 @@ for i=2:length(surfaceRatios)
     Z=Z*4000;
     X=X*.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));Y=Y*0.2*listLOriginalProjection.surfaceRatio(surfaceRatios(i));
     hRef = warp(X,Y,Z,imgRGB,colours);
-    hRef.FaceAlpha = 0.9;
+    %hRef.FaceAlpha = 0.9;
     axis equal
     newFig = gca;
     newFig.CameraPositionMode = refFig.CameraPositionMode;
@@ -37,6 +37,6 @@ for i=2:length(surfaceRatios)
     
     newFig.Visible = 'off';
     set(get(0,'children'),'Color','w')
-    print(strcat('cylinder_Alpha_09_Expansion_', num2str(listLOriginalProjection.surfaceRatio(surfaceRatios(i)))), '-dtiff', '-r600');
+    print(strcat('cylinder_Expansion2_', num2str(listLOriginalProjection.surfaceRatio(surfaceRatios(i)))), '-dtiff', '-r600');
     hold off;
 end
