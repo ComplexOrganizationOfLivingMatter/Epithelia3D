0dbf630abde19e82bf098ef1b28eb9dfbf14653c
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2017-08-07 12:02:47 +0200

    colored cyliderModel readyyyyyyyyyyy! FUCK YOU PABLO

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/colorExpandedCylinderModel.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/colorExpandedCylinderModel.m
@@ -1,16 +1,32 @@
 
-interval1=200;
-interval2=302;
-for i=1:size(listLOriginalProjection,1)
+interval1=1;
+interval2=512;
+steps=[1,3,6,9];
+% order1=randperm(50,50);
+% order2=randperm(50,50);
+% order3=randperm(50,50);
+M=jet(50);
+% cOrder1=M(order1,:);
+% cOrder2=M(order2,:);
+cOrder3=M(order3,:);
+for i=1:length(steps)
 
-    Img=cell2mat(listLOriginalProjection.L_originalProjection(i));
-    surfaceRatio=listLOriginalProjection.surfaceRatio(i);
-    figure
-    i
-    imshow(Img(:,round(interval1*surfaceRatio):round(interval2*surfaceRatio)),colorcube (400))
+    Img=cell2mat(listLOriginalProjection.L_originalProjection(steps(i)));
     
-    print('-f1', '-r300','-dpdf',[ num2str(surfaceRatio) '.pdf'])
-   
+    surfaceRatio=listLOriginalProjection.surfaceRatio(steps(i));
+    %figure('Visible','off')
+    i
+    mask=zeros(1024,2560);
+    mask(Img>0)=Img(Img>0);
+    
+    imshow(mask,cOrder3)
+    
+%     imshow(Img(:,round(interval1*surfaceRatio):round(interval2*surfaceRatio)),colorcube (50))
+%     hold on
+    print('-f1', '-r300','-dpdf',['D:\Pedro\Epithelia3D\docs\cylinderModel\expansion\50cellsExpanded\' num2str(surfaceRatio) '.pdf'])
+    print('-f1', '-r300','-dtiff',['D:\Pedro\Epithelia3D\docs\cylinderModel\expansion\50cellsExpanded\' num2str(surfaceRatio) '.tiff'])
+    
+    %close all
     
 end
 
