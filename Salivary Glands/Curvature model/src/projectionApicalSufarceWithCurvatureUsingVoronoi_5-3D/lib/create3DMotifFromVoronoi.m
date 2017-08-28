6088f1695ec34bcf9b093af0abe003f5ce8c4d17
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-08-28 16:02:58 +0200

    working version of a cylinder in 3D

--- Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/create3DMotifFromVoronoi.m
+++ Salivary Glands/Curvature model/src/projectionApicalSufarceWithCurvatureUsingVoronoi_5-3D/lib/create3DMotifFromVoronoi.m
@@ -3,30 +3,30 @@ function [ ] = create3DMotifFromVoronoi( )
 %   Detailed explanation goes here
     load('D:\Pablo\Epithelia3D\Salivary Glands\Curvature model\data\Image_10.mat')
     
-    selectedCells = [22, 25, 30];
+    selectedCells = 1:max(listLOriginalProjection.L_originalProjection{1});
     %% Rb/Ra = 2
     pixelsPerCell = {};
-    
+    maxPlanes = 6;
     for selectedCell = 1:size(selectedCells, 2)
-        for numPlane = 1:6
+        for numPlane = 1:maxPlanes
             actualPlaneImg = cell2mat(listLOriginalProjection.L_originalProjection(numPlane));
             
             [imgRows,imgCols] = size(actualPlaneImg);
-%             [X,Y,Z] = cylinder(imgRows,imgCols);
-%             Z=Z*4000;
-%             X=X*0.2*listLOriginalProjection.surfaceRatio(numPlane);
-%             Y=Y*0.2*listLOriginalProjection.surfaceRatio(numPlane);
+            [X,Y,~] = cylinder(imgRows,imgCols);
+            X=X*0.2*listLOriginalProjection.surfaceRatio(numPlane);
+            Y=Y*0.2*listLOriginalProjection.surfaceRatio(numPlane);
 %             hRef = warp(X,Y,Z,actualPlaneImg);
             
             [actualCellPxX, actualCellPxY] = find(actualPlaneImg == selectedCells(selectedCell));
             
-            actualCellPxX = actualCellPxX * 0.2;
-            actualCellPxY = actualCellPxY * 0.2;
+            actualCellPxX = actualCellPxX';
+            actualCellPxZ = X(1, actualCellPxY);
+            actualCellPxY = Y(1, actualCellPxY);
             
-            actualCellPxs = [actualCellPxX, actualCellPxY];
+            actualCellPxs = vertcat(actualCellPxX, actualCellPxY, actualCellPxZ);
             
-            actualCellBoundary = boundary(actualCellPxX, actualCellPxY, 0);
-            boundaryPxs = [actualCellPxs(actualCellBoundary, :), ones(size(actualCellBoundary, 1), 1) * listLOriginalProjection.surfaceRatio(numPlane) * 4000];
+            %actualCellBoundary = boundary(actualCellPxX', actualCellPxY', actualCellPxZ', 0);
+            boundaryPxs = actualCellPxs';
             if numPlane == 1
                 pixelsPerCell{selectedCell} = boundaryPxs;
             else
@@ -36,19 +36,19 @@ function [ ] = create3DMotifFromVoronoi( )
         pixelsPerCell;
     end
     
-    colours = [255 255 51;  255 0 0; 102 153 51; 102 153 255] / 255;
-    plotFigure3DFromCellBoundaries(pixelsPerCell, colours);
-%     figure;
-%     for numCell = 1:size(selectedCells, 2);
-%         actualCellPxs = pixelsPerCell{numCell};
-%         xCell = actualCellPxs(:, 1);
-%         yCell = actualCellPxs(:, 2);
-%         zCell = actualCellPxs(:, 3);
-%         [yCellPol, zCellPol, xCellPol] = cart2pol(yCell, zCell, xCell);
-%         actualCellPxs = [xCellPol, yCellPol, zCellPol];
-%         k = boundary(actualCellPxs, 0);
-%         trisurf(k, xCellPol, yCellPol, zCellPol, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 1);
-%         hold on;
-%     end
+    %colours = [255 255 51;  255 0 0; 102 153 51; 102 153 255] / 255;
+    colours = colorcube(50);
+%    plotFigure3DFromCellBoundaries(pixelsPerCell, colours);
+    figure;
+    for numCell = 1:size(selectedCells, 2);
+        actualCellPxs = pixelsPerCell{numCell};
+        xCell = actualCellPxs(:, 1);
+        yCell = actualCellPxs(:, 2);
+        zCell = actualCellPxs(:, 3);
+        actualCellPxs = [xCell, yCell, zCell];
+        k = boundary(actualCellPxs, 0);
+        trisurf(k, xCell, yCell, zCell, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 1);
+        hold on;
+    end
 end
 
