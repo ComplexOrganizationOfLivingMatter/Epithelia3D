ba1c6142f7aa2e34e577f8410cdea71b0d1cfb52
TinaForja <tinaforja93@hotmail.es>
2017-09-04 17:50:10 +0200

    Auxiliary function by mistake in the other function.
    You have to modify the main function so you save before any irreversible changes.

--- display_labelled.m
+++ display_labelled.m
@@ -1,5 +1,7 @@
 function [fig]= display_labelled(fig, LayerCentroid)
 
+delete(findall(fig, 'Type', 'Text'));
+
 for numLayer=1:size(LayerCentroid,1)
     for numCentroidLayer=1:size(LayerCentroid{numLayer,1})
         text(LayerCentroid{numLayer,1}(numCentroidLayer,2), LayerCentroid{numLayer,1}(numCentroidLayer, 3),sprintf('%d',numCentroidLayer),'HorizontalAlignment','center','VerticalAlignment','middle','Color','white','FontSize',15);        
--- layer_write_mode.m
+++ layer_write_mode.m
@@ -4,34 +4,34 @@ newLayer1=false;
 
 label=input('Write the number of the wrongly marked label: ');
 
-labelOld=input('Write the old label: ');
-labelNew=input('Write the new label: ');
+layerOld=input('Write the old layer: ');
+layerNew=input('Write the new layer: ');
 
-x1=LayerCentroid{labelOld,1}(label,2);
-y1=LayerCentroid{labelOld,1}(label,3);
+x1=LayerCentroid{layerOld,1}(label,2);
+y1=LayerCentroid{layerOld,1}(label,3);
 Pixel = [round(x1),round(y1)];%x1=round(x1);y1=round(y1);
 Centroids=[x1,y1];
 
 
 %Se trata la capa antigua para elimitar ese centroide de esa capa
-for numCentroidLayer=1:size(LayerCentroid{labelOld,1},1)
-    
-    if LayerCentroid{labelOld,1}(numCentroidLayer,:)== horzcat(numFrame,Centroids)
-        LayerCentroid{labelOld,1}(numCentroidLayer,:)=[];
-        LayerPixel{labelOld,1}(numCentroidLayer,:)=[];
+for numCentroidLayer=1:size(LayerCentroid{layerOld,1},1)  
+    if LayerCentroid{layerOld,1}(numCentroidLayer,:)== horzcat(numFrame,Centroids)
+        LayerCentroid{layerOld,1}(numCentroidLayer,:)=[];
+        LayerPixel{layerOld,1}(numCentroidLayer,:)=[];
+        break
     end
 end
 
-%Se trata la capa nueva para incluir ese centroide en esa capa
 
-if labelNew > size(LayerCentroid, 1) %Si no hay capa creada 
-    LayerCentroid{labelNew, 1} = horzcat(numFrame, Centroids);
-    LayerPixel{labelNew, 1} = horzcat(numFrame, Pixel);
+%Se trata la capa nueva para incluir ese centroide en esa capa
+if layerNew > size(LayerCentroid, 1) %Si no hay capa creada 
+    LayerCentroid{layerNew, 1} = horzcat(numFrame, Centroids);
+    LayerPixel{layerNew, 1} = horzcat(numFrame, Pixel);
     newLayer1=true;
     
-elseif newLayer1==true %Hay capa creada
-    LayerCentroid{labelNew,1}= vertcat(LayerCentroid{labelNew,1}, horzcat(numFrame,Centroids));
-    LayerPixel{labelNew,1}= vertcat(LayerPixel{labelNew,1}, horzcat(numFrame,Pixel));
+else %Hay capa creada
+    LayerCentroid{layerNew,1}= vertcat(LayerCentroid{layerNew,1}, horzcat(numFrame,Centroids));
+    LayerPixel{layerNew,1}= vertcat(LayerPixel{layerNew,1}, horzcat(numFrame,Pixel));
 end
 
 end
