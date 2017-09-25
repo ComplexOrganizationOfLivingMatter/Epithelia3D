2caed15c2b33fb4423048a0c0bc4594042124d5f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-25 10:38:22 +0200

    bugfix

--- Zebra_flor/src/LayerAnalysis/layerAnalysis.m
+++ Zebra_flor/src/LayerAnalysis/layerAnalysis.m
@@ -1,5 +1,7 @@
 load('..\..\docs\LayersCentroids7.mat')
 
+addpath(genpath('findND'));
+
 seeds = round(vertcat(LayerCentroid{:}));
 
 img3D = zeros(max(seeds) + 1);
@@ -24,10 +26,10 @@ for numSeed = 1:size(seeds, 1)
     imgDistPerSeed = bwdist(img3DActual);
     regionActual = imgDistPerSeed == imgDist;
     regionOfCell(numSeed) = {(regionActual) * numSeed};
-    img3DLabelled(regionActual) = numSeed; 
+    img3DLabelled(regionActual) = numSeed;
     img3DActual(actualSeed(1), actualSeed(2), actualSeed(3)) = 0;
     
-    [x, y, z] = findND(img3DLabelled);
+    [x, y, z] = findND(img3DLabelled == numSeed);
     cellFigure = alphaShape(x, y , z);
     plot(cellFigure, 'FaceColor', colours(numSeed, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
     hold on;
