7adee31027472c2013dedb772ab4e3d563ad11af
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-26 13:50:09 +0200

    reconstructing voronoi 3D ellipsoid

--- InSilicoModels/VoronoiEllipsoid/src/create3DVoronoiFromCentroids.m
+++ InSilicoModels/VoronoiEllipsoid/src/create3DVoronoiFromCentroids.m
@@ -23,5 +23,37 @@ function [ img3D ] = create3DVoronoiFromCentroids( centroids,  augmentedCentroid
     end
     
     imgWithDistances = bwdist(img3D);
+    
+    %Removing invalid areas
+    upSide = (ellipsoidInfo.xRadius - cellHeight)^2 * (ellipsoidInfo.yRadius - cellHeight)^2 * (ellipsoidInfo.zRadius - cellHeight)^2;
+    downSide = ((ellipsoidInfo.yRadius - cellHeight)^2 * (ellipsoidInfo.zRadius - cellHeight)^2 * finalCentroids(:, 1).^2) + ((ellipsoidInfo.xRadius - cellHeight)^2 * (ellipsoidInfo.zRadius - cellHeight)^2 * finalCentroids(:, 2).^2) + ((ellipsoidInfo.yRadius - cellHeight)^2 * (ellipsoidInfo.xRadius - cellHeight)^2 * finalCentroids(:, 3).^2);
+    conversorFactor = sqrt(upSide./downSide);
+    
+    %Reconstruct voronoiCells
+    img3DLabelled = zeros(max(augmentedCentroids));
+    img3DActual = zeros(max(augmentedCentroids));
+    colours = colorcube(size(centroids, 1));
+    figure;
+    seedsInfo = [];
+    for numSeed = 1:size(centroids, 1)
+        numSeed
+        img3DActual(img3D == numSeed) = 1;
+        imgDistPerSeed = bwdist(img3DActual);
+        regionActual = imgDistPerSeed == imgWithDistances;
+        img3DLabelled(regionActual) = numSeed;
+        img3DActual(img3D == numSeed) = 0;
+
+        [x, y, z] = findND(img3DLabelled == numSeed);
+        cellFigure = alphaShape(x, y, z, 2);
+        plot(cellFigure, 'FaceColor', colours(numSeed, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+        hold on;
+
+        seedsInfo(numSeed).ID = numSeed;
+        seedsInfo(numSeed).region = regionActual;
+        seedsInfo(numSeed).volume = cellFigure.volume;
+        seedsInfo(numSeed).colour = colours(numSeed, :);
+        seedsInfo(numSeed).pxCoordinates = [x, y, z];
+        seedsInfo(numSeed).cellHeight = max(z) - min(z);
+    end
 end
 
