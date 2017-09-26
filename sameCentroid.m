226cbcac290fa56c50230a3367471bb6848d64f1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-26 17:03:28 +0200

    Last details of the sameCentroid function
    Clean, comment and save the file with the result of the function.

--- sameCentroid.m
+++ sameCentroid.m
@@ -1,13 +1,13 @@
-function [LayerCentroid, trackingCentroid, finalCentroid] = sameCentroid( pathArchMat, initialFrame, maxFrame)
+function [LayerCentroid, trackingCentroid, finalCentroid] = sameCentroid( pathArchMat, initialFrame, maxFrame, folderNumber)
 %UNTITLED3 Summary of this function goes here
 %   Detailed explanation goes here
 
 %Variables
 load(pathArchMat);
 numCell=1;
-repeatCentroid=1;
+n=1;
 
-%Etiquetas a cada célula
+%Labels to each cell
 for numFrame=initialFrame:maxFrame
     for numLayer=1:size(LayerCentroid)
         for numCentroidLayer= 1:size(LayerCentroid{numLayer,1})
@@ -19,7 +19,7 @@ for numFrame=initialFrame:maxFrame
     end   
 end
 
-%Seguimiento de una misma célula
+%Tracking of the same cell
 for numFrame=initialFrame+1:maxFrame
     oldCentroid=ismember(round(centroids{numFrame}), pixel{numFrame-1},'rows');
     trackingCentroid{numFrame,1}=zeros(size(centroids{numFrame,1},1),6);
@@ -33,15 +33,6 @@ for numFrame=initialFrame+1:maxFrame
         end
     end
 end
-field1='ID';
-field2='Centroid';
-n=1;
-% for numFrame=initialFrame:maxFrame
-%     for numCentroid=1:size(centroids{numFrame,1},1)
-%         finalCentroid{numFrame,1}(numCentroid,:)=[n,centroids{numFrame,1}(numCentroid,:)];
-%         n=n+1;
-%     end
-% end
 
 for numLayer=1:size(LayerCentroid)
     for numCentroidLayer=1:size(LayerCentroid{numLayer,1})
@@ -50,35 +41,26 @@ for numLayer=1:size(LayerCentroid)
         finalCentroid{n,2}=coord;
         n=n+1;
     end
-end
+end 
 
 for numFrame=initialFrame+1:maxFrame
     for numCentroid=1:size(centroids{numFrame,1},1)
         if trackingCentroid{numFrame,1}(numCentroid,1)~=0
-            
-                
-                [cen, index] =ismember(trackingCentroid{numFrame,1}(numCentroid,1:3),vertcat(finalCentroid{:,2}), 'rows');
-                
-                    if cen==1
-                        finalCentroid{end+1,1}(1,1)=finalCentroid{index,1}(1,1);
-                        finalCentroid{end,2}(1,1:3)=trackingCentroid{numFrame,1}(numCentroid,4:6);
-                        
-                    end
-                
+            [cen, index] =ismember(trackingCentroid{numFrame,1}(numCentroid,1:3),vertcat(finalCentroid{:,2}), 'rows');
+            if cen==1
+                finalCentroid{end+1,1}(1,1)=finalCentroid{index,1}(1,1);
+                finalCentroid{end,2}(1,1:3)=trackingCentroid{numFrame,1}(numCentroid,4:6);
+            end
         end
     end
 end
-% for numFrame=initialFrame:maxFrame
-%     for numLayer=1:size(LayerCentroid)        
-%         for numCentroidLayer=1:size(LayerCentroid{numLayer,1})
-%             if LayerCentroid{numLayer,1}(numCentroidLayer, 1)==numFrame
-%                 LayerCentroid{numLayer,1}(numCentroidLayer, 2:3)== trackingCentroid{numFrame,1}(numCentroid, )
-%             end
-%         end
-%         
-%     end
-% end
 
+%Sort by labels
 finalCentroid=sortrows(finalCentroid, 1);
+
+% Save the result to a .mat file
+finalFileName=['trackingCentroids' sprintf('%d',folderNumber) '.mat'];
+save(finalFileName, 'finalCentroid')
+
 end
 
