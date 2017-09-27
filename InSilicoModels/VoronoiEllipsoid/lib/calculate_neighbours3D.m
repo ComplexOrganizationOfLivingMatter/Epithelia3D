7eb747e9631a2b6dec47158c0dea40610fc11812
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-27 15:17:18 +0200

    calculate neighbours of 3D image voronoi

---
+++ InSilicoModels/VoronoiEllipsoid/lib/calculate_neighbours3D.m
@@ -0,0 +1,22 @@
+function [neighs_real, sides_cells] = calculate_neighbours3D(L_img)
+
+%% Generate neighbours
+    ratio=2;
+    neighs_real={};
+    cells=sort(unique(L_img));
+    cells=cells(cells~=0);                  %% Deleting cell 0 from range
+
+    [xgrid, ygrid, zgrid] = meshgrid(-ratio:ratio); 
+    ball = (sqrt(xgrid.^2 + ygrid.^2 + zgrid.^2) <= ratio); 
+
+    for cel=1 : length(cells)
+        BW = bwperim(L_img==cells(cel));
+
+        BW_dilate = imdilate(gpuArray(BW), ball);
+        pixels_neighs=find(BW_dilate==1);
+        neighs=unique(L_img(pixels_neighs));
+        neighs_real{cells(cel)}=neighs(neighs ~= 0 & neighs ~= cells(cel));
+        sides_cells(cells(cel))=length(neighs_real{1,cells(cel)});
+    end
+end
+
