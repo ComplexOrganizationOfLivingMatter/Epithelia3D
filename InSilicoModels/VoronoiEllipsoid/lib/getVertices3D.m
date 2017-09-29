f8d79184e3ae0290830f74115c272396ea24b1d9
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-29 12:24:20 +0200

    improving efficiency on getVertices3D

--- InSilicoModels/VoronoiEllipsoid/lib/getVertices3D.m
+++ InSilicoModels/VoronoiEllipsoid/lib/getVertices3D.m
@@ -9,23 +9,47 @@ ball = (sqrt(xgrid.^2 + ygrid.^2 + zgrid.^2) <= ratio);
 neighboursVertices = buildTripletsOfNeighs( neighbours );%intersect dilatation of each cell of triplet
 vertices = cell(size(neighboursVertices, 1), 1);
 
+initBW1 =zeros(size(L_img));
+
+initBW2 = zeros(size(L_img));
+
+initBW3 = zeros(size(L_img));
+
+initBorderImg = zeros(size(L_img));
+
+
 for numTriplet = 1 : size(neighboursVertices,1)
-    BW1=zeros(size(L_img));
-    BW2=zeros(size(L_img));
-    BW3=zeros(size(L_img));
-    borderImg=zeros(size(L_img));
     
-    BW1(L_img==neighboursVertices(numTriplet, 1))=1;
-    BW2(L_img==neighboursVertices(numTriplet, 2))=1;
-    BW3(L_img==neighboursVertices(numTriplet, 3))=1;
+    %Reset variables
+    borderImg = initBorderImg;
+    BW3 = initBW3;
+    BW2 = initBW2;
+    BW1 = initBW1;
+
+    imgNeighs1 = L_img==neighboursVertices(numTriplet, 1);
+    BW1(imgNeighs1)=1;
     
-    BW1_dilate=imdilate(BW1, ball);
-    BW2_dilate=imdilate(BW2, ball);
-    BW3_dilate=imdilate(BW3, ball);
+    imgNeighs2 = L_img==neighboursVertices(numTriplet, 2);
+    BW2(imgNeighs2)=1;
     
+    imgNeighs3 = L_img==neighboursVertices(numTriplet, 3);
+    BW3(imgNeighs3)=1;
+    
+    % We first calculate the perimeter of the cell to improve efficiency
+    % If the image is small, is better not to use bwperim
+    % For larger images it improves a lot the efficiency
+    bw1Perim = bwperim(BW1);
+    BW1_dilate = imdilate(bw1Perim, ball);
+    
+    bw2Perim = bwperim(BW2);
+    BW2_dilate = imdilate(bw2Perim, ball);
+    
+    bw3Perim = bwperim(BW3);
+    BW3_dilate = imdilate(bw3Perim, ball);
+
     borderImg(L_img==0) = 1;
     
-    [xPx, yPx, zPx]=findND((BW1_dilate.*BW2_dilate.*BW3_dilate.*borderImg)==1);
+    [xPx, yPx, zPx] = findND((BW1_dilate .* BW2_dilate .* BW3_dilate .* borderImg) == 1);
     
     if length(xPx)>1
         vertices{numTriplet} = round(mean([xPx, yPx, zPx]));
@@ -33,6 +57,7 @@ for numTriplet = 1 : size(neighboursVertices,1)
         vertices{numTriplet} = [xPx, yPx , zPx];
     end
 end
+
 
 
 %Correct false negative and positive
