52a8107dc7aee9ccfc698da57c3fa0391fcea15f
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-09-29 16:30:26 +0200

    improving again efficiency

--- InSilicoModels/VoronoiEllipsoid/lib/calculate_neighbours3D.m
+++ InSilicoModels/VoronoiEllipsoid/lib/calculate_neighbours3D.m
@@ -13,9 +13,8 @@ function [neighs_real] = calculate_neighbours3D(L_img)
     
     for cell = 1 : length(cells)
         BW_dilate = imdilate(imgPerim==cells(cell), ball);
-        pixels_neighs= find(BW_dilate==1);
-        neighs=unique(L_img(pixels_neighs));
-        neighs_real{cells(cell), 1}=neighs(neighs ~= 0 & neighs ~= cells(cell));
+        neighs=unique(L_img(BW_dilate));
+        neighs_real{cells(cell), 1} = neighs(neighs ~= 0 & neighs ~= cells(cell));
     end
 end
 
