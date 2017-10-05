05e2a366003d6e98ef949a74651d765fdaeae42a
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-10-05 10:52:08 +0200

    offset OUT

--- InSilicoModels/VoronoiEllipsoid/lib/getValidPixels.m
+++ InSilicoModels/VoronoiEllipsoid/lib/getValidPixels.m
@@ -1,9 +1,9 @@
 function [ validPxs, innerLayerPxs, outterLayerPxs ] = getValidPixels(pxXs, pxYs, pxZs, ellipsoidInfo, cellHeight )
 %GETVALIDPIXELS Summary of this function goes here
 %   Detailed explanation goes here
-    pxsInOutterLayer = ((((pxXs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.xOffset).^2) / (ellipsoidInfo.xRadius + cellHeight)^2) + ((((pxYs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.yOffset).^2) / (ellipsoidInfo.yRadius + cellHeight)^2) + ((((pxZs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.zOffset).^2) / (ellipsoidInfo.zRadius + cellHeight)^2);
+    pxsInOutterLayer = ((((pxXs / ellipsoidInfo.resolutionFactor)).^2) / (ellipsoidInfo.xRadius + cellHeight)^2) + ((((pxYs / ellipsoidInfo.resolutionFactor)).^2) / (ellipsoidInfo.yRadius + cellHeight)^2) + ((((pxZs / ellipsoidInfo.resolutionFactor)).^2) / (ellipsoidInfo.zRadius + cellHeight)^2);
 
-    pxsInInnerLayer = ((((pxXs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.xOffset).^2) / ellipsoidInfo.xRadius^2) + ((((pxYs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.yOffset).^2) / ellipsoidInfo.yRadius^2) + ((((pxZs / ellipsoidInfo.resolutionFactor) - ellipsoidInfo.zOffset).^2) / ellipsoidInfo.zRadius^2);
+    pxsInInnerLayer = ((((pxXs / ellipsoidInfo.resolutionFactor)).^2) / ellipsoidInfo.xRadius^2) + ((((pxYs / ellipsoidInfo.resolutionFactor)).^2) / ellipsoidInfo.yRadius^2) + ((((pxZs / ellipsoidInfo.resolutionFactor)).^2) / ellipsoidInfo.zRadius^2);
 
     validPxs = pxsInInnerLayer > 0.98 & pxsInOutterLayer < 1.02;
     innerLayerPxs = pxsInInnerLayer > 0.99 & pxsInInnerLayer < 1.01;
