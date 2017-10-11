84c1cce5b60562521a77835b609ef5bd88bcd2ca
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-10-11 11:21:12 +0200

    tracking cells init

---
+++ Gastrulation/Embryo_Sol/src/trackingCells.m
@@ -0,0 +1,9 @@
+function [ output_args ] = trackingCells( imgInitial, imgEnd )
+%TRACKINGCELLS Summary of this function goes here
+%   Detailed explanation goes here
+
+figure;
+imshow([imgInitial, imgEnd])
+
+end
+
