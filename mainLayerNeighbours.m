de02a407fc81ef4534ac37f9adaf27ed2cbabc8a
TinaForja <tinaforja93@hotmail.es>
2017-10-25 18:03:04 +0200

    Modifications to add new data

--- mainLayerNeighbours.m
+++ mainLayerNeighbours.m
@@ -1,36 +1,83 @@
-load('E:\Tina\results\LayerAnalysis\Layer_all\layerAnalysisVoronoi_10-Oct-2017.mat');
-load('E:\Tina\Epithelia3D\Zebrafish\Results\Sample2\trackingLayer2.mat');
+% load('E:\Tina\results\LayerAnalysis\Layer_all\layerAnalysisVoronoi_16-Oct-2017.mat');
+% load('E:\Tina\Epithelia3D\Zebrafish\Results\Sample2\trackingLayer2.mat');
+% 
+% [neighs_real,sides_cells]=calculate_neighbours3D(img3DLabelled);
+% save('neighbours_layer2.mat','neighs_real');
+% 
+% ratio=4;
+% [xgrid, ygrid, zgrid] = meshgrid(-ratio:ratio);
+% ball = (sqrt(xgrid.^2 + ygrid.^2 + zgrid.^2) <= ratio);
+% ids=vertcat(finalCentroid{:,1});
+% 
+% for numCell=1:size(neighs_real,2)
+%     numCell
+%     volumen=sum(sum(sum(img3DLabelled==numCell)));
+%     BW = bwperim(img3DLabelled==numCell);
+%     area=sum(sum(sum(BW)));
+%     
+%     BW_dilate=imdilate(BW ,ball);
+%     neighs=img3DLabelled(BW_dilate==1);
+%     [C,ia,ic] = unique(neighs);
+%     C=C(C~=0 & C~=numCell);
+%     neighs= neighs( neighs~=0 & neighs~=numCell);
+%     
+%     basicDates(numCell+1,1)=numCell;
+%     basicDates(numCell+1,2)=size(C,1);
+%     basicDates(numCell+1,3)=area;
+%     basicDates(numCell+1,4)=volumen;
+%     
+%     for numNeighs=1:size(C,1)           
+%         areaShared=sum(neighs==C(numNeighs));
+%         percentage=areaShared/size(neighs,1);
+%         
+%         neigh_real{1,numCell}(numNeighs,1)=numCell;
+%         neigh_real{1,numCell}(numNeighs,2)=neighs_real{1,numCell}(numNeighs,1);
+%         neigh_real{1,numCell}(numNeighs,3)=areaShared; 
+%         neigh_real{1,numCell}(numNeighs,4)=percentage;
+%         Ind = find(neighs_real{numCell}(numNeighs)==ids);
+%         neigh_real{1,numCell}(numNeighs,5)=finalCentroid{Ind(1,1), 3};
+%     end
+%     
+%     if isempty(neighs_real{1,numCell})==0
+%         dilatedArea(1,numCell)=sum(neigh_real{1,numCell}(:,3)); 
+%         
+%         for numNeighs=1:size(neighs_real{1,numCell},1)
+%             neigh_real{1,numCell}(numNeighs,6)= ((neigh_real{1,numCell}(numNeighs,3))*area)/ dilatedArea(1,numCell);
+%         end
+%     end
+% end
+% 
+% clear neighs_real
+% neighs_real=neigh_real;
+% 
+% save('neighbours_layer2.mat','neighs_real', 'basicDates');
 
-[neighs_real,sides_cells]=calculate_neighbours3D(img3DLabelled);
 
-ratio=4;
-[xgrid, ygrid, zgrid] = meshgrid(-ratio:ratio);
-ball = (sqrt(xgrid.^2 + ygrid.^2 + zgrid.^2) <= ratio);
-ids=vertcat(finalCentroid{:,1});
-
-for numCell=1:size(neighs_real,2)
-    
-    BW = bwperim(img3DLabelled==numCell);
-    BW_dilate=imdilate( BW ,ball);
-    neighs=img3DLabelled(BW_dilate==1);
-    [C,ia,ic] = unique(neighs);
-    C=C(C~=0 & C~=numCell);
-    neighs= neighs( neighs ~=0 &  neighs~=numCell);
-    
-    for numNeighs=1:size(C,1)    
-        percentage=sum(neighs==C(numNeighs))/size(neighs,1);    
-        neighs_real{1,numCell}(numNeighs,2)=percentage; 
-
-        Ind = find(neighs_real{numCell}(numNeighs)==ids);
-        neighs_real{1,numCell}(numNeighs,3)=finalCentroid{Ind(1,1), 3};
-        
+acum=2;
+for numCell=1:size(neighs_real,2)  
+    for numNeigh=1:size(neighs_real{1,numCell})
+            neigh_real(acum,:)=neighs_real{1,numCell}(numNeigh,:);
+            acum=acum+1;
     end
-    
 end
-save('neighbours_layer2.mat','neighs_real');
 
-xlswrite('neighbours_layer2.xlsx','neighs_real');
+% neigh_real(1,1)='ID';
+% neigh_real(1,2)='neighbours';
+% neigh_real(1,3)='sharedDilateArea';
+% neigh_real(1,4)='%Shared';
+% neigh_real(1,5)='Layers';
+% neigh_real(1,6)='sharedRealArea';
+% 
+% basicDates(1,1)='ID';
+% basicDates(1,2)='nºNeighbours';
+% basicDates(1,3)='area';
+% basicDates(1,4)='volumen';
+
+clear neighs_real
+neighs_real=neigh_real;
 
 
-
+save('neighbours_layer2.mat','neighs_real', 'basicDates');
+xlswrite('neighbours_layer2.xlsx',basicDates,'basicDates');
+xlswrite('neighbours_layer2.xlsx',neighs_real,'neighs_real');
 
