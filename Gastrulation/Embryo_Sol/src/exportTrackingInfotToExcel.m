4a2ad2b70b13c2bfab9439cf54681b0a8f419f3c
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-10-27 17:36:31 +0200

    adding comments and mean and std

--- Gastrulation/Embryo_Sol/src/exportTrackingInfotToExcel.m
+++ Gastrulation/Embryo_Sol/src/exportTrackingInfotToExcel.m
@@ -15,6 +15,15 @@ for numFile = 1:size(trackingFiles, 1)
     imwrite(ismember(imgEndNewLabels, trackingInfo.newLabel(logical(trackingInfo.validCell))) .* imgEndNewLabels, colorcube(size(trackingInfo, 1)), strcat(actualFile(1:end-4), '_imgEnd_ValidCells.bmp'));
     
     %Export excel
-    xlsTable = vertcat(trackingInfo.Properties.VariableNames(6:12) , table2cell(trackingInfo(:, 6:12)));
-    xlswrite(strcat(resultsPath, 'noFolds_Cells_', date), xlsTable, actualFileSplitted{4});
+    trackingInfo.neighboursInitial = cellfun(@(x) size(x, 1), table2cell(trackingInfo(:, 7)));
+    trackingInfo.neighboursEnd = cellfun(@(x) size(x, 1), table2cell(trackingInfo(:, 8)));
+    %Order:
+    % - Header
+    % - Number of neighbours, perimeter and area of every valid cells at
+    % initial and final frame
+    % - Mean of every column
+    % - STD of every column
+    xlsTable = vertcat(trackingInfo.Properties.VariableNames(7:12) , table2cell(trackingInfo(logical(trackingInfo.validCell), 7:12)), mat2cell(mean(table2array(trackingInfo(logical(trackingInfo.validCell), 7:12))), 1, ones(6, 1)), mat2cell(std(table2array(trackingInfo(logical(trackingInfo.validCell), 7:12))), 1, ones(6, 1)));
+    
+    xlswrite(strcat(resultsPath, 'noFolds_Cells_', date), xlsTable, actualFileSplitted{4}, 'C5');
 end
