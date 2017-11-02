ad4f4807f115651d9edb369deac7707dabaefb34
Pablo Vicente Munuera <pablovm1990@gmail.com>
2017-11-02 11:21:28 +0100

    adding more info

--- Gastrulation/Embryo_Sol/src/exportTrackingInfotToExcel.m
+++ Gastrulation/Embryo_Sol/src/exportTrackingInfotToExcel.m
@@ -11,12 +11,33 @@ for numFile = 1:size(trackingFiles, 1)
     actualFileSplitted = strsplit(actualFile, '\');
     load(actualFile);
     %Write imgs with valid cells
-    imwrite(ismember(imgInitialNewLabels, trackingInfo.newLabel(logical(trackingInfo.validCell))) .* imgInitialNewLabels, colorcube(size(trackingInfo, 1)), strcat(actualFile(1:end-4), '_imgInitial_ValidCells.bmp'));
-    imwrite(ismember(imgEndNewLabels, trackingInfo.newLabel(logical(trackingInfo.validCell))) .* imgEndNewLabels, colorcube(size(trackingInfo, 1)), strcat(actualFile(1:end-4), '_imgEnd_ValidCells.bmp'));
+    imwrite(ismember(imgInitialNewLabels, trackingInfo.newLabel(logical(trackingInfo.validCell))) .* imgInitialNewLabels + 1, colorcube(size(trackingInfo, 1)+2), strcat(actualFile(1:end-4), '_imgInitial_ValidCells.bmp'));
+    imwrite(ismember(imgEndNewLabels, trackingInfo.newLabel(logical(trackingInfo.validCell))) .* imgEndNewLabels + 1, colorcube(size(trackingInfo, 1)+2), strcat(actualFile(1:end-4), '_imgEnd_ValidCells.bmp'));
+    
+    imwrite(imgInitialNewLabels+1, colorcube(size(trackingInfo, 1)+2), strcat(actualFile(1:end-4), '_imgInitial.bmp'));
+    imwrite(imgEndNewLabels+1, colorcube(size(trackingInfo, 1)+2), strcat(actualFile(1:end-4), '_imgEnd.bmp'));
     
     %Export excel
+    
+    trackingInfo.neighboursInitial = calculateNeighbours(imgInitialNewLabels)';
+    trackingInfo.neighboursEnd = calculateNeighbours(imgEndNewLabels)';
+    
+    trackingInfo.AreaInitial = struct2array(regionprops(imgInitialNewLabels, 'area'))';
+    trackingInfo.AreaEnd = struct2array(regionprops(imgEndNewLabels, 'area'))';
+    
+    trackingInfo.PerimeterInitial = struct2array(regionprops(imgInitialNewLabels, 'perimeter'))';
+    trackingInfo.PerimeterEnd = struct2array(regionprops(imgEndNewLabels, 'perimeter'))';
+    
+    trackingInfo.regionPropsInitial = regionprops(imgInitialNewLabels, 'all');
+    trackingInfo.regionPropsEnd = regionprops(imgEndNewLabels, 'all');
+    
     trackingInfo.neighboursInitial = cellfun(@(x) size(x, 1), table2cell(trackingInfo(:, 7)));
     trackingInfo.neighboursEnd = cellfun(@(x) size(x, 1), table2cell(trackingInfo(:, 8)));
+    
+%     imshow(ismember(imgInitialNewLabels, trackingInfo.newLabel(trackingInfo.neighboursInitial ~= trackingInfo.neighboursEnd)) .* imgInitialNewLabels);
+%     
+%     percentageOfScutoids = sum(trackingInfo.neighboursInitial(logical(trackingInfo.validCell)) ~= trackingInfo.neighboursEnd(logical(trackingInfo.validCell))) / sum(trackingInfo.validCell)
+%     percentageOfScutoids = sum(trackingInfo.neighboursInitial ~= trackingInfo.neighboursEnd) / size(trackingInfo, 1)
     %Order:
     % - Header
     % - Number of neighbours, perimeter and area of every valid cells at
