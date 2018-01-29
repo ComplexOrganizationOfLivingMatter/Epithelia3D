c45029e3b254f5110a7858b9e6e229469aecd441
Pablo Vicente Munuera <pvicente1@us.es>
2018-01-29 12:49:32 +0100

    improvement not to keep images, just ids of cell dilated

--- InSilicoModels/VoronoiEllipsoid/lib/calculate_neighbours3D.m
+++ InSilicoModels/VoronoiEllipsoid/lib/calculate_neighbours3D.m
@@ -9,11 +9,14 @@ function [ellipsoidInfo] = calculate_neighbours3D(L_img, ellipsoidInfo)
     [xgrid, ygrid, zgrid] = meshgrid(-ratio:ratio); 
     ball = (sqrt(xgrid.^2 + ygrid.^2 + zgrid.^2) <= ratio);
     
-    imgPerim = bwperim(L_img) .* L_img;
+    imgPerim = uint16(bwperim(L_img)) .* L_img;
     
     for cell = 1 : length(cells)
+        %c = gpuArray(imgPerim==cells(cell)); %Maximum variable size allowed on the device is exceeded.
         BW_dilate = imdilate(imgPerim==cells(cell), ball);
-        ellipsoidInfo.cellDilated{cell} = BW_dilate;
+        [x, y, z] = findND(BW_dilate);
+        ellipsoidInfo.cellDilated{cell} = [uint16(x), uint16(y), uint16(z)];
+        
         neighs=unique(L_img(BW_dilate));
         neighs_real{cells(cell), 1} = neighs(neighs ~= 0 & neighs ~= cells(cell));
     end
