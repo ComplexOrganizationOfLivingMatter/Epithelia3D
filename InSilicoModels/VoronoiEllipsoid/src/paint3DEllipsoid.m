8c908184b8dfaa49690dae495d1d5bf4accfaf43
Pablo Vicente Munuera <pvicente1@us.es>
2018-02-08 16:40:01 +0100

    good painting of figure

--- InSilicoModels/VoronoiEllipsoid/src/paint3DEllipsoid.m
+++ InSilicoModels/VoronoiEllipsoid/src/paint3DEllipsoid.m
@@ -2,16 +2,29 @@ function [ ] = paint3DEllipsoid(centroids, img3DLabelled )
 %PAINT3DELLIPSOID Summary of this function goes here
 %   Detailed explanation goes here
     colours = colorcube(size(centroids, 1));
+    colours = colours(randperm(size(centroids, 1)), :);
     figure;
-    %img3DLabelledPerim = uint16(bwperim(img3DLabelled)) .* img3DLabelled;
+    img3DLabelledPerim = uint16(bwperim(img3DLabelled)) .* img3DLabelled;
     disp('Building figure');
     for numSeed = 1:size(centroids, 1)
+        numSeed
         % Painting each cell
-        [x, y, z] = findND(img3DLabelled == numSeed);
-        cellFigure = alphaShape(x, y, z, 10);
+        [x, y, z] = findND(img3DLabelledPerim == numSeed);
+        cellFigure = alphaShape(x, y, z, 1000);
         plot(cellFigure, 'FaceColor', colours(numSeed, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
         hold on;
     end
 
+    axis equal
+    camlight left;
+    camlight right;
+    lighting flat
+    material dull
+    
+    newFig = gca;
+    newFig.XGrid = 'off';
+    newFig.YGrid = 'off';
+    newFig.ZGrid = 'off';
+    set(get(0,'children'),'Color','w')
 end
 
