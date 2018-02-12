620f12aa4120be2424fa623bed259fc5ec837c41
Pablo Vicente Munuera <pvicente1@us.es>
2018-02-12 11:58:48 +0100

    unnecessaary codeeeee

--- InSilicoModels/VoronoiSphere/paintRegion.m
+++
@@ -1,8 +0,0 @@
-function [ ] = paintRegion(x, y, z, regionPoints )
-%PAINTREGION Summary of this function goes here
-%   Detailed explanation goes here
-    tri = delaunay(x(regionPoints), y (regionPoints));
-    trisurf(tri, x(regionPoints), y (regionPoints), z (regionPoints));
-    hold on;
-end
-
