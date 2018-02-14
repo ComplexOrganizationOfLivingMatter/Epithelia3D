542db9279ee7559b0c30f9331cbc9112c5ab487b
TinaForja <tinaforja93@hotmail.es>
2018-02-14 12:39:46 +0100

    creating polar histograms and pipelines

---
+++ Graphics/polarHistogramWTsalivaryGland/createPolarHistogram.m
@@ -0,0 +1,23 @@
+function [ ] = createPolarHistogram( inputAngles, colours, titleFig )
+%CREATEPOLARHISTOGRAM Summary of this function goes here
+%   Detailed explanation goes here
+
+figure;
+polarhistogram(deg2rad(inputAngles),'BinWidth',deg2rad(6),'Normalization','probability','FaceColor', colours,'FaceAlpha',1,'LineWidth',1)
+
+
+ax=gca;
+ax.ThetaZeroLocation='right';
+% %ax.ThetaDir='clockwise';
+ax.ThetaLimMode='manual';
+ax.ThetaLim=[0,90];
+ax.RLimMode='manual';
+ax.GridAlpha=1;
+ax.LineWidth=1;
+ax.FontSize=30;
+ax.RLim=[0,0.35];
+ax.FontName='Helvetica-Narrow';
+title(titleFig, 'FontSize', 16);
+
+end
+
