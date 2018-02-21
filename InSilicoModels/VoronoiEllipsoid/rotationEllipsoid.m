4b3a81906586352411c7afd162e986db52f85b39
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-02-21 17:02:02 +0100

    rotation of ellipsoid working!

--- InSilicoModels/VoronoiEllipsoid/rotationEllipsoid.m
+++ InSilicoModels/VoronoiEllipsoid/rotationEllipsoid.m
@@ -2,7 +2,7 @@ addpath(genpath('lib'))
 
 filePathStage4='results\Stage 4\';
 
-%load([filePathStage4 'random_1_10\random_1\ellipsoid_x12642_y1_z1_cellHeight02216.mat'],'ellipsoidInfo','initialEllipsoid')
+load([filePathStage4 'random_1\ellipsoid_x12642_y1_z1_cellHeight02216.mat'],'ellipsoidInfo','initialEllipsoid')
 
 round([initialEllipsoid.xCenter,initialEllipsoid.yCenter,initialEllipsoid.zCenter]*initialEllipsoid.resolutionEllipse);
 
@@ -14,28 +14,30 @@ deg=45;
 newPixels = {};
 figure;
 colours = colorcube(size(initialEllipsoid.cellArea, 1));
-%colours = colours(randperm(size(initialEllipsoid.cellArea, 1)), :);
-%[R, t] = AxelRot(deg, u, x0); 
+
+
 for numCell = 1:size(initialEllipsoid.cellArea, 1)
     [x, y, z] = findND(initialEllipsoid.img3DLayer == numCell);
     XYZOld = horzcat(x, y, z)';
     [XYZnew, R, t] = AxelRot(XYZOld, deg, u, x0);
     XYZnew = XYZnew';
     newPixels(numCell) = {XYZnew};
-    cellFigure = alphaShape(XYZnew(:, 1), XYZnew(:, 2), XYZnew(:, 3), 500);
-    plot(cellFigure, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
-    hold on;
+%     cellFigure = alphaShape(XYZnew(:, 1), XYZnew(:, 2), XYZnew(:, 3), 500);
+%     plot(cellFigure, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+%     hold on;
 end
-% M=AxelRot(deg,u,x0);
-%T=affine3d([[R;0 0 0],[0;0;0;1]]);
-%ellipsoidRotated=imwarp(initialEllipsoid.img3DLayer,T);
+
+
+
 
 allPixels = vertcat(newPixels{:});
-newImage = zeros(max(round(allPixels))+1, 'uint16');
+newImage = zeros(size(initialEllipsoid.img3DLayer), 'uint16');
 for numCell = 1:size(initialEllipsoid.cellArea, 1)
     actualPixels = round(newPixels{numCell});
     for numPixel = 1:size(actualPixels, 1)
         newImage(actualPixels(numPixel, 1), actualPixels(numPixel, 2), actualPixels(numPixel, 3)) = numCell;
     end
 end
+
+%save rotated image
 
