cb0481a6d639bd5334000730b57a61f32a3128e7
Pablo Vicente Munuera <pvicente1@us.es>
2018-02-21 14:52:07 +0100

    obtaining vertices and extrapolating them

--- InSilicoModels/VoronoiEllipsoid/pipelineFrustaModel.m
+++ InSilicoModels/VoronoiEllipsoid/pipelineFrustaModel.m
@@ -1,4 +1,11 @@
 clear
-
+addpath(genpath('src'));
+addpath(genpath('lib'));
 load('D:\Pablo\Epithelia3D\InSilicoModels\VoronoiEllipsoid\results\Stage 4\random_1_10\random_1\ellipsoid_x12642_y1_z1_cellHeight02216.mat')
 
+initialEllipsoid = getVertices3D( initialEllipsoid.img3DLayer, initialEllipsoid.neighbourhood, initialEllipsoid );
+
+initialEllipsoid.resolutionFactor = ellipsoidInfo.resolutionFactor;
+initialEllipsoid.resolutionEllipse = 2000;
+
+[ finalCentroidsAugmented] = getAugmentedCentroids( initialEllipsoid, vertcat(initialEllipsoid.verticesPerCell{:}), ellipsoidInfo.cellHeight);
