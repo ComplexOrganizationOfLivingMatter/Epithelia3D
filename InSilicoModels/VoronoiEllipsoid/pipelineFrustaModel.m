11aedfb84b9062bbe8513a64a9eed2ec8f1a92bd
Pablo Vicente Munuera <pvicente1@us.es>
2018-02-22 09:57:27 +0100

    with voxelise we got the pixels of each cell and now can paint the new all frusta image

--- InSilicoModels/VoronoiEllipsoid/pipelineFrustaModel.m
+++ InSilicoModels/VoronoiEllipsoid/pipelineFrustaModel.m
@@ -6,17 +6,54 @@ load('D:\Pablo\Epithelia3D\InSilicoModels\VoronoiEllipsoid\results\Stage 4\rando
 initialEllipsoid = getVertices3D( initialEllipsoid.img3DLayer, initialEllipsoid.neighbourhood, initialEllipsoid );
 
 initialEllipsoid.resolutionFactor = ellipsoidInfo.resolutionFactor;
-initialEllipsoid.resolutionEllipse = 2000;
+initialEllipsoid.resolutionEllipse = 1000;
 
 [ finalCentroidsAugmented] = getAugmentedCentroids( initialEllipsoid, vertcat(initialEllipsoid.verticesPerCell{:}), ellipsoidInfo.cellHeight);
 
-allFrustaImage = size(ellipsoidInfo.img3DLayer);
+allFrustaImage = zeros(size(ellipsoidInfo.img3DLayer), 'uint16');
+
+
+colours = colorcube(size(centroids, 1));
+figure;
 
 for numCell = 1:size(initialEllipsoid.centroids, 1)
-    verticesOfActualCell = initialEllipsoid.verticesPerCell(any(ismember(initialEllipsoid.verticesConnectCells, numCell), 2), :);
-    verticesOfActualCell = vertcat(verticesOfActualCell{:});
-    [k,v] = boundary(verticesOfActualCell(:, 1), verticesOfActualCell(:, 2), verticesOfActualCell(:, 3))
-    vertex1
-    vertex2
-    allFrustaImage = Drawline3D(allFrustaImage, vertex1(1), vertex1(2), vertex1(3), vertex2(1), vertex2(2), vertex2(3), 0);
+    numCell
+    indicesVerticesOfActualCell = find(any(ismember(initialEllipsoid.verticesConnectCells, numCell), 2));
+    
+    verticesConnectingCellsOfActualCell = initialEllipsoid.verticesConnectCells(indicesVerticesOfActualCell, :);
+    verticesOfActualCell = cell2mat(initialEllipsoid.verticesPerCell(indicesVerticesOfActualCell, :));
+    
+    KVert = convhulln(verticesOfActualCell);
+    
+    fv.faces = KVert;
+    fv.vertices = verticesOfActualCell;
+    
+    [outputGrid,gridCOx,gridCOy,gridCOz] = VOXELISE(max(verticesOfActualCell(:, 1)), max(verticesOfActualCell(:, 2)), max(verticesOfActualCell(:, 3)), fv);
+    
+    [x, y, z] = findND(outputGrid == 1);
+    
+    realPixels = unique(round(vertcat(gridCOx(x), gridCOy(y), gridCOz(z)))', 'rows');
+    
+    for numPixel = 1:size(realPixels, 1)
+        allFrustaImage(realPixels(numPixel, 1), realPixels(numPixel, 2), realPixels(numPixel, 3)) = numCell;
+    end
+    
+    cellFigure = alphaShape(realPixels(numPixel, 1), realPixels(numPixel, 2), realPixels(numPixel, 3), 500);
+    plot(cellFigure, 'FaceColor', colours(numSeed, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+    hold on;
+    
+%     figure;
+%     boundaryShaped = alphaShape(realPixels(:, 1), realPixels(:, 2), realPixels(:, 3));
+%     plot(boundaryShaped);
+    
+%    verticesConnectingCellsOfActualCell = mat2cell(verticesConnectingCellsOfActualCell, ones(size(verticesConnectingCellsOfActualCell, 1), 1));
+    
+%    verticesConnectingCellsOfActualCell = cell2mat(cellfun(@(x) x(x ~= numCell), verticesConnectingCellsOfActualCell, 'UniformOutput', false));
+%     for numVertex = 1:size(verticesConnectingCellsOfActualCell, 1)
+%         vertex1 = double(initialEllipsoid.verticesPerCell{verticesConnectingCellsOfActualCell(numVertex, 1)});
+%         vertex2 = double(initialEllipsoid.verticesPerCell{verticesConnectingCellsOfActualCell(numVertex, 2)});
+%         allFrustaImage = Drawline3D(allFrustaImage, vertex1(1), vertex1(2), vertex1(3), vertex2(1), vertex2(2), vertex2(3), 0);
+%     end
 end
+
+
