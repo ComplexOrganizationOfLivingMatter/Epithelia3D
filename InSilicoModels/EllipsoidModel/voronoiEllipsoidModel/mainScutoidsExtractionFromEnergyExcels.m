d44cbcddda56c7358987b395ab69785b51cb2efa
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-03-06 12:50:08 +0100

    including sphere

--- InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/mainScutoidsExtractionFromEnergyExcels.m
+++ InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/mainScutoidsExtractionFromEnergyExcels.m
@@ -19,17 +19,23 @@ globeNoTransition2Path= 'results\Globe\energy\energyNoTransitionEdgesNonPreserve
 rugbyNoTransition05Path= 'results\Rugby\energy\energyNoTransitionEdgesNonPreservedMotifs_cellHeight05_05-Mar-2018.xls';
 rugbyNoTransition1Path= 'results\Rugby\energy\energyNoTransitionEdgesNonPreservedMotifs_cellHeight1_05-Mar-2018.xls';
 rugbyNoTransition2Path= 'results\Rugby\energy\energyNoTransitionEdgesNonPreservedMotifs_cellHeight2_05-Mar-2018.xls';
-% sphereNoTransitionPath= ;
+sphereNoTransition05Path= 'results\Sphere\energy\energyNoTransitionEdges_cellHeight05_02-Mar-2018.xls';
+sphereNoTransition1Path= 'results\Sphere\energy\energyNoTransitionEdges_cellHeight1_02-Mar-2018.xls';
+sphereNoTransition2Path= 'results\Sphere\energy\energyNoTransitionEdges_cellHeight2_02-Mar-2018.xls';
+
 
 transitionPaths={stage8TransitionPath,stage4TransitionPath,globeTransition05Path,globeTransition1Path,...
-    globeTransition2Path,rugbyTransition05Path,rugbyTransition1Path,rugbyTransition2Path};
+    globeTransition2Path,rugbyTransition05Path,rugbyTransition1Path,rugbyTransition2Path,...
+    sphereNoTransition05Path,sphereNoTransition1Path,sphereNoTransition2Path};
 noTransitionPaths={stage8NoTransitionPath,stage4NoTransitionPath,globeNoTransition05Path,...
-    globeNoTransition1Path,globeNoTransition2Path,rugbyNoTransition05Path,rugbyNoTransition1Path,rugbyNoTransition2Path};
+    globeNoTransition1Path,globeNoTransition2Path,rugbyNoTransition05Path,...
+    rugbyNoTransition1Path,rugbyNoTransition2Path,...
+    sphereNoTransition05Path,sphereNoTransition1Path,sphereNoTransition2Path};
 
 meanScutoids=zeros(length(transitionPaths),1);
 stdScutoids=zeros(length(transitionPaths),1);
 meanTotalCells=zeros(length(transitionPaths),1);
 stdTotalCells=zeros(length(transitionPaths),1);
-for i=1:length(transitionPaths)
+for i=length(transitionPaths)-2:length(transitionPaths)%1:length(transitionPaths)
     [meanScutoids(i),stdScutoids(i),meanTotalCells(i),stdTotalCells(i)]=extractionOfScutoidsFromExcel(noTransitionPaths{i}, transitionPaths{i});
 end
