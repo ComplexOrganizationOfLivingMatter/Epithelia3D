e37d12202aa93e8af522a5e8d3f622dfabf703e9
Pablo Vicente Munuera <pvicente1@us.es>
2018-03-06 10:40:11 +0100

    checking if a file has already created

--- InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/pipelineFrustaModel.m
+++ InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/pipelineFrustaModel.m
@@ -2,11 +2,11 @@ clear
 addpath(genpath('src'));
 addpath(genpath('lib'));
 
-resultFiles = getAllFiles('results/');
+resultFiles = getAllFiles('results/VoronoiModel/');
 ellipsoidFiles = resultFiles(cellfun(@(x) isempty(strfind(x, '.mat')) == 0 & isempty(strfind(x, 'ellipsoid_x')) == 0, resultFiles));
 
-stage8_ValidRandom = [1:5 7:8 10 13 15:17 19:20 22 25 26 29 30];
-stage4_ValidRandom = [7 15 19 20 25 33 36 39 46 61:64 69 80 81 96 102 106 119 124 135 145 150 154 162 170:172 180];
+% stage8_ValidRandom = [1:5 7:8 10 13 15:17 19:20 22 25 26 29 30];
+% stage4_ValidRandom = [7 15 19 20 25 33 36 39 46 61:64 69 80 81 96 102 106 119 124 135 145 150 154 162 170:172 180];
 
 parfor numEllipsoid = 1:size(ellipsoidFiles, 1)
     validRandom = 0;
@@ -17,14 +17,15 @@ parfor numEllipsoid = 1:size(ellipsoidFiles, 1)
     numRandom = str2double(numRandom{end});
     
     if isempty(strfind(inputFile, 'Sphere'))
-        if ismember(nameRandom, stage4_ValidRandom) && isempty(strfind(inputFile, 'Stage4'))
-            validRandom = 1;
-        elseif ismember(nameRandom, stage8_ValidRandom) && isempty(strfind(inputFile, 'Stage8'))
-            validRandom = 1;
-        end
+%         if ismember(nameRandom, stage4_ValidRandom) && isempty(strfind(inputFile, 'Stage4'))
+%             validRandom = 1;
+%         elseif ismember(nameRandom, stage8_ValidRandom) && isempty(strfind(inputFile, 'Stage8'))
+%             validRandom = 1;
+%         end
+        validRandom = 1;
         
         if validRandom
-            disp(['NumEllipsoid: ' num2str(numEllipsoid)]);
+            disp(nameRandom);
             createFrustaModelFromApicalImage(inputFile)
         end
     end
--- InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/src/createFrustaModelFromApicalImage.m
+++ InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/src/createFrustaModelFromApicalImage.m
@@ -1,68 +1,75 @@
 function [ ] = createFrustaModelFromApicalImage(inputFile )
 %CREATEFRUSTAMODELFROMAPICALIMAGE Summary of this function goes here
 %   Detailed explanation goes here
-    load(inputFile);
-
-    disp('Getting vertices 3D');
-    %Get vertices info on the apical layer
-    initialEllipsoid = getVertices3D( initialEllipsoid.img3DLayer, initialEllipsoid.neighbourhood, initialEllipsoid );
-
-    initialEllipsoid.resolutionFactor = ellipsoidInfo.resolutionFactor;
-    initialEllipsoid.resolutionEllipse = 500;
-
-    disp('Getting outter layer vertices');
-    %New vertices on basal
-    [ finalCentroidsAugmented] = getAugmentedCentroids( initialEllipsoid, vertcat(initialEllipsoid.verticesPerCell{:}), ellipsoidInfo.cellHeight);
-
-    allFrustaImage = initialEllipsoid.img3DLayer;
-
-%     colours = colorcube(size(initialEllipsoid.centroids, 1));
-%     outputFigure = figure;
-
-    % Now we have to get the actual pixels from the faces of the cells
-    for numCell = 1:size(initialEllipsoid.centroids, 1)
-        %disp(['NumCell: ' num2str(numCell)]);
-        indicesVerticesOfActualCell = any(ismember(initialEllipsoid.verticesConnectCells, numCell), 2);
-
-        verticesOfActualCell = round(finalCentroidsAugmented(indicesVerticesOfActualCell, :));
-
-        %Get the faces of the cell
-        KVert = convhulln(verticesOfActualCell);
-
-        %Create the appropiate structure for voselise function
-        cellStructure.faces = KVert;
-        cellStructure.vertices = verticesOfActualCell;
-
-        %Get the pixels of the cell
-        [outputGrid, gridCOx, gridCOy, gridCOz] = VOXELISE(max(verticesOfActualCell(:, 1)), max(verticesOfActualCell(:, 2)), max(verticesOfActualCell(:, 3)), cellStructure);
-
-        gridCOx = round(gridCOx);
-        gridCOy = round(gridCOy);
-        gridCOz = round(gridCOz);
-        
-        [x, y, z] = findND(outputGrid == 1);
-        realPixels = round(vertcat(gridCOx(x), gridCOy(y), gridCOz(z)))';
-        for numPixel = 1:size(realPixels, 1)
-            allFrustaImage(realPixels(numPixel, 1), realPixels(numPixel, 2), realPixels(numPixel, 3)) = numCell;
-        end
-
-        %Paint the cell to check if everything fits
-%         cellFigure = alphaShape(realPixels(:, 1), realPixels(:, 2), realPixels(:, 3), 500);
-%         plot(cellFigure, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
-%         hold on;
-    end
-
-    %Save necessary info
+    
     outputFile = strrep(inputFile, 'VoronoiModel', 'FrustaModel');
     outputFileSplitted = strsplit(outputFile, '\');
     outputFileDirectory = strjoin(outputFileSplitted(1:end-1), '\');
     mkdir(outputFileDirectory)
-
-    %savefig(strcat(outputFileDirectory, '\frustaEllipsoidModel_OutterLayer_', date, '.fig'));
-
-    save(strcat(outputFileDirectory, '\frustaEllipsoidModel_', date, '.mat'), 'allFrustaImage');
-
-    %close outputFigure
+    
+    outputFilesChecking = getAllFiles(outputFileDirectory);
+    
+    if isempty(outputFilesChecking)
+        
+        load(inputFile);
+        
+        disp('Getting vertices 3D');
+        %Get vertices info on the apical layer
+        initialEllipsoid = getVertices3D( initialEllipsoid.img3DLayer, initialEllipsoid.neighbourhood, initialEllipsoid );
+        
+        initialEllipsoid.resolutionFactor = ellipsoidInfo.resolutionFactor;
+        initialEllipsoid.resolutionEllipse = 500;
+        
+        disp('Getting outter layer vertices');
+        %New vertices on basal
+        [ finalCentroidsAugmented] = getAugmentedCentroids( initialEllipsoid, vertcat(initialEllipsoid.verticesPerCell{:}), ellipsoidInfo.cellHeight);
+        
+        allFrustaImage = initialEllipsoid.img3DLayer;
+        
+        %     colours = colorcube(size(initialEllipsoid.centroids, 1));
+        %     outputFigure = figure;
+        
+        % Now we have to get the actual pixels from the faces of the cells
+        for numCell = 1:size(initialEllipsoid.centroids, 1)
+            %disp(['NumCell: ' num2str(numCell)]);
+            indicesVerticesOfActualCell = any(ismember(initialEllipsoid.verticesConnectCells, numCell), 2);
+            
+            verticesOfActualCell = round(finalCentroidsAugmented(indicesVerticesOfActualCell, :));
+            
+            %Get the faces of the cell
+            KVert = convhulln(verticesOfActualCell);
+            
+            %Create the appropiate structure for voselise function
+            cellStructure.faces = KVert;
+            cellStructure.vertices = verticesOfActualCell;
+            
+            %Get the pixels of the cell
+            [outputGrid, gridCOx, gridCOy, gridCOz] = VOXELISE(max(verticesOfActualCell(:, 1)), max(verticesOfActualCell(:, 2)), max(verticesOfActualCell(:, 3)), cellStructure);
+            
+            gridCOx = round(gridCOx);
+            gridCOy = round(gridCOy);
+            gridCOz = round(gridCOz);
+            
+            [x, y, z] = findND(outputGrid == 1);
+            realPixels = round(vertcat(gridCOx(x), gridCOy(y), gridCOz(z)))';
+            for numPixel = 1:size(realPixels, 1)
+                allFrustaImage(realPixels(numPixel, 1), realPixels(numPixel, 2), realPixels(numPixel, 3)) = numCell;
+            end
+            
+            %Paint the cell to check if everything fits
+            %         cellFigure = alphaShape(realPixels(:, 1), realPixels(:, 2), realPixels(:, 3), 500);
+            %         plot(cellFigure, 'FaceColor', colours(numCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+            %         hold on;
+        end
+        
+        %Save necessary info
+        
+        %savefig(strcat(outputFileDirectory, '\frustaEllipsoidModel_OutterLayer_', date, '.fig'));
+        
+        save(strcat(outputFileDirectory, '\frustaEllipsoidModel_', date, '.mat'), 'allFrustaImage', '-v7.3');
+        
+        %close outputFigure
+    end
 
 end
 
