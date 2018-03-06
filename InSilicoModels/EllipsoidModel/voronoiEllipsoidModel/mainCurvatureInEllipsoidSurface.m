09b7ead7e40debdf3b5ee5950991b90ef756dca6
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-03-06 11:58:44 +0100

    curvatures from ellipsoids radius and 2/3 xRadius ROI

--- InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/mainCurvatureInEllipsoidSurface.m
+++ InSilicoModels/EllipsoidModel/voronoiEllipsoidModel/mainCurvatureInEllipsoidSurface.m
@@ -11,9 +11,7 @@ for i = 1 : length(filePaths)
         
     nameFile=dir([filePath '\random_1\ellipsoid*']);
 
-    
-    
-    for nHeight=1:length(nameFile)
+    for nHeight = 1 : length(nameFile)
 
         load([filePath '\random_1\' nameFile(nHeight).name],'ellipsoidInfo','initialEllipsoid')
         
@@ -33,18 +31,21 @@ for i = 1 : length(filePaths)
             end
                 
             %getting pixels in surface of ellipsoid
-            [x,y,z]=findND(ellipsoidMask.img3DLayer>0);
+%             [x,y,z]=findND(ellipsoidMask.img3DLayer>0);
 
-            xCenterEllipsoid=round(ellipsoidMask.xCenter*resolutionFactor);
+%             xCenterEllipsoid=round(ellipsoidMask.xCenter*resolutionFactor);
+            
+            %defining cell height depending if ellipsoids are oldest (radius is the apical radius) or
+            %newest (cellHeight into the radius)
+            if length(nameFile)>1
+                cellHeight=0;
+            else
+                cellHeight=ellipsoidMask.cellHeight;
+            end
 
             %defining pixels inside chosen ROI for energy
-            limitXDown=xCenterEllipsoid-xRadiusROI*resolutionFactor*(ellipsoidMask.xRadius+ellipsoidMask.cellHeight);
-            limitXUp=xCenterEllipsoid+xRadiusROI*resolutionFactor*(ellipsoidMask.xRadius+ellipsoidMask.cellHeight);
-            roiEnergyCoord=logical((x>limitXDown).*(x<limitXUp));
-
-            xROI=x(roiEnergyCoord);
-            yROI=y(roiEnergyCoord);
-            zROI=z(roiEnergyCoord);
+            limitXRoi=xRadiusROI*(ellipsoidMask.xRadius+cellHeight);
+            
             
             
 %             mask3D=zeros(size(ellipsoidMask.img3DLayer));
@@ -65,28 +66,55 @@ for i = 1 : length(filePaths)
 %                     hold on;
 %                 end
 %             end
-            
-            %reallocation of coordinates to the origin (0,0,0)
-            x=(xROI/resolutionFactor-ellipsoidMask.xCenter);
-            y=(yROI/resolutionFactor-ellipsoidMask.yCenter);
-            z=(zROI/resolutionFactor-ellipsoidMask.zCenter);   
+%             
+%             %reallocation of coordinates to the origin (0,0,0)
+%             x=(xROI/resolutionFactor-ellipsoidMask.xCenter);
+%             y=(yROI/resolutionFactor-ellipsoidMask.yCenter);
+%             z=(zROI/resolutionFactor-ellipsoidMask.zCenter);   
 
 
             %radii refactoring
-            a=ellipsoidMask.xRadius+ellipsoidMask.cellHeight;
-            b=ellipsoidMask.yRadius+ellipsoidMask.cellHeight;
-            c=ellipsoidMask.zRadius+ellipsoidMask.cellHeight;
+                      
+            
+            a=ellipsoidMask.xRadius+cellHeight;
+            b=ellipsoidMask.yRadius+cellHeight;
+            c=ellipsoidMask.zRadius+cellHeight;
+            
+            %Create ellipsoid coordinates
+            [xEllip,yEllip,zEllip] = ellipsoid(0,0,0,a,b,c,200);
+            xyzEllipsoid=[xEllip(:),yEllip(:),zEllip(:)];
+            roiEnergyCoord=logical((xyzEllipsoid(:,1)>(-limitXRoi)).*(xyzEllipsoid(:,1)<limitXRoi));
+            xyzEllipsoid=xyzEllipsoid(roiEnergyCoord,:);
+            
+%             xROI=x(roiEnergyCoord);
+%             yROI=y(roiEnergyCoord);
+%             zROI=z(roiEnergyCoord);
+            
+%             %extrapolate surface coordinates to real ellipsoid coordinates
+%             xyzRefactor=zeros(length(x),3);
+%             tic
+%             parfor it=1:length(x)
+%                 [inn,indexM]=min(pdist2([x(it),y(it),z(it)],xyzEllipsoid));
+%                 xyzRefactor(it,:)=xyzEllipsoid(indexM(1),:);
+%             end
+%             toc
+%             xyzRefactor=unique(xyzRefactor,'rows');
             
             %initiallitation of curvature parameters
-            R1=zeros(length(x),1);
-            R2=zeros(length(x),1);
-            k1=zeros(length(x),1);
-            k2=zeros(length(x),1);
+            R1=zeros(size(xyzEllipsoid,1),1);
+            R2=zeros(size(xyzEllipsoid,1),1);
+            k1=zeros(size(xyzEllipsoid,1),1);
+            k2=zeros(size(xyzEllipsoid,1),1);
 
-            parfor j=1:length(x)
-                [R1(j),R2(j),k1(j),k2(j)] = testingCurvatureInEllipsoidCoordinate(x(j),y(j),z(j),a,b,c);
+            %testing curvatures
+            parfor j=1:size(xyzEllipsoid,1)
+                [R1(j),R2(j),k1(j),k2(j)] = testingCurvatureInEllipsoidCoordinate(xyzEllipsoid(j,1),xyzEllipsoid(j,2),xyzEllipsoid(j,3),a,b,c);
+                
             end
             
+            R1=R1(~isnan(R1));
+            R2=R2(~isnan(R2));
+
             R1_mean=mean(R1);        
             R1_std=std(R1);
             R2_mean=mean(R2);
@@ -100,7 +128,7 @@ for i = 1 : length(filePaths)
                save([filePath,'\curvatureOuterEllipsoid_' splittedCellHeight(1:end-4) '_'  date '.mat' ],'R1_mean','R2_mean','R1_std','R2_std','k1_mean','k2_mean','k1_std','k2_std','R1','R2','k1','k2')
             else
                 if nHeight==1
-                    save([filePath,'\curvatureInnerEllipsoid ' date '_.mat'],'R1_mean','R2_mean','R1_std','R2_std','k1_mean','k2_mean','k1_std','k2_std','R1','R2','k1','k2')
+                    save([filePath,'\curvatureInnerEllipsoid_' date '_.mat'],'R1_mean','R2_mean','R1_std','R2_std','k1_mean','k2_mean','k1_std','k2_std','R1','R2','k1','k2')
                 end
             end
             
