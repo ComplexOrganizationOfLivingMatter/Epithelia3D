fc56624d5569c7e095af70694e68cd4978425b14
Pablo Vicente Munuera <pvicente1@us.es>
2018-03-23 12:07:30 +0100

    deleting 3d vertices and use of 2d projections vertices

--- InSilicoModels/EllipsoidModel/frustaEllipsoidModel/src/checkMaxProjectionExist.m
+++ InSilicoModels/EllipsoidModel/frustaEllipsoidModel/src/checkMaxProjectionExist.m
@@ -1,10 +1,10 @@
-function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex, verticesOuter] = checkMaxProjectionExist(ellipsoidPath,filePaths,nRand,nCellHeight,splittedCellHeight,nPath,cellHeight)
+function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex] = checkMaxProjectionExist(ellipsoidPath,filePaths,nRand,nCellHeight,splittedCellHeight,nPath,cellHeight)
 
     if exist([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'file') || exist([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'file')
             if nCellHeight>1
-                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
+                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
             else
-                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
+                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
             end
     else
 
@@ -13,17 +13,17 @@ function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,p
         load([strrep(filePaths{nPath},'results','..\voronoiEllipsoidModel\results') 'random_' num2str(nRand) '\' ellipsoidPath(cellHeight).name],'initialEllipsoid','ellipsoidInfo')
         ellipsoidInfo.img3DLayer=allFrustaImage;
         
-        verticesOuter.verticesConnectCells = verticesConnectCellsInitial;
-        verticesOuter.verticesPerCell = verticesPerCellInitial;
+%         verticesOuter.verticesConnectCells = verticesConnectCellsInitial;
+%         verticesOuter.verticesPerCell = verticesPerCellInitial;
 
         %getting 4 projections from 3d ellipsoid
         tic
         [~,~,projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex]=maxProjectionVerticesFrustaEllipsoid( ellipsoidInfo,initialEllipsoid,finalVerticesAugmented,cell2mat(verticesPerCellInitial),verticesConnectCellsInitial);
         toc
         if nCellHeight>1
-            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
+            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
         else
-            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
+            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
         end
 
     end
