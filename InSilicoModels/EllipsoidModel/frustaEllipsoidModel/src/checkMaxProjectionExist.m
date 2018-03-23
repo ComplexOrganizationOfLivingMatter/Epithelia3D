9b19dbd6064a2ef4aa911c5701322b120c324062
Pablo Vicente Munuera <pvicente1@us.es>
2018-03-23 10:32:48 +0100

    saving vertices outer properly

--- InSilicoModels/EllipsoidModel/frustaEllipsoidModel/src/checkMaxProjectionExist.m
+++ InSilicoModels/EllipsoidModel/frustaEllipsoidModel/src/checkMaxProjectionExist.m
@@ -1,10 +1,10 @@
-function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex] = checkMaxProjectionExist(ellipsoidPath,filePaths,nRand,nCellHeight,splittedCellHeight,nPath,cellHeight)
+function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex, verticesOuter] = checkMaxProjectionExist(ellipsoidPath,filePaths,nRand,nCellHeight,splittedCellHeight,nPath,cellHeight)
 
     if exist([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'file') || exist([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'file')
             if nCellHeight>1
-                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
+                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
             else
-                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
+                load([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
             end
     else
 
@@ -12,15 +12,18 @@ function [projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,p
         ellipsoidPath=dir([strrep(filePaths{nPath},'results','..\voronoiEllipsoidModel\results') 'random_' num2str(nRand) '\*llipsoid*' ]);
         load([strrep(filePaths{nPath},'results','..\voronoiEllipsoidModel\results') 'random_' num2str(nRand) '\' ellipsoidPath(cellHeight).name],'initialEllipsoid','ellipsoidInfo')
         ellipsoidInfo.img3DLayer=allFrustaImage;
+        
+        verticesOuter.verticesConnectCells = verticesConnectCellsInitial;
+        verticesOuter.verticesPerCell = verticesPerCellInitial;
 
         %getting 4 projections from 3d ellipsoid
         tic
         [~,~,projectionsInnerWater,projectionsOuterWater,projectionsInnerVertices,projectionsOuterVertices,projectionsCellsConnectedToVertex]=maxProjectionVerticesFrustaEllipsoid( ellipsoidInfo,initialEllipsoid,finalVerticesAugmented,cell2mat(verticesPerCellInitial),verticesConnectCellsInitial);
         toc
         if nCellHeight>1
-            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
+            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections_' splittedCellHeight],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
         else
-            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex')
+            save([filePaths{nPath} 'random_' num2str(nRand) '\roiProjections.mat'],'projectionsInnerWater','projectionsOuterWater','projectionsInnerVertices','projectionsOuterVertices','projectionsCellsConnectedToVertex', 'verticesOuter')
         end
 
     end
