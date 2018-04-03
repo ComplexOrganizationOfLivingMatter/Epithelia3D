1fd34104444e3715e8c19d6c98fd4a41a05996eb
Pablo Vicente Munuera <pvicente1@us.es>
2018-04-03 15:44:38 +0200

    new fileNames

---
+++ Graphics/pipelineTubularModel.m
@@ -0,0 +1,18 @@
+
+numSeeds = '60';
+surfaceRatio = '6.6667';
+
+voronoiModelNoTrans = readtable(strcat('D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AngleThreshold\Voronoi\Matching_Apical_Basal\unfiltered\voronoiModelEnergy_', numSeeds,'seeds_surfaceRatio', surfaceRatio,'_NoTransitions_AngleThreshold_02-Apr-2018.xls'));
+voronoiModelTrans = readtable(strcat('D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AngleThreshold\Voronoi\Matching_Apical_Basal\unfiltered\voronoiModelEnergy_', numSeeds,'seeds_surfaceRatio', surfaceRatio,'_Transitions_AngleThreshold_02-Apr-2018.xls'));
+
+allFrustaModel_Apical = readtable(strcat('D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AngleThreshold\AllFrusta\AllMotifs\allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_1_AngleThreshold_02-Apr-2018.xls'));
+allFrustaModel_Basal = readtable(strcat('D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AngleThreshold\AllFrusta\AllMotifs\allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_', surfaceRatio,'_AngleThreshold_02-Apr-2018.xls'));
+
+[ allFrustaModel ] = unifyApicalAndBasal( allFrustaModel_Apical, allFrustaModel_Basal );
+
+[ allFrustaFakeTrans, allFrustaNoTrans, allFrustaFakeTransFiltered, allFrustaNoTransFiltered  ] = correspondanceFrustaAndVoronoi( voronoiModelNoTrans, voronoiModelTrans, allFrustaModel );
+
+writetable(allFrustaNoTrans, strcat('allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_', surfaceRatio,'_NoTransitions_AngleThreshold_02-Apr-2018.xls'));
+writetable(allFrustaFakeTrans, strcat('allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_', surfaceRatio,'_FakeTransitions_AngleThreshold_02-Apr-2018.xls'));
+writetable(allFrustaNoTransFiltered, strcat('allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_', surfaceRatio,'_NoTransitions_filtered_AngleThreshold_02-Apr-2018.xls'));
+writetable(allFrustaFakeTransFiltered, strcat('allFrustaEnergy_', numSeeds,'seeds_surfaceRatio_', surfaceRatio,'_FakeTransitions_filtered_AngleThreshold_02-Apr-2018.xls'));
