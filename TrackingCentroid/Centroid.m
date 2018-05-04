3797beaf44479721901b69ee87b0cacf85672811
TinaForja <tinaforja93@hotmail.es>
2018-05-04 12:43:44 +0200

    Reorganization of functions
    The functions that can be used are added to the TransitionLayers folder.

---
+++ TrackingCentroid/Centroid.m
@@ -0,0 +1,67 @@
+function [centroidsC, pixel, maskBW] = Centroid( photoPath, name)
+
+
+%%Load images
+Img=imread(photoPath);
+I=Img>80;  %Convert image to binary image, based on threshold
+           %(>80 in this case, but this will change according to the sample) 
+
+%%Deleting objects smaller than 5 pix
+BW2= bwareaopen(I,5);
+
+
+% D = bwdist(~BW2);
+% D = -D;
+% L = watershed(D);
+% L(~BW2) = 0;
+% maskBW=double(L);
+% imshow(maskBW)
+ 
+%%Dilatation
+se=strel('disk',2);
+BW2=imdilate(BW2,se);
+
+%%Calculate area of objects
+Area_ob = regionprops(BW2, 'area');
+Area_ob = cat(1, Area_ob.Area);
+area_mean=mean(Area_ob);
+
+%%Separation between cells according to size
+L=bwlabel(BW2,8);
+mask=zeros(size(BW2));
+
+for i=1:max(max(L))
+ M=zeros(size(BW2));
+ M(L==i)=1;
+ if Area_ob(i)> area_mean*1.5
+   se = strel('disk',5); %With 7 in the sample 5, it works. Usually
+   BWM = imerode(M,se);
+ else
+    BWM=M;
+ end
+ mask=mask+BWM;
+end
+
+maskBW=bwlabel(mask,8);
+
+ 
+%%Specify the center of mass of the region
+centroids = regionprops(maskBW, 'Centroid', 'PixelList');
+centroidsC = vertcat(centroids.Centroid);
+pixel = vertcat(centroids.PixelList);
+
+
+%%Show the created image along with the image of the centroids and Voronoi
+% f=figure('Visible', 'on');
+% imshow(Img);
+% hold on;
+% 
+% [m,n] = size(centroids);
+% 
+% for i=1:m
+%    plot(centroidsC(i,1), centroidsC(i, 2), 'b*');
+% end
+
+
+end
+
--- TransitionLayers/imgLabelled.m
+++ TransitionLayers/imgLabelled.m
@@ -1,17 +1,22 @@
-load('neighbours_layer2.mat');
-load('E:\Tina\Epithelia3D\Zebrafish\Results\Sample2\trackingLayer2.mat');
-load('transition_layers2.mat')
-load('E:\Tina\pseudostratifiedEpithelia\LayerAnalysis\imgVoronoi2D_16-Oct-2017.mat');
+% load('neighbours_layer2.mat');
+% load('E:\Tina\Epithelia3D\Zebrafish\Results\Sample2\trackingLayer2.mat');
+% load('transition_layers2.mat')
+% load('E:\Tina\pseudostratifiedEpithelia\LayerAnalysis\imgVoronoi2D_16-Oct-2017.mat');
 
 initialFrame=6;
-maxFrame=48;
-folderNumber=2;
+maxFrame=71;
+folderNumber=1;
+
+neighs_real=table2array(neigh_real);
+basicInfos=table2array(basicInfo);
+basicInfos=cell2mat(basicInfos);
+outputDir=['E:\Tina\Epithelia3D\Zebrafish\Results\Sample' sprintf('%d',folderNumber) '\Labelled\Labelled' sprintf('%d',folderNumber) '\'];
 
 %Representation of actual images with layered labels
-for numLayer=1:size(transition_layer,1)
+for numLayer=1:3%size(transition_layer,1)
     for numFrame=initialFrame:maxFrame
         FileName=['E:\Tina\Epithelia3D\Zebrafish\50epib_' sprintf('%d',folderNumber) '\50epib_'  sprintf('%d',folderNumber) '_z' sprintf('%03d', numFrame) '_c001.tif'];
-        f=figure('Visible','off');
+        f=figure('Visible','on');
         imshow(FileName)
         for numCentroid=1:size(finalCentroid)
             coordenates{numCentroid,1}(:,1)=finalCentroid{numCentroid,2}(1:2);
@@ -23,9 +28,16 @@ for numLayer=1:size(transition_layer,1)
             end
         end
         % Save images to return
-        framePhoto=['E:\Tina\Epithelia3D\Zebrafish\50epib_' sprintf('%d',folderNumber) '\Labelled\Labelled' sprintf('%d',numLayer) '\50epib_'  sprintf('%d',folderNumber) '_z' sprintf('%03d', numFrame) '_c001'];
+%         framePhoto=['E:\Tina\Epithelia3D\Zebrafish\Results\Sample_' sprintf('%d',folderNumber) '\Labelled\Labelled' sprintf('%d',folderNumber) '\50epib_'  sprintf('%d',numLayer) '_z' sprintf('%03d', numFrame) '_c001.bmp'];
+%         set(gcf,'InvertHardCopy','off');
+%         print('-dtiff',framePhoto);
+        
         set(gcf,'InvertHardCopy','off');
-        print('-dtiff',framePhoto);
+        framePhoto=['50epib_'  sprintf('%d',numLayer) '_z' sprintf('%03d', numFrame) '_c001.bmp'];
+        saveas(gcf,strcat(outputDir,framePhoto)); 
+        
+        
+        
 
     end
 end
@@ -42,7 +54,7 @@ for numZ = 1:size(Img, 2)
         hold on
         label=text(centroid(numCentroid,1), centroid(numCentroid,2),sprintf('%d',numCentroid),'HorizontalAlignment','center','VerticalAlignment','middle','Color','red','FontSize',9);   
     end
-    FileName=['E:\Tina\results\LayerAnalysis\img_z_' sprintf('%d',numZ) '.bmp'];
+    FileName=['E:\Tina\results\LayerAnalysis\Layer_all' sprintf('%d',folderNumber) '\Labelled\imgLabelled_z_' sprintf('%d',numZ) '.bmp'];
     print('-dtiff',FileName);
 end
 
