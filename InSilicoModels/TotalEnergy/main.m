e110eb3dca3daee6d33500a65db8061a17219244
Pablo Vicente Munuera <pvicente1@us.es>
2018-05-23 11:49:01 +0200

    800 seeds

--- InSilicoModels/TotalEnergy/main.m
+++ InSilicoModels/TotalEnergy/main.m
@@ -49,8 +49,8 @@ addpath('lib/');
 
 %% With no matching (all the motifs of the layer
 
-inputDirectoriesFrusta = 'D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AllFrusta_energy_200seeds\';
-inputDirectoriesVoronoi = 'D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\Voronoi_energy_200seeds\';
+inputDirectoriesFrusta = 'D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\AllFrusta_energy_800seeds\';
+inputDirectoriesVoronoi = 'D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\docs\Voronoi_energy_800seeds\';
 surfaceRatios = {'1', '1.25', '1.6667', '2', '5'};
 
 finalTable = table();
@@ -65,8 +65,8 @@ definedAngles = 0:15:90;
 
 for numSR = 1:length(surfaceRatios)
     
-    voronoiFile = dir(strcat(inputDirectoriesVoronoi, 'allMotifsEnergy_200seeds_surfaceRatio', surfaceRatios{numSR}, '_*'));
-    frustaFile = dir(strcat(inputDirectoriesFrusta, 'allFrustaEnergy_200seeds_surfaceRatio_', surfaceRatios{numSR}, '_*'));
+    voronoiFile = dir(strcat(inputDirectoriesVoronoi, 'allMotifsEnergy_800seeds_surfaceRatio', surfaceRatios{numSR}, '_*'));
+    frustaFile = dir(strcat(inputDirectoriesFrusta, 'allFrustaEnergy_800seeds_surfaceRatio_', surfaceRatios{numSR}, '_*'));
     [uniqueValidCells, modelFrusta, modelVoronoi, frustaPolDist, voronoiPolDist ] = getValidOfValidCells( readtable(strcat(inputDirectoriesFrusta, frustaFile(1).name)), readtable(strcat(inputDirectoriesVoronoi, voronoiFile(1).name)), str2double(surfaceRatios{numSR}));
     
     % It was not necessary to perform the Edge length cutoff because we do
