f5066fa41d16109833c19ba4d20eac0305d2dd3f
Pablo Vicente Munuera <pvicente1@us.es>
2018-05-23 16:30:39 +0200

    painting tension of each edge

---
+++ InSilicoModels/TotalEnergy/src/paintLineTensionEdges.m
@@ -0,0 +1,14 @@
+function [] = paintLineTensionEdges( modelFrusta, modelVoronoi, surfaceRatio )
+%PAINTLINETENSIONEDGES Summary of this function goes here
+%   Detailed explanation goes here
+
+    maxRandoms = 20;
+    for nRandom = 1:maxRandoms
+        load(strcat('D:\Pablo\Epithelia3D\InSilicoModels\TubularModel\data\voronoiModel\expansion\512x4096_800seeds\Image_', num2str(nRandom), '_Diagram_5\Image_', num2str(nRandom),'_Diagram_5.mat'));
+        
+        
+    end
+
+
+end
+
