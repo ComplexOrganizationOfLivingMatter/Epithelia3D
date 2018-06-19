6bc3ef155106b314d2563c363bd0aa6a0f4ba5a4
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-06-19 17:47:16 +0200

    updating code

--- InSilicoModels/TubularModel/src/connectVerticesIn3D.m
+++ InSilicoModels/TubularModel/src/connectVerticesIn3D.m
@@ -1,6 +1,11 @@
 function connectVerticesIn3D(dataVertID,clusterOfNeighs,verticesInfoDown,verticesInfoUp)
 
+    %add id to scutoid vertices
+    indexesScuVert=size(dataVertID,1)+1:size(dataVertID,1)+size(clusterOfNeighs,1);
+    indexesScuVert=num2cell(indexesScuVert');
+    clusterOfNeighs=[indexesScuVert,clusterOfNeighs];
 
+    %differencing apical-basal-extremes
     indVertApical=cell2mat(dataVertID(:,2))==min(cell2mat(dataVertID(:,2)));
     indVertBasal=cell2mat(dataVertID(:,2))==max(cell2mat(dataVertID(:,2)));
 
@@ -9,14 +14,21 @@ function connectVerticesIn3D(dataVertID,clusterOfNeighs,verticesInfoDown,vertice
     indVertBordApical=cellfun(@(x) length(x)==2,dataVertIDApical(:,7));
     indVertBordBasal=cellfun(@(x) length(x)==2,dataVertIDBasal(:,7));
     dataVertIDApicalBord=dataVertIDApical(indVertBordApical,:);
-    dataVertIDBasalBord=dataVertIDApical(indVertBordBasal,:);
+    dataVertIDBasalBord=dataVertIDBasal(indVertBordBasal,:);
 
     dataVertIDApical=dataVertIDApical(~indVertBordApical,:);
     dataVertIDBasal=dataVertIDBasal(~indVertBordBasal,:);
 
+    %join vertices apical
     for nVertApical=1:size(dataVertIDApical,1)
         vert2Join=dataVertIDApical(nVertApical,:);
+        vertId=vert2Join{3};
+        cellsInVert=vert2Join{end};
         
+        matchVert=cellfun(@(x) sum(ismember(cellsInVert,x))==3,clusterOfNeighs(:,2));
+        indexesMatch=find(matchVert);
+        [~,ind]=min(cell2mat(clusterOfNeighs(matchVert,4)));
+        joinedVertices = [vertId indexesMatch(ind)];
         
     end
     
