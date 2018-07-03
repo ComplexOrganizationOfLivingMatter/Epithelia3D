24f91c353a083a5cef1e00bac525be49c8b7e640
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-07-03 16:18:33 +0200

    save hypocotyl per surfaces and layers

--- PLANTS/src/getColHypocotylPerSurfaces.m
+++ PLANTS/src/getColHypocotylPerSurfaces.m
@@ -1,30 +1,122 @@
-function [surfacesHypocotyl]=getColHypocotylPerSurfaces(neighbourhoodInfo,img3d)
+function [outerSurfaceLayer1,innerSurfaceLayer1,outerSurfaceLayer2,innerSurfaceLayer2,cellsLayer1,cellsLayer2]=getColHypocotylPerSurfaces(neighbourhoodInfo,img3d,cellCorrectLayer1)
 
-    %calculate outer layer of Col_hypocotyl
+    %calculate outer surface of layer 1 of Col_hypocotyl
     mask3d=false(size(img3d));
     mask3d(img3d>0)=1;
     mask3dFilled=imfill(mask3d,'holes');
     zerosMask=logical(1-mask3dFilled);
-    zerosMaskDilated=imdilate(zerosMask,strel('sphere',1));
+    se=strel('sphere',1);
+    zerosMaskDilated=imdilate(zerosMask,se);
     maskCellsOuter=zerosMaskDilated-zerosMask;
-    maskOuterCellsOuterSurface=zeros(size(img3d));
-    maskOuterCellsOuterSurface(maskCellsOuter==1)=img3d(maskCellsOuter==1);
+    outerSurfaceLayer1=zeros(size(img3d));
+    outerSurfaceLayer1(maskCellsOuter==1)=img3d(maskCellsOuter==1);
+    volumeReg=regionprops3(outerSurfaceLayer1,'Volume');
+    volumeCells=cat(1,volumeReg.Volume);
+    cells2delete=find(volumeCells>0 & volumeCells<50);
 
+    for nCell=cells2delete'
+        outerSurfaceLayer1(outerSurfaceLayer1==nCell)=0;
+    end
     %calculate cells in Layer 1 (outerLayer)
-    cellsOuter=unique(maskOuterCellsOuterSurface);
-    cellsOuter=cellsOuter(cellsOuter~=0);
+    cellsLayer1=unique(outerSurfaceLayer1);
+    cellsLayer1=cellsLayer1(cellsLayer1~=0);
+    
+    if ~isempty(cellCorrectLayer1)
+        cells2delete=setdiff(cellsLayer1,cellCorrectLayer1);
+        for nCell=cells2delete'
+            outerSurfaceLayer1(outerSurfaceLayer1==nCell)=0;
+        end
+        cellsLayer1 = intersect(cellsLayer1,cellCorrectLayer1);        
+    end
+
     
     neighsGlobal=neighbourhoodInfo.neighbourhood;
-    neighsOuterCells=neighsGlobal(cellsOuter);
+    neighsOuterCells=neighsGlobal(cellsLayer1);
     
     %calculate cells in layer 2
-    neighsCellsLayer1=cellfun(@(x) x(~ismember(x,cellsOuter)),neighsOuterCells,'UniformOutput',false); 
+    neighsCellsLayer1=cellfun(@(x) x(~ismember(x,cellsLayer1)),neighsOuterCells,'UniformOutput',false); 
     cellsLayer2=unique(vertcat(neighsCellsLayer1{:}));   
     
     %calculate cells in layer 3
     neighsSecondlayerCells=neighsGlobal(cellsLayer2);
-    innerNeighsCellsLayer2=cellfun(@(x) x(~ismember(x,[cellsOuter;cellsLayer2])),neighsSecondlayerCells,'UniformOutput',false);
+    innerNeighsCellsLayer2=cellfun(@(x) x(~ismember(x,[cellsLayer1;cellsLayer2])),neighsSecondlayerCells,'UniformOutput',false);
     cellsLayer3=unique(vertcat(innerNeighsCellsLayer2{:}));   
 
     
+    %layer 1
+    mask3dLayer1=false(size(img3d));
+    for i = cellsLayer1'
+        mask3dLayer1 = mask3dLayer1 | (img3d==i);
+    end
+    
+    %layer 1 with dilated cells
+    mask3dLayer1Dilated=false(size(img3d));
+    for i = cellsLayer1'
+        indDilated=sub2ind(size(img3d),neighbourhoodInfo.cellDilated{i}(:,1),...
+            neighbourhoodInfo.cellDilated{i}(:,2),neighbourhoodInfo.cellDilated{i}(:,3));
+        mask3dLayer1Dilated(indDilated)=1;
+    end
+    
+    %layer 2
+    mask3dLayer2=false(size(img3d));
+    for i = cellsLayer2'
+        mask3dLayer2 = mask3dLayer2 | (img3d==i);
+    end
+    
+    %layer 2 with dilated cells
+    mask3dLayer2Dilated=false(size(img3d));
+    for i = cellsLayer2'
+        indDilated=sub2ind(size(img3d),neighbourhoodInfo.cellDilated{i}(:,1),...
+            neighbourhoodInfo.cellDilated{i}(:,2),neighbourhoodInfo.cellDilated{i}(:,3));
+        mask3dLayer2Dilated(indDilated)=1;
+    end
+    
+    %layer 3 with dilated cells
+    mask3dLayer3Dilated=false(size(img3d));
+    for i = cellsLayer3'
+        indDilated=sub2ind(size(img3d),neighbourhoodInfo.cellDilated{i}(:,1),...
+            neighbourhoodInfo.cellDilated{i}(:,2),neighbourhoodInfo.cellDilated{i}(:,3));
+        mask3dLayer3Dilated(indDilated)=1;
+    end
+    
+    %calculate inner surface - Layer 1
+    innerSurfaceLayer1=zeros(size(img3d));
+    innerLayer1Indices=mask3dLayer1 & mask3dLayer2Dilated;
+    innerSurfaceLayer1(innerLayer1Indices)=img3d(innerLayer1Indices);
+    
+    volumeReg=regionprops3(innerSurfaceLayer1,'Volume');
+    volumeCells=cat(1,volumeReg.Volume);
+    cells2delete=find(volumeCells>0 & volumeCells<50);
+    for nCell=cells2delete'
+        innerSurfaceLayer1(innerSurfaceLayer1==nCell)=0;
+    end
+        
+    
+    %calculate outer surface - Layer 2
+    outerSurfaceLayer2=zeros(size(img3d));
+    outerLayer2Indices=mask3dLayer1Dilated & mask3dLayer2;
+    outerSurfaceLayer2(outerLayer2Indices)=img3d(outerLayer2Indices);
+    volumeReg=regionprops3(outerSurfaceLayer2,'Volume');
+    volumeCells=cat(1,volumeReg.Volume);
+    cells2delete=find(volumeCells>0 & volumeCells<50);
+    for nCell=cells2delete'
+        outerSurfaceLayer2(outerSurfaceLayer2==nCell)=0;
+    end
+    
+    %calculate inner surface - Layer 2
+    innerSurfaceLayer2=zeros(size(img3d));
+    innerLayer2Indices=mask3dLayer3Dilated & mask3dLayer2;
+    innerSurfaceLayer2(innerLayer2Indices)=img3d(innerLayer2Indices);
+    volumeReg=regionprops3(innerSurfaceLayer2,'Volume');
+    volumeCells=cat(1,volumeReg.Volume);
+    cells2delete=find(volumeCells>0 & volumeCells<50);
+    for nCell=cells2delete'
+        innerSurfaceLayer2(innerSurfaceLayer2==nCell)=0;
+    end
+
+    
+    cellsLayer2Out=unique(outerSurfaceLayer2);
+    cellsLayer2Inn=unique(outerSurfaceLayer2);
+    cellsLayer2=intersect(cellsLayer2Out,cellsLayer2Inn);
+    cellsLayer2=cellsLayer2(cellsLayer2~=0);
 end
--- PLANTS/src/selectCellsPerSurface.m
+++ PLANTS/src/selectCellsPerSurface.m
@@ -8,33 +8,41 @@ addpath(genpath('lib'))
 for nNam=1:2%1:length(names)
     load(['..\' names{nNam} '\image3d_' strrep(names{nNam},' ','_') '.mat'],'img3d');
 
-%     tic
-%     [neighbourhoodInfo] = calculate_neighbours3D(img3d);
-%     toc
-%     save(['..\' names{nNam} '\neighboursInfo.mat'],'neighbourhoodInfo')
-    load(['..\' names{nNam} '\neighboursInfo.mat'],'neighbourhoodInfo')
-   
+    if ~exist(['..\' names{nNam} '\neighboursInfo.mat'],'file')
+        [neighbourhoodInfo] = calculate_neighbours3D(img3d);
+        save(['..\' names{nNam} '\neighboursInfo.mat'],'neighbourhoodInfo')
+    else
+        load(['..\' names{nNam} '\neighboursInfo.mat'],'neighbourhoodInfo')
+    end
     
-    [surfacesHypocotyl]=getColHypocotylPerSurfaces(neighbourhoodInfo,img3d);
-  
-
+    if exist(['..\' names{nNam} '\cellsLayer1Correction.mat'],'file')
+        load(['..\' names{nNam} '\cellsLayer1Correction.mat'],'cellsCorrectLayer1')
+        [layer1.outerSurface,layer1.innerSurface,layer2.outerSurface,layer2.innerSurface,setOfCells.Layer1,setOfCells.Layer2]=getColHypocotylPerSurfaces(neighbourhoodInfo,img3d,cellsCorrectLayer1);
+    end
+    
+    [layer1.outerSurface,layer1.innerSurface,layer2.outerSurface,layer2.innerSurface,setOfCells.Layer1,setOfCells.Layer2]=getColHypocotylPerSurfaces(neighbourhoodInfo,img3d,[]);
+    
+    centroids=regionprops3(img3d,'Centroid');
+    centroids=cat(1,centroids.Centroid);
+    save(['..\' names{nNam} '\cellsAndSurfacesPerLayer.mat'],'-v7.3','layer1','layer2','setOfCells','centroids')
 
 %     verticesInfoOuterLayerOuterSurface=getVertices3d(maskOuterSurface,neighboursLayer,neighbourhoodInfo);
 
-        
-%     colours = jet(max(totalBorderCells));
-%     colours = colours(randperm(max(totalBorderCells)), :);
-%     
-%     
-%     centroids=regionprops3(img3d,'Centroid');
-%     centroids=cat(1,centroids.Centroid);
+    
+%     colours = jet(double(max(setOfCells.Layer1)));
+%     colours = colours(randperm(max(setOfCells.Layer1)), :);
+% 
 %     h=figure;    
-%     for nCell=totalBorderCells
+%     for nCell=setOfCells.Layer1'
         
-%         plot3(centroids(nCell,1),centroids(nCell,2),centroids(nCell,3),'*')
-%         [x,y,z] = ind2sub(size(maskCellsOuter),find(maskCellsOuter==nCell));
+%         [x,y,z] = ind2sub(size(layer2.outerSurface),find(layer2.outerSurface==nCell));
 %         shp=alphaShape(x,y,z,500);
-%         plot(shp, 'FaceColor', colours(nCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+%         plot(shp, 'FaceColor', colours(nCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 1);
+%         hold on
+%         
+%         [x,y,z] = ind2sub(size(layer1.innerSurface),find(layer1.innerSurface==nCell));
+%         shp=alphaShape(x,y,z,500);
+%         plot(shp, 'FaceColor', colours(nCell, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 1);
 %         hold on
 %     end
 %     
