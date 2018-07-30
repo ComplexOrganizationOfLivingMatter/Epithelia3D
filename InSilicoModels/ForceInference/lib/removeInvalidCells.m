68a96901197a9b313114e06ff8cffd2e33742f04
Pablo Vicente Munuera <pvicente1@us.es>
2018-07-30 16:27:21 +0200

    ForceInference to other repository

--- InSilicoModels/ForceInference/lib/removeInvalidCells.m
+++
@@ -1,7 +0,0 @@
-function [forceInference] = removeInvalidCells(forceInference)
-%REMOVEINVALIDCELLS Summary of this function goes here
-%   Detailed explanation goes here
-borderCells = forceInference.RealSides ~= forceInference.NumEdgesOfTension;
-forceInference(borderCells, :) = [];
-end
-
