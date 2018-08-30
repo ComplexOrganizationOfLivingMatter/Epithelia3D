dde041cdbdc2393c8b76bfe14ac31f301b44fc8d
Pablo Vicente Munuera <pvicente1@us.es>
2018-08-30 16:17:14 +0200

    missing vertices

--- InSilicoModels/TubularModel/src/createSamiraFormatExcel.m
+++ InSilicoModels/TubularModel/src/createSamiraFormatExcel.m
@@ -25,24 +25,47 @@ function [samiraTable] = createSamiraFormatExcel(pathFile, surfaceRatios)
             verticesOfCell = verticesInfo.verticesPerCell(verticesOfCellIDs);
             verticesOfCell = cell2mat(verticesOfCell);
             
-            orderBoundary = boundary(verticesOfCell(:, 1), verticesOfCell(:, 2), 0.2);
+            orderBoundary = boundary(verticesOfCell(:, 1), verticesOfCell(:, 2), 0.1);
             
             if length(orderBoundary)-1 ~= size(verticesOfCell, 1)
                disp(strcat('Warning: cell number', num2str(numCell), ' may be wrongly done'));
+               disp('Correcting...')
+               missingVertices = setdiff(1:size(verticesOfCell, 1), orderBoundary);
+               if length(missingVertices) == 1
+                   matDistance = pdist(verticesOfCell);
+                   matDistance = squareform(matDistance);
+                   [~, closestVertex] = sort(matDistance(missingVertices, :));
+                   closestVertex = closestVertex(2);
+                   closestVertexPosition = find(orderBoundary == closestVertex);
+                   possiblePartner = [orderBoundary(closestVertexPosition(end) - 1), orderBoundary(closestVertexPosition(1) + 1)];
+                   distanceToPossiblePartner1 = matDistance(missingVertices, possiblePartner(1)) - matDistance(closestVertex, possiblePartner(1));
+                   distanceToPossiblePartner2 = matDistance(missingVertices, possiblePartner(2)) - matDistance(closestVertex, possiblePartner(2));
+                   
+                   if distanceToPossiblePartner1 < distanceToPossiblePartner2
+                       newLocation = closestVertexPosition(end) - 1;
+                   else
+                       newLocation = closestVertexPosition(1) + 1;
+                   end
+                   
+                   orderBoundary = [orderBoudary(1:newLocation), missingVertices, orderBoudary(newLocation+1:end)];
+               end
+%                
+            else
+                % Should be connected clockwise
+                % I.e. from bigger numbers to smaller ones
+                % Or the second vertex should in the left hand of the first
+                [newOrderX, newOrderY] = poly2cw(verticesOfCell(orderBoundary(1:end-1), 1), verticesOfCell(orderBoundary(1:end-1), 2));
             end
             
-            % Should be connected clockwise
-            % I.e. from bigger numbers to smaller ones
-            % Or the second vertex should in the left hand of the first
-            [newOrderX, newOrderY] = poly2cw(verticesOfCell(orderBoundary(1:end-1), 1), verticesOfCell(orderBoundary(1:end-1), 2));
+            
 
             verticesRadius = [];
-            %figure;
-            %previousVertex = [newOrderX(end), newOrderY(end)];
+            figure;
+            previousVertex = [newOrderX(end), newOrderY(end)];
             for numVertex = 1:length(newOrderX)
-                %plot([previousVertex(1), newOrderX(numVertex)], [previousVertex(2), newOrderY(numVertex)]);
-                %hold on;
-                %previousVertex = [newOrderX(numVertex), newOrderY(numVertex)];
+                plot([previousVertex(1), newOrderX(numVertex)], [previousVertex(2), newOrderY(numVertex)]);
+                hold on;
+                previousVertex = [newOrderX(numVertex), newOrderY(numVertex)];
                 verticesRadius(end+1) = newOrderX(numVertex);
                 verticesRadius(end+1) = newOrderY(numVertex);
             end
