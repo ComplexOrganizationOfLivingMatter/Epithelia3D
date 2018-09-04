5f1fe2958b652ad108a6bb3d03e2764d66b9fffc
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-04 16:47:56 +0200

    pipeline and processing lumen

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -2,12 +2,18 @@
 addpath(genpath('src'))
 addpath(genpath('lib'))
 
+addpath(genpath(fullfile('..', '..', 'TubularModel', 'src')));
+
 dataDirs = dir('data/*/');
 
 for numDir = 3:size(dataDirs, 1)
     
     actualFile = dataDirs(numDir);
     
-    processCells(fullfile(actualFile.folder, actualFile.name, 'Cells', filesep))
+    labelledImage = processCells(fullfile(actualFile.folder, actualFile.name, 'Cells', filesep));
+    
+    labelledImage = processLumen(fullfile(actualFile.folder, actualFile.name, 'Lumen', filesep), labelledImage);
+    
+    [image3dInfo] = calculateNeighbours3D(labelledImage);
     
 end
