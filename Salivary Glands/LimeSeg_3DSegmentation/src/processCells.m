b3616826a24850d69da5e7cc16d2dbc03065a195
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-04 15:03:14 +0200

    labelled image with the number of cell

--- Salivary Glands/LimeSeg_3DSegmentation/src/processCells.m
+++ Salivary Glands/LimeSeg_3DSegmentation/src/processCells.m
@@ -4,12 +4,29 @@ function [] = processCells(directoryOfCells)
 
     cellFiles = dir(fullfile(directoryOfCells, 'state', 'cell_*'));
     
-    figure;
+    cellsPoints = {};
+    
+    labelledImage = zeros(1, 1, 1);
+    
+%     figure;
+    figure
     for numCell = 1:size(cellFiles, 1)
         plyFile = fullfile(cellFiles(numCell).folder, cellFiles(numCell).name, 'T_1.ply');
         ptCloud = pcread(plyFile);
-        pcshow(ptCloud)
+        cellsPoints{numCell} = ptCloud;
+        pixelLocations = round(double(ptCloud.Location));
+        tic
+        for numPixel = 1:size(pixelLocations, 1)
+            labelledImage(pixelLocations(numPixel, 1), pixelLocations(numPixel, 2), pixelLocations(numPixel, 3)) = numCell;
+        end
+        toc
+        
+        [x,y,z] = ind2sub(size(labelledImage),find(labelledImage>0));
+        pcshow([x,y,z]);
         hold on;
+        
+%         pcshow(ptCloud)
+%         hold on;
     end
     disp('done');
 end
