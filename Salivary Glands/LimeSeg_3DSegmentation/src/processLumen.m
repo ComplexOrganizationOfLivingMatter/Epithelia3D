e7c07d8c2b066390f0b0a18d475868b1d5ca006e
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-04 17:03:24 +0200

    apical layer done

--- Salivary Glands/LimeSeg_3DSegmentation/src/processLumen.m
+++ Salivary Glands/LimeSeg_3DSegmentation/src/processLumen.m
@@ -5,13 +5,15 @@ function [labelledImage, apicalLayer] = processLumen(lumenDir, labelledImage)
     lumenPC = pcread(fullfile(lumenFile.folder, lumenFile.name));
     %pcshow(lumenPC);
     pixelLocations = round(double(lumenPC.Location));
-    apicalLayer = zeros(size(labelledImage));
+    lumenImage = zeros(size(labelledImage));
     for numPixel = 1:size(pixelLocations, 1)
         labelledImage(pixelLocations(numPixel, 1), pixelLocations(numPixel, 2), pixelLocations(numPixel, 3)) = 0;
-        apicalLayer(pixelLocations(numPixel, 1), pixelLocations(numPixel, 2), pixelLocations(numPixel, 3)) = 1;
+        lumenImage(pixelLocations(numPixel, 1), pixelLocations(numPixel, 2), pixelLocations(numPixel, 3)) = 1;
     end
     
     %% Get apical layer by dilating the lumen
-    
+    se = strel('sphere', 3);
+    dilatedLumen = imdilate(lumenImage, se);
+    apicalLayer = dilatedLumen .* labelledImage;
     
 end
