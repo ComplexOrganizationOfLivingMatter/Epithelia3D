1c11347630f38e1bff22c14c01e805c1782bbf21
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-07 16:56:36 +0200

    export correctly

--- Salivary Glands/LimeSeg_3DSegmentation/src/exportAsImageSequence.m
+++ Salivary Glands/LimeSeg_3DSegmentation/src/exportAsImageSequence.m
@@ -9,15 +9,21 @@ function [] = exportAsImageSequence(labelledImage, outputDir)
     colours = colours(randperm(max(labelledImage(:))), :);
     colours = vertcat([1 1 1], colours);
     
-    figure;
+    figure; %'Visible', 'off', 
     for numZ = 1:size(labelledImage, 3)
         imshow(labelledImage(:, :, numZ), colours);
+        set(gcf, 'units','normalized','outerposition',[0 0 1 1]);
         centroids = regionprops(labelledImage(:, :, numZ), 'Centroid');
         centroids = vertcat(centroids.Centroid);
         for numCentroid = 1:size(centroids, 1)
-            text(centroids(numCentroid, 1), centroids(numCentroid, 2), num2str(numCentroid), 'HorizontalAlignment', 'center', 'BackgroundColor', 'white');
+            if mean(colours(numCentroid, :)) < 0.4
+                text(centroids(numCentroid, 1), centroids(numCentroid, 2), num2str(numCentroid), 'HorizontalAlignment', 'center', 'Color', 'white');
+            else
+                text(centroids(numCentroid, 1), centroids(numCentroid, 2), num2str(numCentroid), 'HorizontalAlignment', 'center');
+            end
         end
-        imwrite(labelledImage(:, :, numZ), , fullfile(outputDir, strcat('labelledImage_', num2str(numZ), '.tif')));
+        print(fullfile(outputDir, strcat('labelledImage_', num2str(numZ), '.tif')), '-dtiff');
+        %imwrite(labelledImage(:, :, numZ), , );
     end
 end
 
