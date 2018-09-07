a91e4695429168e7e96c36c6876a64ea7f65d2f8
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-07 16:45:43 +0200

    putting cell ids on the exported image

--- Salivary Glands/LimeSeg_3DSegmentation/src/exportAsImageSequence.m
+++ Salivary Glands/LimeSeg_3DSegmentation/src/exportAsImageSequence.m
@@ -9,8 +9,15 @@ function [] = exportAsImageSequence(labelledImage, outputDir)
     colours = colours(randperm(max(labelledImage(:))), :);
     colours = vertcat([1 1 1], colours);
     
+    figure;
     for numZ = 1:size(labelledImage, 3)
-        imwrite(labelledImage(:, :, numZ), colours, fullfile(outputDir, strcat('labelledImage_', num2str(numZ), '.tif')));
+        imshow(labelledImage(:, :, numZ), colours);
+        centroids = regionprops(labelledImage(:, :, numZ), 'Centroid');
+        centroids = vertcat(centroids.Centroid);
+        for numCentroid = 1:size(centroids, 1)
+            text(centroids(numCentroid, 1), centroids(numCentroid, 2), num2str(numCentroid), 'HorizontalAlignment', 'center', 'BackgroundColor', 'white');
+        end
+        imwrite(labelledImage(:, :, numZ), , fullfile(outputDir, strcat('labelledImage_', num2str(numZ), '.tif')));
     end
 end
 
