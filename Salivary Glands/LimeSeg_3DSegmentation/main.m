52516e605b7bb376201fccfeaf335cad40407e77
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-10 12:18:00 +0200

    updating main

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -6,21 +6,48 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
 dataDirs = dir('data/*/');
 
-
-numDepth = 2;
 resizeImg = 0.25;
 
 for numDir = 3:size(dataDirs, 1)
     
     actualFile = dataDirs(numDir);
     
-    [labelledImage, basalLayer] = processCells(fullfile(actualFile.folder, actualFile.name, 'Cells', filesep), resizeImg, numDepth);
-    [labelledImage, apicalLayer] = processLumen(fullfile(actualFile.folder, actualFile.name, 'Lumen', filesep), labelledImage, resizeImg, numDepth);
+    outputDir = fullfile(actualFile.folder, actualFile.name);
+    imageSequenceFiles = dir(fullfile(outputDir, 'ImageSequence'));
+    demoFile =  imageSequenceFiles(3);
+    demoImg = imread(fullfile(demoFile.folder, demoFile.name));
     
-    paint3D(basalLayer);
-    paint3D(apicalLayer);
-    paint3D(labelledImage);
+    imgSize = round(size(demoImg)*resizeImg);
     
-    [apical3dInfo] = calculateNeighbours3D(apicalLayer);
-    [basal3dInfo] = calculateNeighbours3D(basalLayer);
+    [labelledImage, basalLayer] = processCells(fullfile(actualFile.folder, actualFile.name, 'Cells', filesep), resizeImg, imgSize);
+    
+    [labelledImage, apicalLayer, lumenImage] = processLumen(fullfile(actualFile.folder, actualFile.name, 'Lumen', filesep), labelledImage, resizeImg);
+    
+    exportAsImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep));
+    
+    setappdata(0,'outputDir',fullfile(actualFile.folder, actualFile.name));
+    setappdata(0,'labelledImage',labelledImage);
+    setappdata(0,'resizeImg',resizeImg);
+    
+    [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = plotMissingCells(labelledImage, lumenImage, apicalLayer, basalLayer);
+    
+    %% Insert no valid cells
+    [noValidCells] = insertNoValidCells();
+    while isequal(answer, 'No')
+        h = window();
+        waitfor(h);
+
+        labelledImage = getappdata(0, 'labelledImage');
+
+        exportAsImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep));
+        
+        
+        %% Calculate neighbours and plot missing cells
+        [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(labelledImage, lumenImage, apicalLayer, basalLayer);
+%         labelledImage = importImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep));
+%         
+%         [apical3dInfo] = calculateNeighbours3D(apicalLayer);
+%         [basal3dInfo] = calculateNeighbours3D(basalLayer);
+
+    end
 end
