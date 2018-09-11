2b91eec7945932aeb34cf5109a2d8e56f4fdce9e
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-09-11 17:22:49 +0200

    extrapolated image 3d wrking

--- PLANTS/src/extrapolate3DCylinder2Dplane.m
+++ PLANTS/src/extrapolate3DCylinder2Dplane.m
@@ -4,15 +4,15 @@ function [maskNewCyl] = extrapolate3DCylinder2Dplane(img3D,radii,centers)
     
     [allX,allY,allZ]=ind2sub(size(img3D),find(img3D>0));
     
-    maskNewCyl=zeros(round(2*pi*radiusAverage),length(allX));
-    maskInd=zeros(round(2*pi*radiusAverage),length(allX));
+    maskNewCyl=zeros(ceil(2*pi*radiusAverage),max(allY));
+    maskInd=zeros(ceil(2*pi*radiusAverage),max(allY));
 
     for nInd = 1 : length(allX)
         
         %3D to 2D by angle
         centroidYCircle=centers{allY(nInd)};
         angleWithCentroid = atan2d((allZ(nInd)-centroidYCircle(3)),(allX(nInd)-centroidYCircle(1)));
-        newX=round(deg2rad(wrapTo360(angleWithCentroid))*radiusAverage);
+        newX=ceil(deg2rad(wrapTo360(angleWithCentroid))*radiusAverage);
         newY=allY(nInd);
         
         %if the image possition is busy, choose the longest
@@ -20,8 +20,8 @@ function [maskNewCyl] = extrapolate3DCylinder2Dplane(img3D,radii,centers)
             maskNewCyl(newX,newY)=img3D(allX(nInd),allY(nInd),allZ(nInd));
             maskInd(newX,newY)=nInd;
         else
-            newDist=pdist2([centroidYCircle;[allX(nInd),allY(nInd),allZ(nInd)]]);
-            oldDist=pdist2([centroidYCircle;[allX(maskInd(newX,newY)),allY(maskInd(newX,newY)),allZ(maskInd(newX,newY))]]);
+            newDist=pdist2(centroidYCircle,[allX(nInd),allY(nInd),allZ(nInd)]);
+            oldDist=pdist2(centroidYCircle,[allX(maskInd(newX,newY)),allY(maskInd(newX,newY)),allZ(maskInd(newX,newY))]);
             
             if newDist > oldDist
                 maskNewCyl(newX,newY)=img3D(allX(nInd),allY(nInd),allZ(nInd));
--- PLANTS/src/extrapolatedImageAndVertices2DCylinder.m
+++ PLANTS/src/extrapolatedImageAndVertices2DCylinder.m
@@ -7,13 +7,19 @@ function extrapolatedImageAndVertices2DCylinder(name,layer1,layer2,verticesInfoL
     setOfRadii={radiiBasalLayer1,radiiApicalLayer1,radiiBasalLayer2,radiiApicalLayer2};
     setOf3DImages={layer1.outerSurface,layer1.innerSurface,layer2.outerSurface,layer2.innerSurface};
     
+    maxCell=max([unique(setOf3DImages{1});unique(setOf3DImages{2});unique(setOf3DImages{3});unique(setOf3DImages{4})]);
+    c=colorcube(double(maxCell));
+    indRand=randperm(maxCell);
+    
     for nSet = 1:length(setOfRadii)
 
         %get 2D image from hypocotil surface
         [maskNewCyl] = extrapolate3DCylinder2Dplane(setOf3DImages{nSet},setOfRadii{nSet},centers);
-        figure;imshow(maskNewCyk,colorcube(2000));
+        
+        figure;imshow(maskNewCyl,c(indRand,:));
+        axis equal
         %get vertices connections
-        connectVertices(setOfVertices{nSet},setOfRadii{nSet},centers)
+%         connectVertices(setOfVertices{nSet},setOfRadii{nSet},centers)
 
         
     end
