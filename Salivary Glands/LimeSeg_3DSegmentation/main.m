0747aeaf1d46d3db450dda8583cd3b558750b236
Pablo Vicente Munuera <pvicente1@us.es>
2018-09-11 15:02:32 +0200

    main updated for one file

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -4,51 +4,46 @@ addpath(genpath('lib'))
 
 addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
-dataDirs = dir('data/*/');
+selpath = uigetdir('data');
 
 resizeImg = 0.25;
 
 tipValue = 4;
+    
+outputDir = selpath;
+imageSequenceFiles = dir(fullfile(outputDir, 'ImageSequence'));
+demoFile =  imageSequenceFiles(3);
+demoImg = imread(fullfile(demoFile.folder, demoFile.name));
 
-for numDir = 3:size(dataDirs, 1)
+imgSize = round(size(demoImg)*resizeImg);
+
+[labelledImage, basalLayer] = processCells(fullfile(outputDir, 'Cells', filesep), resizeImg, imgSize, tipValue);
+
+[labelledImage, apicalLayer, lumenImage] = processLumen(fullfile(outputDir, 'Lumen', filesep), labelledImage, resizeImg, tipValue);
+
+[colours] = exportAsImageSequence(labelledImage, fullfile(outputDir, 'Cells', 'labelledSequence', filesep), [], tipValue);
+
+setappdata(0,'outputDir', outputDir);
+setappdata(0,'labelledImage',labelledImage);
+setappdata(0, 'lumenImage', lumenImage);
+setappdata(0,'resizeImg',resizeImg);
+setappdata(0,'tipValue', tipValue);
+
+%     [noValidCells] = insertNoValidCells();
+
+[answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(labelledImage, lumenImage, apicalLayer, basalLayer, colours, noValidCells);
+
+%% Insert no valid cells
+while isequal(answer, 'Yes')
+    h = window();
+    waitfor(h);
     
-    actualFile = dataDirs(numDir);
+    savingResults = saveResults();
     
-    outputDir = fullfile(actualFile.folder, actualFile.name);
-    imageSequenceFiles = dir(fullfile(outputDir, 'ImageSequence'));
-    demoFile =  imageSequenceFiles(3);
-    demoImg = imread(fullfile(demoFile.folder, demoFile.name));
-    
-    imgSize = round(size(demoImg)*resizeImg);
-    
-    [labelledImage, basalLayer] = processCells(fullfile(actualFile.folder, actualFile.name, 'Cells', filesep), resizeImg, imgSize, tipValue);
-    
-    [labelledImage, apicalLayer, lumenImage] = processLumen(fullfile(actualFile.folder, actualFile.name, 'Lumen', filesep), labelledImage, resizeImg, tipValue);
-    
-    [colours] = exportAsImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep), [], tipValue);
-    
-    setappdata(0,'outputDir',fullfile(actualFile.folder, actualFile.name));
-    setappdata(0,'labelledImage',labelledImage);
-    setappdata(0, 'lumenImage', lumenImage);
-    setappdata(0,'resizeImg',resizeImg);
-    setappdata(0,'tipValue', tipValue);
-    
-    [noValidCells] = insertNoValidCells();
-    
-    [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(labelledImage, lumenImage, apicalLayer, basalLayer, colours, noValidCells);
-    
-    %% Insert no valid cells
-    while isequal(answer, 'No')
-        h = window();
-        waitfor(h);
-        
-        savingResults = saveResults();
-        
-        if isequal(savingResults.output, 'Yes')
-            labelledImage = getappdata(0, 'labelledImageTemp');
-        end
-        
-        exportAsImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep), colours, tipValue);
+    if isequal(savingResults, 'Yes')
+        labelledImage = getappdata(0, 'labelledImageTemp');
+        close all
+        exportAsImageSequence(labelledImage, fullfile(outputDir, 'Cells', 'labelledSequence', filesep), colours, tipValue);
         
         %% Calculate neighbours and plot missing cells
         [basalLayer] = getBasalFrom3DImage(labelledImage, tipValue);
@@ -56,9 +51,13 @@ for numDir = 3:size(dataDirs, 1)
         
         [answer, apical3dInfo, notFoundCellsApical, basal3dInfo, notFoundCellsBasal] = calculateMissingCells(labelledImage, lumenImage, apicalLayer, basalLayer, colours, noValidCells);
         
-        polygonDistributions(numDir - 2, :) = {apical3dInfo, basal3dInfo};
+        validCells = setdiff(1:max(labelledImage(:)), noValidCells);
         
-%         labelledImage = importImageSequence(labelledImage, fullfile(actualFile.folder, actualFile.name, 'Cells', 'labelledSequence', filesep), tipValue);
+        [polygon_distribution_Apical] = calculate_polygon_distribution(cellfun(@length, apical3dInfo.neighbourhood), validCells);
+        [polygon_distribution_Basal] = calculate_polygon_distribution(cellfun(@length, basal3dInfo.neighbourhood), validCells);
         
+        polygonDistributions = {apical3dInfo.neighbourhood(validCells), basal3dInfo.neighbourhood(validCells)};
+    else
+        [answer] = isEverythingCorrect();
     end
 end
