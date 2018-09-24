5ee4f6d5ac2f1ae2c980085a287da835d5dbab72
anttagjan <atagua-ibis@us.es>
2018-09-24 15:38:55 +0200

    Save poligon_distribution.mat
    Save in the date file both .mat (apical and basal poligon distribution).

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -6,3 +6,7 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
 close all
 [polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions] = pipeline();
+
+ruta= uigetdir();
+save(strcat(ruta,'polygon_distribution_Apical.mat'))
+save(strcat(ruta,'polygon_distribution_Basal.mat'))
