bfa024c84465ffe007939b6b577dd47788d2d527
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-09-26 13:32:00 +0200

    images as output

--- PLANTS/src/Hypocotyl/cleanAndGetMeasurements.m
+++ PLANTS/src/Hypocotyl/cleanAndGetMeasurements.m
@@ -1,4 +1,4 @@
-function cleanAndGetMeasurements(deployedImg)
+function [finalImages,cellsLayer1,cellsLayer2]=cleanAndGetMeasurements(deployedImg)
 
     layer1Outer=deployedImg{1};
     layer1Inner=deployedImg{2};
@@ -12,16 +12,18 @@ function cleanAndGetMeasurements(deployedImg)
 
     
     [finalLayer1Outer,validCellsLayer1Outer,noValidCellsLayer1Outer]=getFinalImageAndNoValidCells(layer1Outer,c);
-    figure;imshow(finalLayer1Outer,c)
+%     figure;imshow(finalLayer1Outer,c)
 
     [finalLayer1Inner,validCellsLayer1Inner,noValidCellsLayer1Inner]=getFinalImageAndNoValidCells(layer1Inner,c);
-    figure;imshow(finalLayer1Inner,c)
+%     figure;imshow(finalLayer1Inner,c)
 
     [finalLayer2Outer,validCellsLayer2Outer,noValidCellsLayer2Outer]=getFinalImageAndNoValidCells(layer2Outer,c);
-    figure;imshow(finalLayer2Outer,c)
+%     figure;imshow(finalLayer2Outer,c)
 
     [finalLayer2Inner,validCellsLayer2Inner,noValidCellsLayer2Inner]=getFinalImageAndNoValidCells(layer2Inner,c);
-    figure;imshow(finalLayer2Inner,c)
+%     figure;imshow(finalLayer2Inner,c)
 
-    
+    cellsLayer1 = unique([validCellsLayer1Outer;noValidCellsLayer1Outer;validCellsLayer1Inner;noValidCellsLayer1Inner]);
+    cellsLayer2 = unique([validCellsLayer2Outer;noValidCellsLayer2Outer;validCellsLayer2Inner;noValidCellsLayer2Inner]);
+    finalImages = {finalLayer1Outer,finalLayer1Inner,finalLayer2Outer,finalLayer2Inner};
 end
