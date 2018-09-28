ea729bb91b62133fd7c27eeb344a6f0f00f8d24e
anttagjan <atagua-ibis@us.es>
2018-09-28 14:35:40 +0200

    Choose between a preliminary and a comple analysis
    Design a GUI to choose if you want to do a preliminary analysis (label only the cells) or to do a complete analysis (label cells and lumen and calculate poligon distribution)

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -5,8 +5,14 @@ addpath(genpath('gui'))
 addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
 close all
-[polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
 
+TypeOfAnalysis = ChooseTypeOfAnalysis();
+ if isequal(TypeOfAnalysis, 'Preliminary')
+            LabelImageSequence
+ else
+[polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
+ 
 
 save(strcat(selpath,'polygon_distribution_Apical.mat'))
 save(strcat(selpath,'polygon_distribution_Basal.mat'))
+ end
