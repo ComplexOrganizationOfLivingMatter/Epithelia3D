3a3be3ee494eecd16a6c42af4a2a7043e5e07c63
anttagjan <atagua-ibis@us.es>
2018-09-28 15:16:59 +0200

    Add a break and a reiteration in case of preliminary analysis

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -9,10 +9,11 @@ close all
 TypeOfAnalysis = ChooseTypeOfAnalysis();
  if isequal(TypeOfAnalysis, 'Preliminary')
             LabelImageSequence
- else
+            TypeOfAnalysis = ChooseTypeOfAnalysis();
+ elseif isequal(TypeOfAnalysis, 'Complete')
 [polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
- 
-
 save(strcat(selpath,'polygon_distribution_Apical.mat'))
 save(strcat(selpath,'polygon_distribution_Basal.mat'))
+ else 
+    
  end
