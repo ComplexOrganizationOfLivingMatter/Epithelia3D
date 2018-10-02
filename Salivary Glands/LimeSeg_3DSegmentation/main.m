b3ab5bb1c549e4fb1b6e97524556e667fab60264
Pablo Vicente Munuera <pvicente1@us.es>
2018-10-02 16:42:38 +0200

    proper indentation

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -7,13 +7,11 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 close all
 
 TypeOfAnalysis = ChooseTypeOfAnalysis();
- if isequal(TypeOfAnalysis, 'Preliminary')
-            LabelImageSequence
-            TypeOfAnalysis = ChooseTypeOfAnalysis();
- elseif isequal(TypeOfAnalysis, 'Complete')
-[polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
-save(strcat(selpath,'polygon_distribution_Apical.mat'))
-save(strcat(selpath,'polygon_distribution_Basal.mat'))
- else 
-    
- end
+if isequal(TypeOfAnalysis, 'Preliminary')
+    LabelImageSequence
+    TypeOfAnalysis = ChooseTypeOfAnalysis();
+elseif isequal(TypeOfAnalysis, 'Complete')
+    [polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
+    save(strcat(selpath,'polygon_distribution_Apical.mat'))
+    save(strcat(selpath,'polygon_distribution_Basal.mat'))
+end
