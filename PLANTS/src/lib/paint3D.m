e8b46e2010c49f728a08e529cb0066a8d1110ad7
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-10-03 13:45:17 +0200

    correcting the paint3d

--- PLANTS/src/lib/paint3D.m
+++ PLANTS/src/lib/paint3D.m
@@ -1,18 +1,19 @@
-function [] = paint3D(varargnin)
+function [] = paint3D(varargin)
 %PAINT3D Summary of this function goes here
 %   Detailed explanation goes here
     if nargin==2 
-        labelledImage=varargnin{1};
-        showingCells=varargnin{2};
+        labelledImage=varargin{1};
+        showingCells=varargin{2};
+        
     else
-        labelledImage=varargnin;
+        labelledImage=varargin;
         showingCells = 1:max(labelledImage(:));
     end
     colours = colorcube(double(max(labelledImage(:))));
     colours = colours(randperm(max(labelledImage(:))), :);
 
     figure;
-    for numSeed = showingCells
+    for numSeed = unique(showingCells)'
         % Painting each cell
         [x,y,z] = ind2sub(size(labelledImage),find(labelledImage == numSeed));
         pcshow([x,y,z], colours(numSeed, :));
