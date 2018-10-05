648bcb500704c6a04eabdba1e26db78f8025f840
anttagjan <38495085+anttagjan@users.noreply.github.com>
2018-10-05 13:06:43 +0200

    IImprove the script to determine incorrect cells
    Apply a cellfun.
    But we have to show the real cell labels. We need to compare the index outputs with the ValidCells variable

--- Salivary Glands/LimeSeg_3DSegmentation/FindIncorrectCells.m
+++ Salivary Glands/LimeSeg_3DSegmentation/FindIncorrectCells.m
@@ -1,6 +1,4 @@
 function [IncorrectCells]=FindIncorrectCells(NeighboursData)
 TotalNeighbours=sum(length(NeighboursData));
-IncorrectCellsInf = find(TotalNeighbours < 3); 
-IncorrectCellsSup = find(TotalNeighbours > 10);
-IncorrectCells=[IncorrectCellsInf IncorrectCellsSup];
+IncorrectCells = find(TotalNeighbours < 3 | TotalNeighbours > 10); 
 end 
--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -14,6 +14,12 @@ while ~isequal(typeOfAnalysis, '')
         [polygon_distribution_Apical, polygon_distribution_Basal, polygonDistributions,selpath] = pipeline();
         save(fullfile(selpath,'polygon_distribution_Apical.mat'))
         save(fullfile(selpath,'polygon_distribution_Basal.mat'))
+        
+        if sum([polygon_distribution_Apical{2,:}])|| sum([polygon_distribution_Basal{2,:}])
+            IncorrectApicalCells= cellfun(@FindIncorrectCells,(polygonDistributions{1,1}),'UniformOutput',false);
+            IncorrectBasalCells= cellfun(@FindIncorrectCells,(polygonDistributions{1,2}),'UniformOutput',false);
+        end
+        
     end
     typeOfAnalysis = ChooseTypeOfAnalysis();
 end
