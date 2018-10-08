5815ea341954b36bb817f1f6018892739f806073
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-10-08 12:43:08 +0200

    delete main

--- InSilicoModels/TubularModel/main.m
+++
@@ -1,4 +0,0 @@
-addpath(genpath('src'))
-for numImage = 1:10
-    createSamiraFormatExcel(strcat('data\tubularVoronoiModel\expansion\2048x4096_200seeds\Image_', num2str(numImage),'_Diagram_5\'), 1.6667)
-end
