6991ed7217ed24b824e79c64087ff5d153461071
Pablo Vicente Munuera <pvicente1@us.es>
2018-10-11 12:59:05 +0200

    rotation corrected

--- Salivary Glands/LimeSeg_3DSegmentation/lib/unrollTube.m
+++ Salivary Glands/LimeSeg_3DSegmentation/lib/unrollTube.m
@@ -7,26 +7,27 @@ function [] = unrollTube(img3d)
     [Y, X, Z] = ndgrid(1:size(img3d,1), 1:size(img3d,2), 1:size(img3d,3));
     XYZ0 = [X(:), Y(:), Z(:), zeros(numel(X),1)];
     
-    figure; surf(X(:,:,1), Y(:,:,1), Z(:,:,1), img3d(:,:,1), 'edgecolor', 'none')
-    
-    imgProperties = regionprops3(img3d>0, {'Orientation', 'PrincipalAxisLength'})
+    imgProperties = regionprops3(img3d>0, {'Orientation', 'PrincipalAxisLength'});
     angleRotation = deg2rad(-cat(2,imgProperties.Orientation));
 
     M = makehgtform('zrotate', angleRotation(1)); %X axis
     
     newXYZ0 = XYZ0 * M;
-    nX = reshape(newXYZ0(:,1), size(X));
-    nY = reshape(newXYZ0(:,2), size(Y));
-    nZ = reshape(newXYZ0(:,3), size(Z));
+    nX = round(reshape(newXYZ0(:,1), size(X)));
+    nY = round(reshape(newXYZ0(:,2), size(Y)));
+    nZ = round(reshape(newXYZ0(:,3), size(Z)));
 
-    imgProperties = regionprops3(img3dRotated>0, {'Orientation', 'PrincipalAxisLength'})
     %paint3D(img3dRotated)
-    figure; pcshow([X(img3d(:)>0), Y(img3d(:)>0), Z(img3d(:)>0)])
+%     figure; pcshow([X(img3d(:)>0), Y(img3d(:)>0), Z(img3d(:)>0)])
     figure; pcshow([nX(img3d(:)>0), nY(img3d(:)>0), nZ(img3d(:)>0)])
     
-    img3Rotated = zeros(max(nX), max(nY), max(nZ));
+    img3DRotated = zeros(max(nX(:)), max(nY(:)), max(nZ(:)));
     
+    labelIndices = sub2ind(size(img3DRotated), nX(img3d(:)>0), nY(img3d(:)>0), nZ(img3d(:)>0));
     
+    img3DRotated(labelIndices) = img3d(img3d(:)>0);
+    
+
     %% Unroll
     imgFinalCoordinates=cell(size(img3d,3),1);
     centroids=centers{nImg};
