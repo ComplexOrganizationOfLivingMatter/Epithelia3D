5ef830475d68ed08e67e39ac1fec5998fe67deed
anttagjan <atagua-ibis@us.es>
2018-10-15 17:32:07 +0200

    Indent main

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -9,20 +9,21 @@ close all
 typeOfAnalysis = ChooseTypeOfAnalysis();
 while ~isequal(typeOfAnalysis, '')
     if isequal(typeOfAnalysis, 'New')
-       [noValidCells,validCells,polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
+        [noValidCells,validCells,polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
     elseif isequal(typeOfAnalysis, 'Continue')
         [noValidCells,validCells,polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
-    end 
-        save(fullfile(selpath,'polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
-        save(fullfile(selpath,'valid_cells.mat'), 'noValidCells', 'validCells')
-       
-        if sum([polygon_distribution_Apical{2,:}])|| sum([polygon_distribution_Basal{2,:}])
-            IncorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
-            IncorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
-        end
+    end
     
-       
-        
-  
+    save(fullfile(selpath,'polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
+    save(fullfile(selpath,'valid_cells.mat'), 'noValidCells', 'validCells')
+    
+    if sum([polygon_distribution_Apical{2,:}])|| sum([polygon_distribution_Basal{2,:}])
+        IncorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
+        IncorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
+    end
+    
+    
+    
+    
     typeOfAnalysis = ChooseTypeOfAnalysis();
 end
