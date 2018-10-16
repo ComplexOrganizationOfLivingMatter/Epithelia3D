961ecd246a2d5291d7f6e254495c02325fdd876c
Pablo Vicente Munuera <pvicente1@us.es>
2018-10-16 16:25:24 +0200

    saving all in the correct place

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -9,17 +9,16 @@ close all
 typeOfAnalysis = ChooseTypeOfAnalysis();
 while ~isequal(typeOfAnalysis, '')
     if isequal(typeOfAnalysis, 'New')
-        [noValidCells,validCells,polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
+        [polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
     elseif isequal(typeOfAnalysis, 'Continue')
-        [noValidCells,validCells,polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
+        [polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
     end
     
     save(fullfile(selpath,'polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
-    save(fullfile(selpath,'valid_cells.mat'), 'noValidCells', 'validCells')
     
-    if sum([polygon_distribution_Apical{2,:}])|| sum([polygon_distribution_Basal{2,:}])
-        IncorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
-        IncorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
+    if sum([polygon_distribution_Apical{2,:}]) || sum([polygon_distribution_Basal{2,:}])
+        incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
+        incorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
     end
      
     typeOfAnalysis = ChooseTypeOfAnalysis();
