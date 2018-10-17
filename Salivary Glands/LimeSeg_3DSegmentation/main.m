5faa9286d486931877ecbec52a4527c614e91419
anttagjan <38495085+anttagjan@users.noreply.github.com>
2018-10-17 12:40:08 +0200

    Delete GUI to choose analysis

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -6,20 +6,10 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
 close all
 
-typeOfAnalysis = ChooseTypeOfAnalysis();
-while ~isequal(typeOfAnalysis, '')
-    if isequal(typeOfAnalysis, 'New')
-        [polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
-    elseif isequal(typeOfAnalysis, 'Continue')
-        [polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
-    end
+[polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,selpath] = pipeline();
+save(fullfile(selpath,'polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
     
-    save(fullfile(selpath,'polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
-    
-    if sum([polygon_distribution_Apical{2,:}]) || sum([polygon_distribution_Basal{2,:}])
-        incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
-        incorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
-    end
-     
-    typeOfAnalysis = ChooseTypeOfAnalysis();
+if sum([polygon_distribution_Apical{2,:}]) || sum([polygon_distribution_Basal{2,:}])
+    incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
+    incorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
 end
