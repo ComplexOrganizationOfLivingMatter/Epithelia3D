3c5dee18d7fcb87f367516c535d0f94e1ceb1e07
anttagjan <atagua-ibis@us.es>
2018-10-26 18:52:10 +0200

    Add name fields to the results

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -7,9 +7,14 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 close all
 
 [polygon_distribution_Apical, polygon_distribution_Basal, NeighboursData,neighboursUnrollTube,polygon_distribution_UnrollTube,selpath] = pipeline();
-save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
-    
+
 if sum([polygon_distribution_Apical{2,:}]) || sum([polygon_distribution_Basal{2,:}])
     incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,1})));
     incorrectBasalCells= find(~cellfun(@FindIncorrectCells,(NeighboursData{1,2})));
 end
+
+rowHeadings = {'Apical', 'Basal'};
+polygon_distribution_UnrollTube=cell2struct(polygon_distribution_UnrollTube,rowHeadings,2);
+NeighboursData=cell2struct(NeighboursData,rowHeadings,2);
+neighboursUnrollTube=cell2struct(neighboursUnrollTube,rowHeadings,2);
+save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution_Apical', 'polygon_distribution_Basal', 'NeighboursData')
