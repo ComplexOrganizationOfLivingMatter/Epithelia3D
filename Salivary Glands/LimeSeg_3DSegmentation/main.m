199e92f09671076f1edb3eb3c566db2c88815648
anttagjan <atagua-ibis@us.es>
2018-10-30 11:32:54 +0100

    Fix minor problems

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -10,7 +10,7 @@ close all
 
 save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution', 'neighbours_data', 'neighbours_UnrollTube','polygon_distribution_UnrollTube')
 
-if round(sum(cellfun(@sum,(polygon_distribution.Apical{1,1}(2,:))))) ~=1 || round(sum(cellfun(@sum,(polygon_distribution.Basal{1,1}(2,:))))) ~= 1
+if round(sum(cellfun(@sum,(polygon_distribution.Apical(2,:))))) ~=1 || round(sum(cellfun(@sum,(polygon_distribution.Basal(2,:))))) ~= 1
     incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(neighbours_data.Apical{1,1})));
     incorrectBasalCells= find(~cellfun(@FindIncorrectCells,(neighbours_data.Basal{1,1})));
 end
