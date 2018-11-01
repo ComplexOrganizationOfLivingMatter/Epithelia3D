83280587df06242e7a653af9fa356e593c25a4bc
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-11-01 12:57:30 +0100

    related previous and previous commit

--- PLANTS/src/cylindricalShapes/calculateCenterRadiusCylSection.m
+++ PLANTS/src/cylindricalShapes/calculateCenterRadiusCylSection.m
@@ -1,45 +1,29 @@
-function [centers, radii] = calculateCenterRadiusCylSection(img3d,name2save)
-    mkdir(name2save)
+function [centersOuter, centersInner, radiiOuter, radiiInner] = calculateCenterRadiusCylSection(img3d,name2save,layer2save)
   
+    %Permute image axes
     img3d=permute(img3d,[1 3 2]);
     mask3d=false(size(img3d));
-    mask3d(img3d>0)=1;
-    [x,y,z]=ind2sub(size(mask3d),find(mask3d==1));
-    shp=alphaShape(x,y,z,200);
+    
+    %Get alpha shape of layer image
+    [x,y,z]=ind2sub(size(img3d),find(img3d>0));
+    shp=alphaShape(x,y,z,50);
 %     plot(shp)
-    [qx,qy,qz]=ind2sub(size(mask3d),find(mask3d>=0));
+    [qx,qy,qz]=ind2sub(size(img3d),find(img3d>=0));
     tf = inShape(shp,qx,qy,qz);
     
-    mask3d=uint16(zeros(size(mask3d)));
     indFilImg=sub2ind(size(mask3d),qx(tf),qy(tf),qz(tf));
     mask3d(indFilImg)=1;
+    mask3dInvert = ~mask3d;
+    innerSurface = bwlabeln(mask3dInvert) == 2;
     
-    centers=cell(size(mask3d,3),1);
-    radii=cell(size(mask3d,3),1);
-    for i=1:size(mask3d,3)
-        mask=false(size(mask3d(:,:,i)));
-        [xq,yq]=find(~mask);
-        mask(mask3d(:,:,i)>0)=1;
+    name2saveOuter = ['data\' name2save '\maskLayers\outerMask' layer2save '\'];
+    name2saveInner = ['data\' name2save '\maskLayers\outerMask' layer2save '\'];
+    
+    
+    [centersOuter, radiiOuter] = saveImageGettingCentroids(mask3d,name2saveOuter);
+    [centersInner, radiiInner] = saveImageGettingCentroids(innerSurface,name2saveInner);
 
-        if sum(sum(ismember(mask,1)))>20
-            [x,y]=find(mask);
-            k = convhull(x,y);
-            [in,on] = inpolygon(xq,yq,x(k),y(k));
-
-            mask(in)=1;
-            mask(on)=1;
-            imwrite(mask,[name2save '\' num2str(i),'.bmp'])
-
-            propsMask=regionprops(mask,'Centroid','MajorAxisLength','MinorAxisLength','Area');
-            area=cat(1,propsMask.Area);
-            [~,indMax]=max(area);
-            centroid=cat(1,propsMask.Centroid);
-            majAxis=cat(1,propsMask.MajorAxisLength);
-            minAxis=cat(1,propsMask.MinorAxisLength);
-            centers{i}=[centroid(indMax,1),i,centroid(indMax,2)];
-            radii{i}=mean([majAxis(indMax,:),minAxis(indMax,:)])/2;
-        end
-    end
+    
 end
 
 
