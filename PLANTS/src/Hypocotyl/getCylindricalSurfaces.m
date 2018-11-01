256abb4e860a5e4433ab6971227764c15bd2d6df
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-11-01 11:47:17 +0100

    reestructuring the pipeline for get surfaces of cylindrical shapes

---
+++ PLANTS/src/Hypocotyl/getCylindricalSurfaces.m
@@ -0,0 +1,50 @@
+function [layer1Limited,layer2Limited,setOfCells] = getCylindricalSurfaces(sampleName,rangeY,zScaleFactorHyp)
+
+    disp(sampleName)
+    if ~exist(['data\' sampleName '\image3d_' strrep(sampleName,' ','_') 'Rotated.mat'],'file')
+        img3d = readImg3d(sampleName,zScaleFactorHyp);
+        img3d = rotateImg3(img3d);      
+    else
+        load(['data\rotatedImage3d_' sampleName '.mat'],'rotatedImg3d');
+        img3d = rotatedImage3d;
+    end
+    
+    disp('1 - neighbours calculated')
+     
+    %% Get surfaces 1 and 2 with its cells   
+    if exist(['data\' sampleName '\cellsAndSurfacesPerLayer.mat'],'file')
+        load(['data\' sampleName '\cellsAndSurfacesPerLayer.mat'],'layer1','layer2','layer1Limited','layer2Limited','setOfCells')
+    else
+        if exist(['data\' sampleName '\cellsLayer1Correction.mat'],'file') 
+            load(['data\' sampleName '\cellsLayer1Correction.mat'],'cellsCorrectLayer1')
+            [layer1,layer2,setOfCells.Layer1,setOfCells.Layer2] = getSplittedCylinderPerSurfaces(img3d,cellsCorrectedLayer1);
+%             [layer1.outerSurface,layer1.innerSurface,layer2.outerSurface,layer2.innerSurface,setOfCells.Layer1,setOfCells.Layer2]=getSplittedCylinderPerSurfaces(neighbourhoodInfo,uint16(img3d),cellsCorrectLayer1);
+        else
+            [layer1,layer2,setOfCells.Layer1,setOfCells.Layer2] = getSplittedCylinderPerSurfaces(img3d,[]);
+%             [layer1.outerSurface,layer1.innerSurface,layer2.outerSurface,layer2.innerSurface,setOfCells.Layer1,setOfCells.Layer2]=getSplittedCylinderPerSurfaces(neighbourhoodInfo,uint16(img3d),[]);
+        end
+
+        layer1Limited = layer1(:,rangeY(1):rangeY(2),:);
+        layer2Limited = layer2(:,rangeY(1):rangeY(2),:);
+
+        save(['data\' sampleName '\cellsAndSurfacesPerLayer.mat'],'-v7.3','layer1','layer2','layer1Limited','layer2Limited','setOfCells')
+        save(['data\' sampleName '\cellsAndSurfacesPerLayer.mat'],'-v7.3','layer1','layer2','layer1Limited','layer2Limited','setOfCells')
+
+    end
+        
+    disp('2 - layers captured')
+       
+    %% Get center and axes length from hypocotyl
+    if ~exist(['data\' sampleName '\maskLayers\certerAndRadiusPerZ.mat'],'file')
+        
+        [centers{1}, radiiBasalLayer1] = calculateCenterRadiusCylSection(layer1.outerSurface,['data\' sampleName '\maskLayers\outerMaskLayer1']);
+        [centers{2}, radiiApicalLayer1] = calculateCenterRadiusCylSection(layer1.innerSurface,['data\' sampleName '\maskLayers\innerMaskLayer1']);
+        [centers{3}, radiiBasalLayer2] = calculateCenterRadiusCylSection(layer2.outerSurface,['data\' sampleName '\maskLayers\outerMaskLayer2']);
+        [centers{4}, radiiApicalLayer2] = calculateCenterRadiusCylSection(layer2.innerSurface,['data\' sampleName '\maskLayers\innerMaskLayer2']);
+        save(['data\' sampleName '\maskLayers\certerAndRadiusPerZ.mat'],'centers','radiiBasalLayer1','radiiApicalLayer1','radiiBasalLayer2','radiiApicalLayer2');               
+        
+    end
+    
+    disp('3 - get centroids and infered cylinder axes')
+
+end
