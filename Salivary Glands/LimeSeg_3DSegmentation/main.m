a6234e4f35ac00117175861a2064d30bf00fdd8f
Pablo Vicente Munuera <pvicente1@us.es>
2018-11-09 16:08:02 +0100

    commenting unroll tube

--- Salivary Glands/LimeSeg_3DSegmentation/main.m
+++ Salivary Glands/LimeSeg_3DSegmentation/main.m
@@ -6,9 +6,9 @@ addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
 
 close all
 
-[polygon_distribution, neighbours_data,neighbours_UnrollTube,polygon_distribution_UnrollTube,selpath] = pipeline();
+[polygon_distribution, neighbours_data,selpath] = pipeline();
 
-save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution', 'neighbours_data', 'neighbours_UnrollTube','polygon_distribution_UnrollTube')
+save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution', 'neighbours_data')
 
 if sum(cellfun(@sum,(polygon_distribution.Apical(2,:)))) ~=1 || sum(cellfun(@sum,(polygon_distribution.Basal(2,:)))) ~= 1
     apical_neighbours={neighbours_data.Apical};
