d85c6fed8902f8c31f227b7308e0ecd4deee577f
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-11-12 14:09:39 +0100

    generalize the 3d Voronoi and Frusta building

--- InSilicoModels/TubularModel/correlationVolumeAreaSidesSurfaceRatio.m
+++ InSilicoModels/TubularModel/correlationVolumeAreaSidesSurfaceRatio.m
@@ -1,16 +1,17 @@
 %% LEWIS - EULER 3D
 addpath(genpath('src'))
 %input parameters
-initialDiagram  = 5;
+initialDiagram  = 1;
 nRealizations = 20;
 W_init = 512;
 H_init = 4096;
 nSeeds = 200;
 typeProjection = 'expansion';
 surfaceRatios = 1./(1:-0.1:0.1);
-reductionFactor = 1;
+reductionFactor = 5;
 totalCells = 1:nSeeds;
 namesSR = arrayfun(@(x) ['sr' strrep(num2str(x),'.','_')],surfaceRatios,'UniformOutput', false);
+cyliderType = 'Voronoi';%Voronoi
 
 
 numNeighPerSurface = cell(nRealizations,1);
@@ -19,27 +20,31 @@ areaCellsPerSurface = cell(nRealizations,1);
 volumePerSurface = cell(nRealizations,1);
 
 %path to load and save
-rootPath =  ['D:\Pedro\Epithelia3D\InSilicoModels\TubularModel\data\tubularVoronoiModel\' typeProjection '\'];
-folder = [num2str(W_init) 'x' num2str(H_init) '_' num2str(nSeeds) 'seeds\'];
+rootPath =  ['data\tubularVoronoiModel\' typeProjection '\'];
+folder = [num2str(W_init) 'x' num2str(H_init) '_' num2str(nSeeds) 'seeds\diagram' num2str(initialDiagram) '\'];
 
-% if ~exist([rootPath folder 'relationAreaVolumeSidesSurfaceRatio.mat'],'file')
+if ~exist([rootPath folder 'relationAreaVolumeSidesSurfaceRatio_' cyliderType '.mat'],'file')
 
     for nImg = 1 : nRealizations
         load([rootPath folder 'Image_' num2str(nImg) '_Diagram_' num2str(initialDiagram)...
             '\Image_' num2str(nImg) '_Diagram_' num2str(initialDiagram) '.mat'],'listLOriginalProjection','listSeedsProjected');
-
+        if iscell(listSeedsProjected)
+            listSeedsProjected= cell2table(listSeedsProjected,'VariableNames',{'surfaceRatio','seedsApical'});
+            listLOriginalProjection = cell2table(listLOriginalProjection,'VariableName',{'surfaceRatio','L_originalProjection'});
+        end
+    
         seedsApical = listSeedsProjected.seedsApical{listSeedsProjected.surfaceRatio==1};
         noValidCells = cell(length(surfaceRatios),1);
         neighsSurface = cell(length(surfaceRatios),1);
         neighsAccumSurfaces = cell(length(surfaceRatios),1);
         areaCells = cell(length(surfaceRatios),1);
         volumes = cell(length(surfaceRatios),1);
+        
+        voronoi3D = [];
         for idSR = 1:length(surfaceRatios)
 
             L_img = listLOriginalProjection.L_originalProjection{listLOriginalProjection.surfaceRatio==surfaceRatios(idSR)};
             [neighsSurface{idSR},~] = calculateNeighbours(L_img);
-
-
 
             noValidCellSR = unique([unique(L_img(1,:)),unique(L_img(end,:))]);
             noValidCells{idSR} = noValidCellSR(noValidCellSR~=0);
@@ -47,14 +52,34 @@ folder = [num2str(W_init) 'x' num2str(H_init) '_' num2str(nSeeds) 'seeds\'];
             areaCells{idSR} = cat(1,area.Area);
 
             if idSR == 1
+                L_imgApical = L_img;
                 neighsAccumSurfaces{idSR} = neighsSurface{idSR};
                 volumes{idSR} =  areaCells{idSR};
+                [voronoi3D] = create3DCylinder( seedsApical(:,2:3), H_init, W_init, surfaceRatios, max(surfaceRatios),reductionFactor,L_imgApical,cyliderType);
+
             else 
+                %get invalid region
+                H_init=round(H_init/reductionFactor);
+                W_init=round(W_init/reductionFactor);
+                R_apical=W_init/(2*pi);
+                R_apical=round(R_apical);
+                R_basal=surfaceRatios(idSR)*R_apical;
+                R_basal=round(R_basal);
+                R_basalMax=max(surfaceRatios)*R_apical;
+                R_basalMax=round(R_basalMax);
+                [imgInvalidRegion,~,~,~]=get3DCylinderLimitsBasalApicalandIntermediate(R_basal,R_basalMax,R_apical,H_init,[]);
+
                 neighsAccumSurfaces{idSR}  = cellfun(@(x,y) unique([x;y]),neighsAccumSurfaces{idSR-1},neighsSurface{idSR},'UniformOutput',false);
-                [voronoi3D] = create3DCylinder( seedsApical(:,2:3), H_init, W_init, surfaceRatios(idSR),reductionFactor,[]);
-%                 voronoi3D = imresize(voronoi3D,reductionFactor,'nearest');
-                volumes{idSR} = arrayfun(@(x) sum(voronoi3D(:) == x),totalCells');
+                voronoi3DSR = voronoi3D;
+                voronoi3DSR(imgInvalidRegion>0)=0;
+                voronoi3Dresized = imresize(voronoi3DSR,reductionFactor,'nearest');
+                totalLabelsRepeated = voronoi3Dresized(voronoi3Dresized(:)>0);
+                if length(unique(totalLabelsRepeated)) < max(totalCells)
+                    disp('ohhhh shit')
+                end
+                volumes{idSR} = arrayfun(@(x) sum(totalLabelsRepeated(:) == x),totalCells');
             end
+            disp(['Completed volume realization - ' num2str(nImg) ' - SR - ' num2str(surfaceRatios(idSR))])
         end
         numNeighPerSurfaceRealization = cellfun(@(x) length(x),cat(1,neighsSurface{:})');
         numNeighAccumPerSurfacesRealization = cellfun(@(x) length(x),cat(1,neighsAccumSurfaces{:})');
@@ -69,15 +94,14 @@ folder = [num2str(W_init) 'x' num2str(H_init) '_' num2str(nSeeds) 'seeds\'];
         areaCellsPerSurface{nImg} = array2table(areaCellsPerSurfaceRealization(validCellsTotal,:),'VariableNames',namesSR);
         volumePerSurface{nImg} = array2table(volumePerSurfaceRealization(validCellsTotal,:),'VariableNames',namesSR);
         
-        disp(['Completed volume realization - ' num2str(nImg)])
+        
     end
 
-%     save([rootPath folder 'relationAreaVolumeSidesSurfaceRatio.mat'],'numNeighPerSurface','numNeighAccumPerSurfaces','areaCellsPerSurface','volumePerSurface')
+    save([rootPath folder 'relationAreaVolumeSidesSurfaceRatio_' cyliderType '.mat'],'numNeighPerSurface','numNeighAccumPerSurfaces','areaCellsPerSurface','volumePerSurface')
     
-% else
-%     load([rootPath folder 'relationAreaVolumeSidesSurfaceRatio.mat'],'numNeighPerSurface','numNeighAccumPerSurfaces','areaCellsPerSurface','volumePerSurface')
-% end
+else
+    load([rootPath folder 'relationAreaVolumeSidesSurfaceRatioVoronoi_' cyliderType '.mat'],'numNeighPerSurface','numNeighAccumPerSurfaces','areaCellsPerSurface','volumePerSurface')
+end
 
-% path2save = [rootPath folder 'lewisEuler\'];
-% 
-% getStatsAndRepresentationsEulerLewis3D(numNeighPerSurface,numNeighAccumPerSurfaces,areaCellsPerSurface,volumePerSurface,path2save);
+path2save = [rootPath folder 'lewisEuler\'];
+getStatsAndRepresentationsEulerLewis3D(numNeighPerSurface,numNeighAccumPerSurfaces,areaCellsPerSurface,volumePerSurface,path2save);
--- InSilicoModels/TubularModel/src/lib/libVoronoiSegment/create3DCylinder.m
+++ InSilicoModels/TubularModel/src/lib/libVoronoiSegment/create3DCylinder.m
@@ -1,4 +1,4 @@
-function [voronoi3D]=create3DCylinder( initialSeeds, H_apical, W_apical, surfaceRatio,reductionFactor,intermediateSurfaceRatios)
+function [maskCylinder3D]=create3DCylinder( initialSeeds, H_apical, W_apical,surfaceRatios, surfaceRatio,reductionFactor,imgApical,cyliderType)
 
     H_apical=round(H_apical/reductionFactor);
     W_apical=round(W_apical/reductionFactor);
@@ -9,23 +9,69 @@ function [voronoi3D]=create3DCylinder( initialSeeds, H_apical, W_apical, surface
     R_apical=round(R_apical);
     R_basal=surfaceRatio*R_apical;
     R_basal=round(R_basal);
+    R_basalMax=max(surfaceRatios)*R_apical;
+    R_basalMax=round(R_basalMax);
     
-    
-    %% Relocate seeds from 2d image to cylinder image
-    [basalCylinderSeedsPositions,apicalCylinderSeedsPositions]=seedsRelocationInCylinderCoordinatesFrom2D(R_basal,R_apical,W_apical,initialSeeds);
-
     %% Get limitations of space and surfaces 3d indexes
-    [imgInvalidRegion,~,~,~]=get3DCylinderLimitsBasalApicalandIntermediate(R_basal,R_apical,H_apical,intermediateSurfaceRatios);
+    [imgInvalidRegion,~,~,~]=get3DCylinderLimitsBasalApicalandIntermediate(R_basal,R_basalMax,R_apical,H_apical,[]);
         
-    %% Find pixels of a seed segment
-    maskOfGlobalImage=zeros(2*R_basal+1,2*R_basal+1,H_apical,'uint16');
-    for i=1:length(apicalCylinderSeedsPositions.x)
-        maskOfGlobalImage=Drawline3D(maskOfGlobalImage,apicalCylinderSeedsPositions.x(i),apicalCylinderSeedsPositions.y(i),apicalCylinderSeedsPositions.z(i), basalCylinderSeedsPositions.x(i), basalCylinderSeedsPositions.y(i), basalCylinderSeedsPositions.z(i),i);
-    end
+    if contains(lower(cyliderType),'frusta')
+        imgBasal = imresize(mask1,[size(imgApical,1),round(size(imgApical,2)*surfaceRatio)],'nearest');
+        
+        maskCylinder3D = zeros(size(imgInvalidRegion),'uint16');
 
-    %% Calculation of distance matrix applying the invalid region
-    distSegments=bwdist(maskOfGlobalImage);
-	voronoi3D=watershed(distSegments,26);
-    voronoi3D(imgInvalidRegion>0) = 0;
+        for nCell = uniqCells'
+            %dilate for getting joined cells in basal
+            [rowCoordApi,colCoordApi] = find(imgApical == nCell);
+            [rowCoordBas,colCoordBas] = find(imgBasal == nCell);
+
+            coordApical = [rowCoordApi, colCoordApi];
+            coordApical = coordApical / reductionFactor;
+            coordBasal = [rowCoordBas, colCoordBas];
+            coordBasal = coordBasal / reductionFactor;
+
+            %%Relocate coord from 2d image to cylinder image and extrapolate to basal
+            [~,coordApicalCyl]=seedsRelocationInCylinderCoordinatesFrom2D(R_basal,R_apical,W_apical,coordApical);
+            [~,coordBasalCyl]=seedsRelocationInCylinderCoordinatesFrom2D(R_basal,R_basal,round(W_apical*surfaceRatio),coordBasal);
+            coord=[[coordBasalCyl.x;coordApicalCyl.x],[coordBasalCyl.y;coordApicalCyl.y],[coordBasalCyl.z;coordApicalCyl.z]];
+                   
+            idZrange = (zIn >= min(coord(:,3)) & zIn <= max(coord(:,3)));
+            idYrange = (yIn >= min(coord(:,2)) & yIn <= max(coord(:,2)));
+            idXrange = (xIn >= min(coord(:,1)) & xIn <= max(coord(:,1)));
+            idBoundBox = idZrange & idYrange & idXrange;
+            
+            coordZrange = [xIn(idBoundBox),yIn(idBoundBox),zIn(idBoundBox)];
+            shp=alphaShape(coord,5);
+            idIn=shp.inShape(coordZrange);
+            labelInd = sub2ind(size(maskCylinder3D),coordZrange(idIn,:));
+            maskCylinder3D(labelInd) = nCell;
+            
+        end
+        maskCylinder3D(imgInvalidRegion>0) = 0;
+
+    else
+
+        %% Relocate seeds from 2d image to cylinder image
+        [basalCylinderSeedsPositions,apicalCylinderSeedsPositions]=seedsRelocationInCylinderCoordinatesFrom2D(R_basalMax,R_apical,W_apical,initialSeeds);
+
+        %% Find pixels of a seed segment
+        maskOfGlobalImage=zeros(2*R_basalMax+1,2*R_basalMax+1,H_apical,'uint16');
+        for i=1:length(apicalCylinderSeedsPositions.x)
+            maskOfGlobalImage=Drawline3D(maskOfGlobalImage,apicalCylinderSeedsPositions.x(i),apicalCylinderSeedsPositions.y(i),apicalCylinderSeedsPositions.z(i), basalCylinderSeedsPositions.x(i), basalCylinderSeedsPositions.y(i), basalCylinderSeedsPositions.z(i),i);
+        end
+
+        %% Calculation of distance matrix applying the invalid region
+        distSegments=bwdist(maskOfGlobalImage);
+        Cylinder3D=watershed(distSegments,26);
+        Cylinder3D(imgInvalidRegion>0) = 0;
+
+        %% Relabel 3D cells
+        maskCylinder3D = Cylinder3D;
+        for numSeed = 1:length(apicalCylinderSeedsPositions.x)
+            cellLabelOld = unique(Cylinder3D(maskOfGlobalImage==numSeed));
+            cellLabelOld = cellLabelOld(cellLabelOld>0);
+            maskCylinder3D(Cylinder3D==cellLabelOld) = numSeed;
+        end
+    end
 
 end
