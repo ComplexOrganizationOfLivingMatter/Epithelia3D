1d485e94d7b79f39ff80cdb09d85266b142162f0
Pablo Vicente Munuera <pvicente1@us.es>
2018-11-20 10:41:25 +0100

    cleaning original image

--- Salivary Glands/LimeSeg_3DSegmentation/lib/fillEmptySpacesByWatershed2D.m
+++ Salivary Glands/LimeSeg_3DSegmentation/lib/fillEmptySpacesByWatershed2D.m
@@ -1,20 +1,39 @@
 function [finalImage] = fillEmptySpacesByWatershed2D(labelMask, invalidRegion, colours)
 %FILLEMPTYSPACESBYWATERSHED Summary of this function goes here
 %   Detailed explanation goes here
-    %labelMaskEroded = imerode(labelMask, strel('sphere', 1));
     
-    labelMaskEroded = labelMask;
+    %erodingRegion = strel('disk', 2);
+    labelMaskCleaned = bwareaopen(labelMask, 10);
+    labelMaskCleaned = labelMask .* labelMaskCleaned;
+    labelMaskEroded = zeros(size(labelMask));
     for numCell = 1:max(labelMask(:))
-        labelMaskPerim = bwperim(labelMask == numCell, 4);
-        labelMaskEroded(labelMaskPerim) = 0;
+        %labelMaskPerim = bwperim(labelMask == numCell, 4);
+        %labelMaskEroded(labelMaskPerim) = 0;
+        %erodedCell = imerode(labelMask == numCell, erodingRegion);
+        dilatedCell = bwmorph(labelMaskCleaned == numCell, 'shrink', 2);
+        dilatedCell = bwmorph(dilatedCell, 'close', 2);
+%         dilatedCell = imdilate(dilatedCell, erodingRegion);
+%         dilatedCell = bwmorph(dilatedCell, 'majority');
+%         erodedCell = imerode(dilatedCell, erodingRegion);
+        labelMaskEroded(dilatedCell) = numCell;
     end
     
-    maskDist=bwdist(labelMaskEroded>0);
+    closedNeighbours = strel([0 1 0; 1 0 1; 0 1 0]);
+    labelMaskFinal = labelMaskEroded;
+    %Remove neighbouring cells
+    for numCell = 1:max(labelMask(:))
+        actualVicinity = imdilate(labelMaskEroded == numCell, closedNeighbours);
+        neighs = unique(labelMaskEroded(actualVicinity));
+        neighs(neighs == numCell | neighs == 0) = [];
+        if isempty(neighs) == 0
+            labelMaskFinal(ismember(labelMaskEroded .* actualVicinity, neighs)) = 0;
+        end
+    end
+    
+    maskDist=bwdist(labelMaskFinal>0);
     maskWater=watershed(maskDist, 4);
-    %     maskDist=bwdist(maskWater>0);
-    %     maskWater=watershed(maskDist,4);
     %figure;imshow(maskWater,c)
-    maskWater(invalidRegion)=0;
+    maskWater(invalidRegion)=0; 
     
     %% relabel cells with original labels
     centroids = regionprops(maskWater,'Centroid');
