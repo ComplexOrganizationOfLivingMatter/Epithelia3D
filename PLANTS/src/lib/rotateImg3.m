31ebf6eae89e5ffecb9c75291edae365992e81d4
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-11-21 11:07:36 +0100

    rotation along the 3 dimensions

--- PLANTS/src/lib/rotateImg3.m
+++ PLANTS/src/lib/rotateImg3.m
@@ -2,8 +2,18 @@ function rotatedImg3d = rotateImg3(img3d)
 
     img3d_reduced=imresize3(img3d,0.05,'nearest');
     orientationObj = regionprops3(img3d_reduced>0, 'Orientation');
-    img3d_rot = imrotate(img3d, - orientationObj.Orientation(1));
+    img3d_rot1 = imrotate(img3d, - orientationObj.Orientation(1));
 
+    xzyImg = permute(img3d_rot1,[1 3 2]);
+    orientationObj = regionprops3(imresize3(xzyImg,0.05,'nearest')>0, 'Orientation');
+    xzyImg3d_rot = imrotate(xzyImg, - orientationObj.Orientation(1));
+
+    yzxImg3d_rot = permute(xzyImg3d_rot,[3 2 1]);
+    orientationObj = regionprops3(imresize3(yzxImg3d_rot,0.05,'nearest')>0, 'Orientation');
+
+    img3d_rotFinal = imrotate(yzxImg3d_rot, - orientationObj.Orientation(1));
+    img3d_rot = permute(img3d_rotFinal,[3 2 1]);
+    
     [xCol, yRow, z] = ind2sub(size(img3d_rot),find(img3d_rot>0));
     if min(xCol) <= 0 || min(yRow) <= 0 || min(z) <= 0
 
