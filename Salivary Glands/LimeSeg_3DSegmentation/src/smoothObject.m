bf501771c883800ecb606f6abd01cba46593fc8b
Pablo Vicente Munuera <pvicente1@us.es>
2018-11-22 11:34:46 +0100

    fixing lumen problem

--- Salivary Glands/LimeSeg_3DSegmentation/src/smoothObject.m
+++ Salivary Glands/LimeSeg_3DSegmentation/src/smoothObject.m
@@ -3,11 +3,12 @@ function [labelledImage] = smoothObject(labelledImage,pixelLocations, numCell)
 %   Detailed explanation goes here
     cellShape = alphaShape(pixelLocations, 20, 'HoleThreshold', size(labelledImage, 1)*size(labelledImage, 2)/2);
     [qx,qy,qz]=ind2sub(size(labelledImage),find(labelledImage == 0));
+    actualCell = zeros(size(labelledImage));
     try
         tf = inShape(cellShape,qx,qy,qz);
         inCellIndices = sub2ind(size(labelledImage), qx(tf), qy(tf), qz(tf));
-        labelledImage(inCellIndices) = numCell;
-        filledCell = imfill(double(labelledImage==numCell),  4, 'holes');
+        actualCell(inCellIndices) = 1;
+        filledCell = imfill(double(actualCell),  4, 'holes');
         filledCellOpen = imopen(filledCell, strel('sphere', 1));
         labelledImage(filledCellOpen>0) = numCell;
         %labelledImage(filledCell) = numCell;
