6a248d96321e753ba317532e79727933f81b4436
Pablo Vicente Munuera <pvicente1@us.es>
2018-11-29 16:27:25 +0100

    now is working with valid cells

--- LimeSeg_3DSegmentation/src/divideObjectInSurfaceRatios.m
+++ LimeSeg_3DSegmentation/src/divideObjectInSurfaceRatios.m
@@ -1,16 +1,18 @@
-function [] = divideObjectInSurfaceRatios(obj_img, startingSurface, endSurface)
+function [allSurfaceRatioImages] = divideObjectInSurfaceRatios(obj_img, startingSurface, endSurface, validCells)
 %DIVIDEOBJECTINSURFACERATIOS Summary of this function goes here
 %   Detailed explanation goes here
 
+    obj_img = ismember(obj_img, validCells) .* obj_img;
+    startingSurface = ismember(startingSurface, validCells) .* startingSurface;
     [outsideObject] = getOutsideGland(obj_img);
-
+    
     stepDilation = strel('sphere', 1);
     %We start at the outer surface
     actualSurface = imclose(startingSurface>0, strel('sphere', 1));
     numSurface = 1;
     
     
-    while any(actualSurface(:) & endSurface(:))
+    while any(endSurface(actualSurface)) == 0
         allSurfaceRatioImages{numSurface} = actualSurface;
         
         outsideObject(actualSurface) = 1;
@@ -20,6 +22,7 @@ function [] = divideObjectInSurfaceRatios(obj_img, startingSurface, endSurface)
         dilatedActualSurface(outsideObject) = 0;
         numSurface = numSurface + 1;
         actualSurface = dilatedActualSurface;
+        figure; paint3D(actualSurface)
     end
 
 end
