0c6d123f1a3644ffd839da1ad819855f76c532bc
anttagjan <atagua-ibis@us.es>
2018-11-29 12:04:45 +0100

    Minor error

--- LimeSeg_3DSegmentation/main.m
+++ LimeSeg_3DSegmentation/main.m
@@ -2,7 +2,7 @@
 addpath(genpath('src'))
 addpath(genpath('lib'))
 addpath(genpath('gui'))
-addpath(genpath(fullfile('..', '..', 'InSilicoModels', 'TubularModel', 'src')));
+addpath(genpath(fullfile('..','InSilicoModels', 'TubularModel', 'src')));
 
 close all
 
