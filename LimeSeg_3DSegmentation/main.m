a45357a6c1fdd9b4636ca5e7fc745e877725b789
Pablo Vicente Munuera <pvicente1@us.es>
2018-11-30 15:33:39 +0100

    refactor of the pipeline

--- LimeSeg_3DSegmentation/main.m
+++ LimeSeg_3DSegmentation/main.m
@@ -6,14 +6,17 @@ addpath(genpath(fullfile('..','InSilicoModels', 'TubularModel', 'src')));
 
 close all
 
-[polygon_distribution, neighbours_data,selpath] = pipeline();
+selpath = uigetdir('data');
+if isempty(selpath) == 0
+    [polygon_distribution, neighbours_data] = pipeline(selpath);
 
-save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution', 'neighbours_data')
+    save(fullfile(selpath,'Results/polygon_distribution.mat'), 'polygon_distribution', 'neighbours_data')
 
-if sum(cellfun(@sum,(polygon_distribution.Apical(2,:)))) ~=1 || sum(cellfun(@sum,(polygon_distribution.Basal(2,:)))) ~= 1
-    apical_neighbours={neighbours_data.Apical};
-    basal_neighbours={neighbours_data.Basal};
-    incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(apical_neighbours{1,1})));
-    incorrectBasalCells= find(~cellfun(@FindIncorrectCells,basal_neighbours{1,1}));
+    if sum(cellfun(@sum,(polygon_distribution.Apical(2,:)))) ~=1 || sum(cellfun(@sum,(polygon_distribution.Basal(2,:)))) ~= 1
+        apical_neighbours={neighbours_data.Apical};
+        basal_neighbours={neighbours_data.Basal};
+        incorrectApicalCells= find(~cellfun(@FindIncorrectCells,(apical_neighbours{1,1})));
+        incorrectBasalCells= find(~cellfun(@FindIncorrectCells,basal_neighbours{1,1}));
+    end
 end
 
