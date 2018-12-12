c8f3cc3f5e678cc3d212128f7b5ba0e8709cf987
Pablo Vicente Munuera <pvicente1@us.es>
2018-12-12 14:47:44 +0100

    working until the surface ratio appears

--- LimeSeg_3DSegmentation/obtainStatsFromGlands.m
+++ LimeSeg_3DSegmentation/obtainStatsFromGlands.m
@@ -1,6 +1,6 @@
 
-
-files = dir('**/3d_layers_info.mat');
+clear all
+files = dir('**/Salivary gland/**/3d_layers_info.mat');
 
 numberOfSurfaceRatios = 2;
 
@@ -8,19 +8,33 @@ for numFile = 1:length(files)
     load(fullfile(files(numFile).folder, files(numFile).name));
     load(fullfile(files(numFile).folder, 'valid_cells.mat'));
     
-    neighsSurface{1} = apical3dInfo.neighbourhood;
-    neighsAccumSurfaces{1} = apical3dInfo.neighbourhood;
+    neighsSurface{1} = apical3dInfo.neighbourhood';
+    neighsAccumSurfaces{1} = apical3dInfo.neighbourhood';
+    area = regionprops(apicalLayer,'Area');
+    areaCells{1} = cat(1,area.Area);
+    volume = regionprops3(labelledImage,'Volume');
+    volumes{1} = cat(1,volume.Volume);
     
     for idSR = 2:numberOfSurfaceRatios
-        [neighsSurface{idSR},~] = basal3dInfo.neighbourhood;
+        neighsSurface{idSR} = basal3dInfo.neighbourhood';
         neighsAccumSurfaces{idSR}  = cellfun(@(x,y) unique([x;y]),neighsAccumSurfaces{idSR-1},neighsSurface{idSR},'UniformOutput',false);
+        
+        area = regionprops(basalLayer,'Area');
+        areaCells{idSR} = cat(1,area.Area);
+        volume = regionprops3(labelledImage,'Volume');
+        volumes{idSR} = cat(1,volume.Volume);
     end
+    
+    areaCellsPerSurfaceRealization = cat(2,areaCells{:});
+    volumePerSurfaceRealization = cat(2,volumes{:});
+    neighsSurface = cat(1,neighsSurface{:})';
+    neighsAccumSurfaces = cat(1,neighsAccumSurfaces{:})';
     
     numNeighPerSurfaceRealization = cellfun(@(x) length(x),neighsSurface);
     numNeighAccumPerSurfacesRealization = cellfun(@(x) length(x),neighsAccumSurfaces);
     
-    numNeighOfNeighPerSurfacesRealization = zeros(numberOfSurfaceRatios);
-    numNeighOfNeighAccumPerSurfacesRealization = zeros(numberOfSurfaceRatios);
+    numNeighOfNeighPerSurfacesRealization = zeros(size(neighsSurface));
+    numNeighOfNeighAccumPerSurfacesRealization = zeros(size(neighsSurface));
     for nSR = 1:numberOfSurfaceRatios
         numNeighOfNeighPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighPerSurfaceRealization(x,nSR)))/length(x),neighsSurface(:,nSR));
         numNeighOfNeighAccumPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighAccumPerSurfacesRealization(x,nSR)))/length(x),neighsAccumSurfaces(:,nSR));
@@ -30,7 +44,7 @@ for numFile = 1:length(files)
     numNeighAccumPerSurfaces{numFile} = array2table(numNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
     numNeighOfNeighPerSurface{numFile} = array2table(numNeighOfNeighPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
     numNeighOfNeighAccumPerSurface{numFile} = array2table(numNeighOfNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
-    areaCellsPerSurface{nImg} = array2table(areaCellsPerSurfaceRealization(validCells,:),'VariableNames',namesSR);
-    volumePerSurface{nImg} = array2table(volumePerSurfaceRealization(validCells,:),'VariableNames',namesSR);
+    areaCellsPerSurface{numFile} = array2table(areaCellsPerSurfaceRealization(validCells,:),'VariableNames',namesSR);
+    volumePerSurface{numFile} = array2table(volumePerSurfaceRealization(validCells,:),'VariableNames',namesSR);
 end
 getStatsAndRepresentationsEulerLewis3D(numNeighOfNeighPerSurface,numNeighOfNeighAccumPerSurface,numNeighPerSurface,numNeighAccumPerSurfaces,areaCellsPerSurface,volumePerSurface,path2save,surfaceRatios);
