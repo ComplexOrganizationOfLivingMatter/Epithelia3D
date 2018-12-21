2e8d77dd2a7bbd781cb707fdae7047245c103c6f
Pedro Javier Gómez Gálvez <pgomez-ibis@us.es>
2018-12-21 14:08:31 +0100

    adding percentaje scutoids vs neighs3D

--- InSilicoModels/TubularModel/src/lib/libEulerLewisAboavWeaire3D/curveFittingLn.m
+++ InSilicoModels/TubularModel/src/lib/libEulerLewisAboavWeaire3D/curveFittingLn.m
@@ -1,56 +1,89 @@
+% clear all
+% close all
+
 sr = 1./(1:-0.1:0.1);
 sr = unique([sr,4,6:9,11:15]);
+voronoiDiagram = 1;
 
-myfittypeLn=fittype('a +b*log(x)',...
-'dependent', {'y'}, 'independent',{'x'},...
-'coefficients', {'a','b'});
+folderPath = 'D:\Pedro\Epithelia3D\InSilicoModels\TubularModel\data\tubularVoronoiModel\expansion\512x4096_200seeds\diagram';
+
+file1 = 'polygonsDistributions\dataPolygonDistributionAndPercentageScutoids.mat';
+file2 = 'VoronoiLewisEuler_redFactor_2\relationAreaVolumeSidesSurfaceRatio.mat';
+
+load([folderPath num2str(voronoiDiagram) '\' file1])
+load([folderPath num2str(voronoiDiagram) '\' file2])
+
+% myfittypeLn=fittype('a +b*log(x)',...
+% 'dependent', {'y'}, 'independent',{'x'},...
+% 'coefficients', {'a','b'});
 
 myfittypeLog10=fittype('a +b*log10(x)',...
 'dependent', {'y'}, 'independent',{'x'},...
 'coefficients', {'a','b'});
 
 
-myfittypeExp=fittype('a+x^b',...
-'dependent', {'y'}, 'independent',{'x'},...
-'coefficients', {'a','b'});
+% myfittypeExp=fittype('a+x^b',...
+% 'dependent', {'y'}, 'independent',{'x'},...
+% 'coefficients', {'a','b'});
+% 
+% myfittypeSqrt=fittype('6+b*sqrt(x)',...
+% 'dependent', {'y'}, 'independent',{'x'},...
+% 'coefficients', {'b'});
+% 
+% myfittypePowFit=fittype('a*x^b',...
+% 'dependent', {'y'}, 'independent',{'x'},...
+% 'coefficients', {'a','b'});
 
-myfittypeSqrt=fittype('6+b*sqrt(x)',...
-'dependent', {'y'}, 'independent',{'x'},...
-'coefficients', {'b'});
+neighBasalAcum=cellfun(@(x) mean(x{:,:}),numNeighAccumPerSurfaces,'UniformOutput',false);
 
-myfittypePowFit=fittype('a*x^b',...
-'dependent', {'y'}, 'independent',{'x'},...
-'coefficients', {'a','b'});
-
-meanNeighBasalAcum=cellfun(@(x) mean(x{:,:}),numNeighAccumPerSurfaces,'UniformOutput',false);
-
-stdNeighBasalAcum = std(cat(1,meanNeighBasalAcum{:,:}));
-meanNeighBasalAcum = mean(cat(1,meanNeighBasalAcum{:,:}));
+stdNeighBasalAcum = std(cat(1,neighBasalAcum{:,:}));
+meanNeighBasalAcum = mean(cat(1,neighBasalAcum{:,:}));
 
 meanNeighBasalAcum = meanNeighBasalAcum(1:length(sr));
 stdNeighBasalAcum = stdNeighBasalAcum(1:length(sr));
 
-myfitSqrt=fit(sr',meanNeighBasalAcum',myfittypeSqrt,'StartPoint',[1]);
-figure;plot(myfitSqrt)
-hold on; plot(sr',meanNeighBasalAcum')
-title('Sqrt')
+meanPercScutoids = mean(cell2mat(percentageScutoids));
+stdPercScutoids = std(cell2mat(percentageScutoids));
+meanPercScutoids = meanPercScutoids(1:length(sr));
+stdPercScutoids = stdPercScutoids(1:length(sr));
 
-myfitLn=fit(sr',meanNeighBasalAcum',myfittypeLn,'StartPoint',[1 6]);
-figure;plot(myfitLn)
-hold on; plot(sr',meanNeighBasalAcum')
-title('Ln')
+% 
+% myfitSqrt=fit(sr',meanNeighBasalAcum',myfittypeSqrt,'StartPoint',[1]);
+% figure;plot(myfitSqrt)
+% hold on; plot(sr',meanNeighBasalAcum')
+% title('Sqrt')
+
+% myfitLn=fit(sr',meanNeighBasalAcum',myfittypeLn,'StartPoint',[1 6]);
+% figure;plot(myfitLn)
+% hold on; plot(sr',meanNeighBasalAcum')
+% title('Ln')
 
 myfitLog10=fit(sr',meanNeighBasalAcum',myfittypeLog10,'StartPoint',[1 6]);
 figure;plot(myfitLog10)
 hold on; plot(sr',meanNeighBasalAcum')
 title('Log10')
 
-myfitExp=fit(sr([1,3,5:end])',meanNeighBasalAcum([1,3,5:end])',myfittypeExp,'StartPoint',[1 6]);
-figure;plot(myfitExp)
-hold on; plot(sr([1,3,5:end])',meanNeighBasalAcum([1,3,5:end])')
-title('Exponential')
+figure;
+errorbar(meanPercScutoids(1:8),meanNeighBasalAcum(1:8),stdNeighBasalAcum(1:8))
+xlabel('percentage scutoids')
+ylabel('neighs 3d')
+title(['Voronoi ' num2str(voronoiDiagram)])
 
-myfitPow=fit(sr',meanNeighBasalAcum',myfittypePowFit,'StartPoint',[1 6]);
-figure;plot(myfitPow)
-hold on; plot(sr',meanNeighBasalAcum')
-title('PowerFit')
+xx = meanPercScutoids(1:8);
+yy = meanNeighBasalAcum(1:8);
+
+figure;
+errorbar(sr(1:8),meanNeighBasalAcum(1:8),stdNeighBasalAcum(1:8))
+xlabel('sr')
+ylabel('neighs 3d')
+title(['Voronoi ' num2str(voronoiDiagram)])
+
+% myfitExp=fit(sr([1,3,5:end])',meanNeighBasalAcum([1,3,5:end])',myfittypeExp,'StartPoint',[1 6]);
+% figure;plot(myfitExp)
+% hold on; plot(sr([1,3,5:end])',meanNeighBasalAcum([1,3,5:end])')
+% title('Exponential')
+% 
+% myfitPow=fit(sr',meanNeighBasalAcum',myfittypePowFit,'StartPoint',[1 6]);
+% figure;plot(myfitPow)
+% hold on; plot(sr',meanNeighBasalAcum')
+% title('PowerFit')
