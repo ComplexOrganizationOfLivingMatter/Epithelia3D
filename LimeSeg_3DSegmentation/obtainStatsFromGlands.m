b515f2cd57a75219e5c91f6cfd23e2d3aaf9d946
Pablo Vicente Munuera <pvicente1@us.es>
2019-01-02 16:18:01 +0100

    shit

--- LimeSeg_3DSegmentation/obtainStatsFromGlands.m
+++ LimeSeg_3DSegmentation/obtainStatsFromGlands.m
@@ -9,8 +9,8 @@ for numFile = 1:length(files)
     
     load(fullfile(files(numFile).folder, files(numFile).name));
     load(fullfile(files(numFile).folder, 'valid_cells.mat'));
-    if exist(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios.mat'), 'file')
-        load(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios.mat'))
+    if exist(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios_PredefinedSR.mat'), 'file')
+       load(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios_PredefinedSR.mat'))
     else
         [infoPerSurfaceRatio, neighbours] = divideObjectInSurfaceRatios(labelledImage, basalLayer, apicalLayer, validCells, noValidCells, colours, files(numFile).folder);
     end
@@ -25,7 +25,7 @@ for numFile = 1:length(files)
             NeighsScutoidsPerSF(GlandsSF,:)=[mean(cell2mat(ActualGland.Total_neighbours)),std(cell2mat(ActualGland.Total_neighbours)),mean(ActualGland.Scutoids),std(ActualGland.Scutoids),mean(ActualGland.Surface_Ratio)];
         end
         meanNeighsScutoidsPerSF_ValidCells=array2table(NeighsScutoidsPerSF,'VariableNames',{'mean_neigh3D','std_neigh3D','mean_PercScutoids','std_PercScutoids','Surface_Ratio'});
-        save(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios.mat'), 'infoPerSurfaceRatio', 'neighbours','meanNeighsScutoidsPerSF_ValidCells');
+        save(fullfile(files(numFile).folder, 'glandDividedInSurfaceRatios_PredefinedSR.mat'), 'infoPerSurfaceRatio', 'neighbours','meanNeighsScutoidsPerSF_ValidCells');
     end
     neighsSurface = cell(numberOfSurfaceRatios,1);
     neighsAccumSurfaces = cell(numberOfSurfaceRatios,1);
