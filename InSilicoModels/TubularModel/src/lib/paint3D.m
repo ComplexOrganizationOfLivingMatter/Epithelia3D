2bd266551df038db44a8c007a8ea5aba74436a9d
Pablo Vicente Munuera <pvicente1@us.es>
2019-01-08 16:27:38 +0100

    adding prettyGraphics to paint3D

--- InSilicoModels/TubularModel/src/lib/paint3D.m
+++ InSilicoModels/TubularModel/src/lib/paint3D.m
@@ -6,15 +6,23 @@ function [] = paint3D(varargin)
         showingCells=varargin{2};
         colours = colorcube(double(max(labelledImage(:))));
         colours = colours(randperm(max(labelledImage(:))), :);
+        prettyGraphics = 0;
     elseif nargin == 3
         labelledImage=varargin{1};
         showingCells=varargin{2};
         colours=varargin{3};
+        prettyGraphics = 0;
+    elseif nargin == 4
+        labelledImage=varargin{1};
+        showingCells=varargin{2};
+        colours=varargin{3};
+        prettyGraphics = varargin{4};
     else
         labelledImage=varargin{1};
         showingCells = (1:max(labelledImage(:)))';
         colours = colorcube(double(max(labelledImage(:))));
         colours = colours(randperm(max(labelledImage(:))), :);
+        prettyGraphics = 0;
     end
 
     if isempty(showingCells)
@@ -35,8 +43,29 @@ function [] = paint3D(varargin)
     for numSeed = showingCells
         % Painting each cell
         [x,y,z] = ind2sub(size(labelledImage),find(labelledImage == numSeed));
-        pcshow([x,y,z], colours(numSeed, :));
+        if prettyGraphics
+            shp = alphaShape(x,y,z, 1);
+            pc = criticalAlpha(shp,'one-region');
+            shp.Alpha = pc+3;
+            plot(shp, 'FaceColor', colours(numSeed, :), 'EdgeColor', 'none', 'AmbientStrength', 0.3, 'FaceAlpha', 0.7);
+        else
+            pcshow([x,y,z], colours(numSeed, :));
+        end
         hold on;
     end
+    
+    if prettyGraphics
+        axis equal
+        camlight left;
+        camlight right;
+        lighting flat
+        material dull
+
+        newFig = gca;
+        newFig.XGrid = 'off';
+        newFig.YGrid = 'off';
+        newFig.ZGrid = 'off';
+    end
+    hold off;
 end
 
