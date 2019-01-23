f3a39ac955f5fd476f77c0ae2b26b0eb0ec064c0
Pablo Vicente Munuera <pvicente1@us.es>
2019-01-23 16:29:33 +0100

    adapting for all the surfaces and just 2

--- LimeSeg_3DSegmentation/obtainStatsFromGlands.m
+++ LimeSeg_3DSegmentation/obtainStatsFromGlands.m
@@ -3,96 +3,102 @@ clear all
 close all
 files = dir('**/Salivary gland/**/Results/3d_layers_info.mat');
 
+resultsFileName = '3d_layers_info.mat';
 %resultsFileName = 'glandDividedInSurfaceRatios.mat';
-resultsFileName = 'glandDividedInSurfaceRatios_PredefinedSR.mat';
+%resultsFileName = 'glandDividedInSurfaceRatios_PredefinedSR.mat';
 
 %namesSR = arrayfun(@(x) ['sr' strrep(num2str(x),'.','_')],1:numberOfSurfaceRatios,'UniformOutput', false);
 for numFile = 1:length(files)
-    
-    load(fullfile(files(numFile).folder, files(numFile).name));
-    load(fullfile(files(numFile).folder, 'valid_cells.mat'));
-%     if exist(fullfile(files(numFile).folder, resultsFileName), 'file')
-%        load(fullfile(files(numFile).folder, resultsFileName))
-%     else
-        resizeImg = 0.25;
-        imgSize = round(size(apicalLayer)/resizeImg);
-        apicalLayer = imresize3(apicalLayer, imgSize, 'nearest');
-        basalLayer = imresize3(basalLayer, imgSize, 'nearest');
-        labelledImage = imresize3(labelledImage, imgSize, 'nearest');
-        [infoPerSurfaceRatio, neighbours] = divideObjectInSurfaceRatios(labelledImage, basalLayer, apicalLayer, validCells, noValidCells, colours, files(numFile).folder);
-%     end
-    numberOfSurfaceRatios = size(infoPerSurfaceRatio, 1);
-    
-    if ~exist('meanNeighsScutoidsPerSF_ValidCells', 'var')
-        GeometricalMeasurementsPerSurfaceRatio={infoPerSurfaceRatio{:,4}};
-        NeighsScutoidsPerSF=zeros(numberOfSurfaceRatios,5);
-        for GlandsSF=1:numberOfSurfaceRatios
-            ActualGland=GeometricalMeasurementsPerSurfaceRatio{1,GlandsSF};
-            ActualGland(noValidCells,:)=[];
-            NeighsScutoidsPerSF(GlandsSF,:)=[mean(cell2mat(ActualGland.Total_neighbours)),std(cell2mat(ActualGland.Total_neighbours)),mean(ActualGland.Scutoids),std(ActualGland.Scutoids),mean(ActualGland.Surface_Ratio)];
+    if contains(lower(files(numFile).folder), 'discarded') == 0
+        load(fullfile(files(numFile).folder, files(numFile).name));
+        load(fullfile(files(numFile).folder, 'valid_cells.mat'));
+        if exist(fullfile(files(numFile).folder, resultsFileName), 'file')
+           load(fullfile(files(numFile).folder, resultsFileName))
+        else
+            resizeImg = 0.25;
+            imgSize = round(size(apicalLayer)/resizeImg);
+            apicalLayer = imresize3(apicalLayer, imgSize, 'nearest');
+            basalLayer = imresize3(basalLayer, imgSize, 'nearest');
+            labelledImage = imresize3(labelledImage, imgSize, 'nearest');
+            [infoPerSurfaceRatio, neighbours] = divideObjectInSurfaceRatios(labelledImage, basalLayer, apicalLayer, validCells, noValidCells, colours, files(numFile).folder);
         end
-        meanNeighsScutoidsPerSF_ValidCells=array2table(NeighsScutoidsPerSF,'VariableNames',{'mean_neigh3D','std_neigh3D','mean_PercScutoids','std_PercScutoids','Surface_Ratio'});
-        save(fullfile(files(numFile).folder, resultsFileName), 'infoPerSurfaceRatio', 'neighbours','meanNeighsScutoidsPerSF_ValidCells');
+        if exist('infoPerSurfaceRatio', 'var')
+            numberOfSurfaceRatios = size(infoPerSurfaceRatio, 1);
+        else
+            numberOfSurfaceRatios = 2;
+        end
+
+        if ~exist('meanNeighsScutoidsPerSF_ValidCells', 'var') && exist('infoPerSurfaceRatio', 'var')
+            GeometricalMeasurementsPerSurfaceRatio={infoPerSurfaceRatio{:,4}};
+            NeighsScutoidsPerSF=zeros(numberOfSurfaceRatios,5);
+            for GlandsSF=1:numberOfSurfaceRatios
+                ActualGland=GeometricalMeasurementsPerSurfaceRatio{1,GlandsSF};
+                ActualGland(noValidCells,:)=[];
+                NeighsScutoidsPerSF(GlandsSF,:)=[mean(cell2mat(ActualGland.Total_neighbours)),std(cell2mat(ActualGland.Total_neighbours)),mean(ActualGland.Scutoids),std(ActualGland.Scutoids),mean(ActualGland.Surface_Ratio)];
+            end
+            meanNeighsScutoidsPerSF_ValidCells=array2table(NeighsScutoidsPerSF,'VariableNames',{'mean_neigh3D','std_neigh3D','mean_PercScutoids','std_PercScutoids','Surface_Ratio'});
+            save(fullfile(files(numFile).folder, resultsFileName), 'infoPerSurfaceRatio', 'neighbours','meanNeighsScutoidsPerSF_ValidCells');
+        end
+        neighsSurface = cell(numberOfSurfaceRatios,1);
+        neighsAccumSurfaces = cell(numberOfSurfaceRatios,1);
+        areaCells = cell(numberOfSurfaceRatios,1);
+        volumes = cell(numberOfSurfaceRatios,1);
+
+        neighsSurface{1} = neighbours{1}';
+        neighsAccumSurfaces{1} = neighbours{1}';
+
+        infoOfCells = infoPerSurfaceRatio{1, 4};
+
+        areaCells(1) = {infoOfCells.Basal_area};
+        volumes(1) = {infoOfCells.Volume};
+
+        for idSR = 2:numberOfSurfaceRatios
+            neighsSurface{idSR} = neighbours{idSR}';
+            neighsAccumSurfaces{idSR} = cellfun(@(x,y) unique([x;y]),neighsAccumSurfaces{idSR-1},neighsSurface{idSR},'UniformOutput',false);
+
+            infoOfCells = infoPerSurfaceRatio{idSR, 4};
+            areaCells(idSR) = {infoOfCells.Basal_area};
+            volumes(idSR) = {infoOfCells.Volume};
+        end
+
+        areaCellsPerSurfaceRealization = cat(2,areaCells{:});
+        volumePerSurfaceRealization = cat(2,volumes{:});
+        neighsSurface = cat(1,neighsSurface{:})';
+        neighsAccumSurfaces = cat(1,neighsAccumSurfaces{:})';
+
+        numNeighPerSurfaceRealization = cellfun(@(x) length(x),neighsSurface);
+        numNeighAccumPerSurfacesRealization = cellfun(@(x) length(x),neighsAccumSurfaces);
+
+        numNeighOfNeighPerSurfacesRealization = zeros(size(neighsSurface));
+        numNeighOfNeighAccumPerSurfacesRealization = zeros(size(neighsSurface));
+        for nSR = 1:numberOfSurfaceRatios
+            numNeighOfNeighPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighPerSurfaceRealization(x,nSR)))/length(x),neighsSurface(:,nSR));
+            numNeighOfNeighAccumPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighAccumPerSurfacesRealization(x,nSR)))/length(x),neighsAccumSurfaces(:,nSR));
+        end
+
+        %%
+        meanNumNeighPerSurfaceRealization = mean(numNeighAccumPerSurfacesRealization(validCells, :), 1);
+        numCells = repmat(length(validCells), 1, size(meanNumNeighPerSurfaceRealization, 2));
+        stdNumNeighPerSurfaceRealization = std(numNeighAccumPerSurfacesRealization(validCells, :), 1);
+        totalAreaPerSR = sum(areaCellsPerSurfaceRealization(validCells, :));
+
+        surfaceRatioOfGland = vertcat(infoPerSurfaceRatio{:, 2})';
+
+        infoEuler3D{numFile, 1} = vertcat(meanNumNeighPerSurfaceRealization, stdNumNeighPerSurfaceRealization, surfaceRatioOfGland, numCells)';
+        infoEuler3D{numFile, 2} = meanNeighsScutoidsPerSF_ValidCells;
+        numNeighPerSurface{numFile, 1} = array2table(numNeighPerSurfaceRealization(validCells, :),'VariableNames',namesSR);
+        numNeighAccumPerSurfaces{numFile, 1} = array2table(numNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
+        numNeighOfNeighPerSurface{numFile, 1} = array2table(numNeighOfNeighPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
+        numNeighOfNeighAccumPerSurface{numFile, 1} = array2table(numNeighOfNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
+        areaCellsPerSurface{numFile, 1} = array2table(areaCellsPerSurfaceRealization(validCells,:),'VariableNames',namesSR);
+        volumePerSurface{numFile, 1} = array2table(volumePerSurfaceRealization(validCells,:),'VariableNames',namesSR);
+
+
+
+        %Scutoids per number of sides    
+        [meanWinningPerSidePerFile{numFile, 1}, cellsPerSide{numFile}] = calculateMeanWinning3DNeighbours(numNeighAccumPerSurfacesRealization, validCells);
+        clearvars 'meanNeighsScutoidsPerSF_ValidCells'
     end
-    neighsSurface = cell(numberOfSurfaceRatios,1);
-    neighsAccumSurfaces = cell(numberOfSurfaceRatios,1);
-    areaCells = cell(numberOfSurfaceRatios,1);
-    volumes = cell(numberOfSurfaceRatios,1);
-
-    neighsSurface{1} = neighbours{1}';
-    neighsAccumSurfaces{1} = neighbours{1}';
-
-    infoOfCells = infoPerSurfaceRatio{1, 4};
-
-    areaCells(1) = {infoOfCells.Basal_area};
-    volumes(1) = {infoOfCells.Volume};
-
-    for idSR = 2:numberOfSurfaceRatios
-        neighsSurface{idSR} = neighbours{idSR}';
-        neighsAccumSurfaces{idSR} = cellfun(@(x,y) unique([x;y]),neighsAccumSurfaces{idSR-1},neighsSurface{idSR},'UniformOutput',false);
-
-        infoOfCells = infoPerSurfaceRatio{idSR, 4};
-        areaCells(idSR) = {infoOfCells.Basal_area};
-        volumes(idSR) = {infoOfCells.Volume};
-    end
-
-    areaCellsPerSurfaceRealization = cat(2,areaCells{:});
-    volumePerSurfaceRealization = cat(2,volumes{:});
-    neighsSurface = cat(1,neighsSurface{:})';
-    neighsAccumSurfaces = cat(1,neighsAccumSurfaces{:})';
-
-    numNeighPerSurfaceRealization = cellfun(@(x) length(x),neighsSurface);
-    numNeighAccumPerSurfacesRealization = cellfun(@(x) length(x),neighsAccumSurfaces);
-
-    numNeighOfNeighPerSurfacesRealization = zeros(size(neighsSurface));
-    numNeighOfNeighAccumPerSurfacesRealization = zeros(size(neighsSurface));
-    for nSR = 1:numberOfSurfaceRatios
-        numNeighOfNeighPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighPerSurfaceRealization(x,nSR)))/length(x),neighsSurface(:,nSR));
-        numNeighOfNeighAccumPerSurfacesRealization(:,nSR) = cellfun(@(x) sum(vertcat(numNeighAccumPerSurfacesRealization(x,nSR)))/length(x),neighsAccumSurfaces(:,nSR));
-    end
-
-    %%
-    meanNumNeighPerSurfaceRealization = mean(numNeighAccumPerSurfacesRealization(validCells, :), 1);
-    numCells = repmat(length(validCells), 1, size(meanNumNeighPerSurfaceRealization, 2));
-    stdNumNeighPerSurfaceRealization = std(numNeighAccumPerSurfacesRealization(validCells, :), 1);
-    totalAreaPerSR = sum(areaCellsPerSurfaceRealization(validCells, :));
-
-    surfaceRatioOfGland = vertcat(infoPerSurfaceRatio{:, 2})';
-
-    infoEuler3D{numFile, 1} = vertcat(meanNumNeighPerSurfaceRealization, stdNumNeighPerSurfaceRealization, surfaceRatioOfGland, numCells)';
-    infoEuler3D{numFile, 2} = meanNeighsScutoidsPerSF_ValidCells;
-%     numNeighPerSurface{numFile, 1} = array2table(numNeighPerSurfaceRealization(validCells, :),'VariableNames',namesSR);
-%     numNeighAccumPerSurfaces{numFile, 1} = array2table(numNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
-%     numNeighOfNeighPerSurface{numFile, 1} = array2table(numNeighOfNeighPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
-%     numNeighOfNeighAccumPerSurface{numFile, 1} = array2table(numNeighOfNeighAccumPerSurfacesRealization(validCells, :),'VariableNames',namesSR);
-%     areaCellsPerSurface{numFile, 1} = array2table(areaCellsPerSurfaceRealization(validCells,:),'VariableNames',namesSR);
-%     volumePerSurface{numFile, 1} = array2table(volumePerSurfaceRealization(validCells,:),'VariableNames',namesSR);
-
-
-
-    %Scutoids per number of sides    
-    meanWinningPerSidePerFile{numFile, 1} = calculateMeanWinning3DNeighbours(numNeighAccumPerSurfacesRealization, validCells);
-    clearvars 'meanNeighsScutoidsPerSF_ValidCells'
 end
 
 dim = ndims(meanWinningPerSidePerFile{1});          %# Get the number of dimensions for your arrays
@@ -184,4 +190,4 @@ myfittypeLog10=fittype('6 +b*log10(x)',...
 
 myfitLog10=fit(infoEuler3DCat(:, 3),infoEuler3DCat(:, 1),myfittypeLog10,'StartPoint',1);
 hold on; plot(myfitLog10);
-%getStatsAndRepresentationsEulerLewis3D(numNeighOfNeighPerSurface,numNeighOfNeighAccumPerSurface,numNeighPerSurface,numNeighAccumPerSurfaces,areaCellsPerSurface,volumePerSurface,'Results/SalivaryGlands/',[1 2]);
+getStatsAndRepresentationsEulerLewis3D(numNeighOfNeighPerSurface,numNeighOfNeighAccumPerSurface,numNeighPerSurface,numNeighAccumPerSurfaces,areaCellsPerSurface,volumePerSurface,'Results/SalivaryGlands/',[1 2]);
