3c007f4746c3661e557b1433840607133fd39552
Pablo Vicente Munuera <pvicente1@us.es>
2019-02-20 10:14:37 +0100

    bugfix

--- InSilicoModels/TubularModel/mainSamira.m
+++ InSilicoModels/TubularModel/mainSamira.m
@@ -3,9 +3,11 @@ surfRatios = 1:0.25:5;
 numSeeds = 1000;
 wImg = 2048;
 hImg = 10240;
-path2load = ['data\tubularVoronoiModel\expansion\' num2str(wImg) 'x' num2str(hImg) '_' num2str(numSeeds) 'seeds\diagram' num2str(initialDiagram) '\'];
 numRealizations = 20;
 initialDiagram = 5;
+
+path2load = ['data\tubularVoronoiModel\expansion\' num2str(wImg) 'x' num2str(hImg) '_' num2str(numSeeds) 'seeds\diagram' num2str(initialDiagram) '\'];
+
 
 for numImage = 1 : numRealizations
     createSamiraFormatExcel_Simulations([path2load 'Image_' num2str(numImage) '_Diagram_' num2str(initialDiagram) '\'], surfRatios);
